8abcbe14658d551d97287354823c6787a9ffefdb
simon.porritt <simon.porritt@ebaf65f8-305e-11df-bfeb-7b4c664f286a>
2010-04-25 20:32:26 +0000

    added to project
    git-svn-id: https://jsplumb.googlecode.com/svn/trunk@137 ebaf65f8-305e-11df-bfeb-7b4c664f286a

---
+++ js/jquery.jsPlumb-flowchart.js
@@ -0,0 +1,22 @@
+/**
+
+a flowchart connector for jsPlumb.
+
+*/
+
+(function() {
+	
+	jsPlumb.Connectors.Flowchart = function(params) {
+		
+		this.compute = function(sourcePos, targetPos, sourceAnchor, targetAnchor, lineWidth) { 
+			var dimensions = [];
+			return dimensions;		
+		};
+		
+		this.paint = function(dimensions, ctx) { 
+			
+		};
+			
+	};
+	
+})();
