75f0590209046b10170179dacf3da2fe98a3d12d
simon.porritt <simon.porritt@ebaf65f8-305e-11df-bfeb-7b4c664f286a>
2010-05-02 16:25:07 +0000

    extracted default connectors from the main script.  will provide a means to reassamble everything at a later stage.
    git-svn-id: https://jsplumb.googlecode.com/svn/trunk@185 ebaf65f8-305e-11df-bfeb-7b4c664f286a

--- js/jquery.jsPlumb-1.1.0-RC1.js
+++ js/jquery.jsPlumb-1.1.0-RC1.js
@@ -45,6 +45,7 @@ if (!Array.prototype.indexOf) {
 	var draggableStates = {};
 	var _draggableByDefault = true;
 	var sizes = [];
+	var _jsPlumbContextNode = null;
 	
 	var DEFAULT_NEW_CANVAS_SIZE = 1200; // only used for IE; a canvas needs a size before the init call to excanvas (for some reason. no idea why.)	
 	
@@ -206,13 +207,22 @@ if (!Array.prototype.indexOf) {
     	_operation(elId, f);    	
     };        
     
+    var _getContextNode = function() {
+    	if (_jsPlumbContextNode == null) {
+    		_jsPlumbContextNode= document.createElement("div");
+    		document.body.appendChild(_jsPlumbContextNode);
+    	}
+    	return $(_jsPlumbContextNode);
+    };
+    
     /**
      * helper to create a canvas.
      * @param clazz optional class name for the canvas.
      */
     var _newCanvas = function(clazz) {
         var canvas = document.createElement("canvas");
-        document.body.appendChild(canvas);
+        //document.body.appendChild(canvas);
+        _getContextNode().append(canvas);
         canvas.style.position="absolute";
         if (clazz) { canvas.className=clazz; }
         
@@ -239,7 +249,7 @@ if (!Array.prototype.indexOf) {
      */
     var _removeElement = function(element) {
     	if (element != null) { 
-    		try { document.body.removeChild(element); }
+    		try { _jsPlumbContextNode.removeChild(element); }
     		catch (e) { }
     	}    	
     };
@@ -284,7 +294,7 @@ if (!Array.prototype.indexOf) {
 		// direction, so have a value of 1 or -1 (or, very rarely, 0).  these values are written by the compute method, and read
 		// by the getOrientation method.
 		var xDir = 0, yDir = 0; 
-		
+		// temporary member used to store an orientation when the floating anchor is hovering over another anchor.
 		var orientation = null;
 		
 		this.compute = function(xy, wh, txy, twh) {
@@ -304,11 +314,11 @@ if (!Array.prototype.indexOf) {
 			}
 		};
 		
-		this.setOrientation = function(o) {
-			orientation = o;			
+		this.over = function(anchor) {
+			orientation = anchor.getOrientation();			
 		};
 		
-		this.clearOrientation = function() {
+		this.out = function() {
 			orientation = null;
 		};
 	};
@@ -346,7 +356,9 @@ if (!Array.prototype.indexOf) {
 		this.addConnection = function(connection) {
 			self.connections.push(connection);
 		};
-		
+
+		// get the jsplumb context...lookups are faster with a context.
+		var contextNode = _getContextNode();
 		var isFull = function() { return self.connections.length >= _maxConnections; };
 		
 		this.paint = function(anchorPoint, connectorPaintStyle, canvas) {
@@ -372,14 +384,13 @@ if (!Array.prototype.indexOf) {
 			var start = function(e, ui) {
 				//if (!isFull()) {
 				n = document.createElement("div");
-				document.body.appendChild(n);
-				//document.body.appendChild(n);...seems to be not needed.
+				contextNode.append(n);
 				// create and assign an id, and initialize the offset.
 				id = new String(new Date().getTime());				
-				$(n).attr("id", id);
+				$(n, contextNode).attr("id", id);
 				_updateOffset(id);
-				$(self.canvas).attr("dragId", id);
-				$(self.canvas).attr("elId", _elementId);
+				$(self.canvas, contextNode).attr("dragId", id);
+				$(self.canvas, contextNode).attr("elId", _elementId);
 				
 				var floatingAnchor = new FloatingAnchor({reference:_anchor});
 				floatingEndpoint = new Endpoint({
@@ -394,11 +405,9 @@ if (!Array.prototype.indexOf) {
 					sourceEndpoint:self, 
 					targetEndpoint:floatingEndpoint,
 					source:$(_element),
-					target:$(n),
+					target:$(n, contextNode),
 					anchors:[_anchor, floatingAnchor],
 					paintStyle : params.connectionStyle, // this can be null. jsPlumbConnection will use the default.
-					// todo parameterize.  it should be defined on the endpoint - what is the style of 
-					// connector this endpoint is the source of?
 					connector: params.connector
 				});
 				
@@ -422,7 +431,7 @@ if (!Array.prototype.indexOf) {
 			var options = $.extend( { opacity:0.5, revert:true, helper:'clone', 
 				start : start,
 				drag: function(e, ui) {				
-					_draw($(n), ui);
+					_draw($(n, contextNode), ui);
 					dragFunc(e, ui);
 				}, 				
 				stop : function(e, ui) {
@@ -434,39 +443,25 @@ if (!Array.prototype.indexOf) {
 					stopFunc(e, ui);
 				}
 			}, dragOptions);
-			
-			/*options.start = _wrap(options.start, start);
-			options.drag = _wrap(options.drag, function(e, ui) { _draw($(n)); });
-			options.stop = _wrap(options.stop, function(e, ui) {
-				_removeFromList(endpointsByElement, id, floatingEndpoint);
-				_removeElements([floatingEndpoint.canvas, n]);
-				if (jpc.endpoints[1] == floatingEndpoint) {						
-					_removeElement(jpc.canvas);						
-				}
-			});*/
-			
-			// todo make parameterisable things like opacity/revert
-			$(self.canvas).draggable(options);
+											
+			$(self.canvas, contextNode).draggable(options);
 		}
 		
 		// connector target
 		if (params.isTarget) {
 			var dropOptions = params.dropOptions || jsPlumb.DEFAULT_DROP_OPTIONS; 
 	    	var originalAnchor = null;
-	    	dropOptions.drop = _wrap(dropOptions.drop , function(e, ui) {
-	    		var id = $(ui.draggable).attr("dragId");
+	    	dropOptions.drop = function(e, ui) {
+	    		var id = $(ui.draggable, contextNode).attr("dragId");
 	    		var jpc = floatingConnections[id];
 	    		jpc.target = _element;
 	    		jpc.targetId = _elementId;
 	    		jpc.anchors[1] = _anchor;
 	    		jpc.endpoints[1] = self;
 	    		self.addConnection(jpc);
-	    		jsPlumb.repaint($(ui.draggable).attr("elId"));
-	    		delete floatingConnections[id];
-	    		
-	    		
-
-			 });
+	    		jsPlumb.repaint($(ui.draggable, contextNode).attr("elId"));
+	    		delete floatingConnections[id];	    			    	
+			 };
 	    	// what to do when something is dropped.
 	    	// 1. find the jpc that is being dragged.  the target endpoint of the jpc will be the
 	    	// one that is being dragged.
@@ -479,28 +474,19 @@ if (!Array.prototype.indexOf) {
 	    	// orientation to be the same as the drop target.  this will cause the connector to snap
 	    	// into the shape it will take if the user drops at that point.
 			 
-			dropOptions.over = _wrap(dropOptions.over, function(event, ui) {  
-				var id = $(ui.draggable).attr("dragId");
+			dropOptions.over = function(event, ui) {  
+				var id = $(ui.draggable, contextNode).attr("dragId");
 		    	var jpc = floatingConnections[id];
-		    	/*originalAnchor = jpc.anchors[1];
-		    	jpc.anchors[1] = _anchor;*/
-		    	jpc.anchors[1].setOrientation(_anchor.getOrientation());
-		    	
-		    	//jsPlumb.repaint($(ui.draggable).attr("elId"));
-			 });
+		    	jpc.anchors[1].over(_anchor);		    	
+			 };
 			 
-			 dropOptions.out = _wrap(dropOptions.out, function(event, ui) {  
-				var id = $(ui.draggable).attr("dragId");
+			 dropOptions.out = function(event, ui) {  
+				var id = $(ui.draggable, contextNode).attr("dragId");
 		    	var jpc = floatingConnections[id];
-		    	//if(originalAnchor)	
-		    		//jpc.anchors[1] = originalAnchor;
-		    	jpc.anchors[1].clearOrientation();
-		    	
-		    //jsPlumb.repaint($(ui.draggable).attr("elId"));
-			 });
+		    	jpc.anchors[1].out();
+			 };
 			 		
-			$(self.canvas).droppable(dropOptions);
-			
+			$(self.canvas, contextNode).droppable(dropOptions);			
 		}
 	};
 	
@@ -555,169 +541,7 @@ if (!Array.prototype.indexOf) {
     	BottomLeft : new Anchor({x:0, y:1, orientation:[0,1] })
     },
 
-    /**
-     * Types of connectors, eg. Straight, Bezier.
-     */
-    Connectors :
-    {
-        /**
-         * The Straight connector draws a simple straight line between the two anchor points.
-         */
-        Straight : function() {
-    	
-    		var self = this;
-
-            /**
-             * Computes the new size and position of the canvas.
-             * @param sourceAnchor Absolute position on screen of the source object's anchor.
-             * @param targetAnchor Absolute position on screen of the target object's anchor.
-             * @param positionMatrix  Indicates the relative positions of the left,top of the
-             *  two plumbed objects.  so [0,0] indicates that the source is to the left of, and
-             *  above, the target.  [1,0] means the source is to the right and above.  [0,1] means
-             *  the source is to the left and below.  [1,1] means the source is to the right
-             *  and below.  this is used to figure out which direction to draw the connector in.
-             * @returns an array of positioning information.  the first two values are
-             * the [left, top] absolute position the canvas should be placed on screen.  the
-             * next two values are the [width,height] the canvas should be.  after that each
-             * Connector can put whatever it likes into the array:it will be passed back in
-             * to the paint call.  This particular function stores the origin and destination of
-             * the line it is going to draw.  a more involved implementation, like a Bezier curve,
-             * would store the control point info in this array too.
-             */
-            this.compute = function(sourcePos, targetPos, sourceAnchor, targetAnchor, lineWidth) {
-    			var w = Math.abs(sourcePos[0] - targetPos[0]);
-                var h = Math.abs(sourcePos[1] - targetPos[1]);
-                var widthAdjusted = false, heightAdjusted = false;
-                // these are padding to ensure the whole connector line appears
-                var xo = 0.45 * w, yo = 0.45 * h;
-                // these are padding to ensure the whole connector line appears
-                w *= 1.9; h *=1.9;
-                
-                var x = Math.min(sourcePos[0], targetPos[0]) - xo;
-                var y = Math.min(sourcePos[1], targetPos[1]) - yo;
-                
-                if (w < 2 * lineWidth) { 
-            		// minimum size is 2 * line Width
-            		w = 2 * lineWidth; 
-            		// if we set this then we also have to place the canvas
-            		x = sourcePos[0]  + ((targetPos[0] - sourcePos[0]) / 2) - lineWidth;
-            		xo = (w - Math.abs(sourcePos[0]-targetPos[0])) / 2;
-            	}
-                if (h < 2 * lineWidth) { 
-            		// minimum size is 2 * line Width
-            		h = 2 * lineWidth; 
-            		// if we set this then we also have to place the canvas
-            		y = sourcePos[1]  + ((targetPos[1] - sourcePos[1]) / 2) - lineWidth;
-            		yo = (h - Math.abs(sourcePos[1]-targetPos[1])) / 2;
-            	}
-                
-                // here we check to see if the delta was very small and so the line in
-                // one direction can be considered straight.                
-                var sx = sourcePos[0] < targetPos[0] ? w-xo : xo;
-                var sy = sourcePos[1] < targetPos[1] ? h-yo : yo;
-                var tx = sourcePos[0] < targetPos[0] ? xo : w-xo;
-                var ty = sourcePos[1] < targetPos[1] ? yo : h-yo;
-                var retVal = [ x, y, w, h, sx, sy, tx, ty ];
-                                
-                // return [canvasX, canvasY, canvasWidth, canvasHeight, 
-                //         sourceX, sourceY, targetX, targetY] 
-                return retVal;
-            };
-
-            this.paint = function(dimensions, ctx)
-            {
-                ctx.beginPath();
-                ctx.moveTo(dimensions[4], dimensions[5]);
-                ctx.lineTo(dimensions[6], dimensions[7]);
-                ctx.stroke();
-            };
-        },
-                
-        /**
-         * This Connector draws a Bezier curve with two control points.
-         * @param curviness How 'curvy' you want the curve to be! This is a directive for the
-         * placement of control points, not endpoints of the curve, so your curve does not 
-         * actually touch the given point, but it has the tendency to lean towards it.  the larger
-         * this value, the greater the curve is pulled from a straight line.
-         * 
-         * a future implementation of this could take the control points as arguments, rather
-         * than fixing the curve to one basic shape.
-         */
-        Bezier : function(curviness) {
-        	
-        	var self = this;
-        	this.majorAnchor = curviness || 150;
-            this.minorAnchor = 10;
-            
-            this._findControlPoint = function(point, sourceAnchorPosition, targetAnchorPosition, sourceAnchor, targetAnchor) {
-            	// determine if the two anchors are perpendicular to each other in their orientation.  we swap the control 
-            	// points around if so (code could be tightened up)
-            	var soo = sourceAnchor.getOrientation(), too = targetAnchor.getOrientation();
-            	var perpendicular = soo[0] != too[0] || soo[1] == too[1]; 
-                var p = [];            
-                var ma = self.majorAnchor, mi = self.minorAnchor;                
-                if (!perpendicular) {
-	                  if (soo[0] == 0) // X
-	                    p.push(sourceAnchorPosition[0] < targetAnchorPosition[0] ? point[0] + mi : point[0] - mi);
-	                else p.push(point[0] - (ma * soo[0]));
-	                                 
-	                 if (soo[1] == 0) // Y
-	                	p.push(sourceAnchorPosition[1] < targetAnchorPosition[1] ? point[1] + mi : point[1] - mi);
-	                else p.push(point[1] + (ma * too[1]));
-                }
-                 else {
-	                if (too[0] == 0) // X
-	                	p.push(targetAnchorPosition[0] < sourceAnchorPosition[0] ? point[0] + mi : point[0] - mi);
-	                else p.push(point[0] + (ma * too[0]));
-	                
-	                if (too[1] == 0) // Y
-	                	p.push(targetAnchorPosition[1] < sourceAnchorPosition[1] ? point[1] + mi : point[1] - mi);
-	                else p.push(point[1] + (ma * soo[1]));
-                 }
-
-                return p;                
-            };
-
-            this.compute = function(sourcePos, targetPos, sourceAnchor, targetAnchor, lineWidth)
-            {
-            	lineWidth = lineWidth || 0;
-                var w = Math.abs(sourcePos[0] - targetPos[0]) + lineWidth, h = Math.abs(sourcePos[1] - targetPos[1]) + lineWidth;
-                var canvasX = Math.min(sourcePos[0], targetPos[0])-(lineWidth/2), canvasY = Math.min(sourcePos[1], targetPos[1])-(lineWidth/2);
-                var sx = sourcePos[0] < targetPos[0] ? w - (lineWidth/2): (lineWidth/2), sy = sourcePos[1] < targetPos[1] ? h-(lineWidth/2) : (lineWidth/2);
-                var tx = sourcePos[0] < targetPos[0] ? (lineWidth/2) : w-(lineWidth/2), ty = sourcePos[1] < targetPos[1] ? (lineWidth/2) : h-(lineWidth/2);
-                var CP = self._findControlPoint([sx,sy], sourcePos, targetPos, sourceAnchor, targetAnchor);
-                var CP2 = self._findControlPoint([tx,ty], targetPos, sourcePos, targetAnchor, sourceAnchor);                
-                var minx1 = Math.min(sx,tx); var minx2 = Math.min(CP[0], CP2[0]); var minx = Math.min(minx1,minx2);
-                var maxx1 = Math.max(sx,tx); var maxx2 = Math.max(CP[0], CP2[0]); var maxx = Math.max(maxx1,maxx2);
-                
-                if (maxx > w) w = maxx;
-                if (minx < 0) {
-                    canvasX += minx; var ox = Math.abs(minx);
-                    w += ox; CP[0] += ox; sx += ox; tx +=ox; CP2[0] += ox;
-                }                
-
-                var miny1 = Math.min(sy,ty); var miny2 = Math.min(CP[1], CP2[1]); var miny = Math.min(miny1,miny2);
-                var maxy1 = Math.max(sy,ty); var maxy2 = Math.max(CP[1], CP2[1]); var maxy = Math.max(maxy1,maxy2);
-                if (maxy > h) h = maxy;
-                if (miny < 0) {
-                    canvasY += miny; var oy = Math.abs(miny);
-                    h += oy; CP[1] += oy; sy += oy; ty +=oy; CP2[1] += oy;
-                }
-
-                // return [ canvasx, canvasy, canvasWidth, canvasHeight,
-                //          sourceX, sourceY, targetX, targetY,
-                //          controlPoint1_X, controlPoint1_Y, controlPoint2_X, controlPoint2_Y
-                return [canvasX, canvasY, w, h, sx, sy, tx, ty, CP[0], CP[1], CP2[0], CP2[1] ];
-            };
-
-            this.paint = function(d, ctx) {
-	            ctx.beginPath();
-	            ctx.moveTo(d[4],d[5]);
-	            ctx.bezierCurveTo(d[8],d[9],d[10],d[11],d[6],d[7]);	            
-	            ctx.stroke();
-            }
-        }
-    },
+    
     
     
     /**
@@ -1103,6 +927,7 @@ if (!Array.prototype.indexOf) {
 		delete sizes;
 		delete floatingConnections;
 		delete draggableStates;		
+		document.body.removeChild(_jsPlumbContextNode);
     }
 };
 
