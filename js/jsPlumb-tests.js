addd1850aa3106da6eadf1bf563624c223b51a54
simon.porritt <simon.porritt@ebaf65f8-305e-11df-bfeb-7b4c664f286a>
2010-05-16 22:06:28 +0000

    added tests for max connections functionality
    git-svn-id: https://jsplumb.googlecode.com/svn/trunk@236 ebaf65f8-305e-11df-bfeb-7b4c664f286a

--- js/jsPlumb-tests.js
+++ js/jsPlumb-tests.js
@@ -16,6 +16,14 @@ var assertContextEmpty = function() {
 	equals(_getContextNode().children.length, 0, 'context empty');
 };
 
+var assertEndpointCount = function(elId, count) {
+	equals(jsPlumb.getTestHarness().endpointCount(elId), count, "d1 has " + count + (count > 1) ? "endpoints" : "endpoint");
+};
+
+var assertConnectionCount = function(endpoint, count) {
+	equals(endpoint.connections.length, count, "endpoint has " + count + " connections");
+};
+
 var _addDiv = function(id) {
 	var d1 = document.createElement("div");
 	document.body.appendChild(d1);
@@ -38,6 +46,8 @@ test('plumb two divs with default options', function() {
 	assertContextExists();
 	jsPlumb.detachEverything();
 	assertContextSize(2);  // the two endpoint canvases are still there.
+	assertEndpointCount("d1", 1);
+	assertEndpointCount("d2", 1);
 });
 
 var e;
@@ -45,11 +55,13 @@ test('create a simple endpoint', function() {
 	e = $("#d1").addEndpoint({});
 	ok(e, 'endpoint exists');
 	assertContextSize(3);  // three endpoints now.
+	assertEndpointCount("d1", 2);
 });
 
 test('remove the simple endpoint', function() {
 	$("#d1").removeEndpoint(e);
 	assertContextSize(2);  // two endpoints now.
+	assertEndpointCount("d1", 1);
 });
 
 test('draggable silently ignored when jquery ui not present', function() {
@@ -62,14 +74,53 @@ test('droppable silently ignored when jquery ui not present', function() {
 	ok(e, 'endpoint exists');
 });
 
+test('defaultEndpointMaxConnections', function() {
+	var d3 = _addDiv("d3"), d4 = _addDiv("d4");
+	var e3 = $("#d3").addEndpoint({isSource:true})[0];
+	ok(e3.anchor, 'endpoint 3 has an anchor');
+	var e4 = $("#d4").addEndpoint({isSource:true})[0];
+	assertEndpointCount("d3", 1);
+	assertEndpointCount("d4", 1);
+	ok(!e3.isFull(), "endpoint 3 is not full.");
+	$("#d3").plumb({target:'d4', sourceEndpoint:e3, targetEndpoint:e4});
+	assertConnectionCount(e3, 1);   // we have one connection
+	$("#d3").plumb({target:'d4', sourceEndpoint:e3, targetEndpoint:e4});
+	assertConnectionCount(e3, 1);  // should have refused the connection; default max is 1.	
+});
+
+test('specifiedEndpointMaxConnections', function() {
+	var d5 = _addDiv("d5"), d6 = _addDiv("d6");
+	var e5 = $("#d5").addEndpoint({isSource:true, maxConnections:3})[0];
+	ok(e5.anchor, 'endpoint 5 has an anchor');
+	var e6 = $("#d6").addEndpoint({isSource:true, maxConnections:2})[0];  // this one has max TWO
+	assertEndpointCount("d5", 1); assertEndpointCount("d6", 1);
+	ok(!e5.isFull(), "endpoint 5 is not full.");
+	$("#d5").plumb({target:'d6', sourceEndpoint:e5, targetEndpoint:e6});
+	assertConnectionCount(e5, 1);   // we have one connection
+	$("#d5").plumb({target:'d6', sourceEndpoint:e5, targetEndpoint:e6});
+	assertConnectionCount(e5, 2);  // two connections
+	$("#d5").plumb({target:'d6', sourceEndpoint:e5, targetEndpoint:e6});
+	assertConnectionCount(e5, 2);  // should have refused; max is 2, for d4.
+});
+
+test('noEndpointMaxConnections', function() {
+	var d3 = _addDiv("d3"), d4 = _addDiv("d4");
+	var e3 = $("#d3").addEndpoint({isSource:true, maxConnections:-1})[0];
+	var e4 = $("#d4").addEndpoint({isSource:true, maxConnections:-1})[0];
+	$("#d3").plumb({target:'d4', sourceEndpoint:e3, targetEndpoint:e4});
+	assertConnectionCount(e3, 1);   // we have one connection
+	$("#d3").plumb({target:'d4', sourceEndpoint:e3, targetEndpoint:e4});
+	assertConnectionCount(e3, 2);  // we have two.  etc (default was one. this proves max is working).	
+});
+
 /**
  * leave this test at the bottom!
- *
+ */
 test('unload test', function() {
 	jsPlumb.unload();
 	var contextNode = $("._jsPlumb_context");
 	ok(contextNode.length == 0, 'context node unloaded');
 
-});*/
+});
 
 
