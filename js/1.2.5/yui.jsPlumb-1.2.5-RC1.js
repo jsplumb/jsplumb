3d123b6041a16b1b958b888b679b4834d235afc8
simon.porritt <simon.porritt@ebaf65f8-305e-11df-bfeb-7b4c664f286a>
2011-01-21 05:20:33 +0000

    more work on the YUI draggable connectors.  not done yet, but closer.
    git-svn-id: https://jsplumb.googlecode.com/svn/trunk@715 ebaf65f8-305e-11df-bfeb-7b4c664f286a

--- js/1.2.5/yui.jsPlumb-1.2.5-RC1.js
+++ js/1.2.5/yui.jsPlumb-1.2.5-RC1.js
@@ -1,3 +1,29 @@
+
+/**
+ * addClass				adds a class to the given element
+ * animate				calls the underlying library's animate functionality
+ * appendElement		appends a child element to a parent element.
+ * bind					binds some event to an element
+ * dragEvents			a dictionary of event names
+ * extend				extend some js object with another.  probably not overly necessary; jsPlumb could just do this internally.
+ * getAttribute			gets some attribute from an element
+ * getDragObject		gets the object that is being dragged, by extracting it from the arguments passed to a drag callback
+ * getElementObject		turns an id or dom element into an element object of the underlying library's type.
+ * getOffset			gets an element's offset
+ * getScrollLeft		gets an element's scroll left.  TODO: is this actually used?  will it be?
+ * getScrollTop			gets an element's scroll top.  TODO: is this actually used?  will it be?
+ * getSize				gets an element's size.
+ * getUIPosition		gets the position of some element that is currently being dragged, by extracting it from the arguments passed to a drag callback.
+ * initDraggable		initializes an element to be draggable 
+ * initDroppable		initializes an element to be droppable
+ * isDragSupported		returns whether or not drag is supported for some element.
+ * isDropSupported		returns whether or not drop is supported for some element.
+ * removeClass			removes a class from a given element.
+ * removeElement		removes some element completely from the DOM.
+ * setAttribute			sets an attribute on some element.
+ * setDraggable			sets whether or not some element should be draggable.
+ * setOffset			sets the offset of some element.
+ */
 (function() {
 	
 	var Y;
@@ -6,12 +32,89 @@
 		Y = _Y;
 	});
 	
+	var ddEvents = [
+	     "drag:mouseDown", 
+	     "drag:afterMouseDown", 
+	     "drag:mouseup", 
+	     "drag:align",
+	     "drag:removeHandle",
+	     "drag:addHandle",
+	     "drag:removeInvalid",
+	     "drag:addInvalid",
+	     "drag:start",
+	     "drag:end",
+	     "drag:drag",
+	     "drag:over",
+	     "drag:enter",
+	     "drag:exit",
+	     "drag:drophit",
+	     "drag:dropmiss",
+	     "drop:over",
+	     "drop:enter",
+	     "drop:exit",
+	     "drop:hit"	     	               
+	];
+	
+	/**
+	 * helper function to curry callbacks for some element. 
+	 */
+	var _wrapper = function(fn, el) {
+		return function() { fn(el); };
+	};
+	
+	/**
+	 * extracts options from the given options object, leaving out event handlers.
+	 */
+	var _getDDOptions = function(options) {
+		var o = {};
+		for (var i in options) if (ddEvents.indexOf(i) == -1) o[i] = options[i];
+		return o;
+	};
+	
+	/**
+	 * attaches all event handlers found in options to the given dragdrop object, and registering
+	 * the given el as the element of interest.
+	 */
+	var _attachDDListeners = function(dd, options, el) {
+		// attach event listeners
+	    for (var ev in options) {
+	    	if (ddEvents.indexOf(ev) != -1) {
+	    		var w = _wrapper(options[ev], el);
+	    		dd.on(ev, w);
+	    	}
+	    }
+	};
+	                	
 	jsPlumb.CurrentLibrary = {
+			
+		addClass : function(el, clazz) {
+			el.addClass(clazz);
+		},	
+		
+		/**
+		 * animates the given element.
+		 */
+		animate : function(el, properties, options) {
+			var o = jsPlumb.CurrentLibrary.extend({node:el, to:properties}, options);
+			new Y.Anim(o).run();
+		},
+		
+		appendElement : function(child, parent) {
+			jsPlumb.CurrentLibrary.getElementObject(parent).append(child);			
+		},
+		
+		/**
+		 * event binding wrapper.  
+		 */
+		bind : function(el, event, callback) {
+			el = jsPlumb.CurrentLibrary.getElementObject(el);
+			el.on(event, callback);
+		},
 			
 		dragEvents : {
 			'start':'drag:start', 'stop':'drag:stop', 'drag':'drag:drag', 'step':'step',
-			'over':'over', 'out':'out', 'drop':'drop'
-		},
+			'over':'drag:over', 'out':'drag:exit', 'drop':'drop:hit'
+		},								
 			
 		extend : function(o1, o2) {
 			for (var i in o2)
@@ -19,18 +122,19 @@
 			return o1;
 		},
 		
-		getElementObject : function(el) {
-			return typeof el == 'string' ? Y.one('#' + el) : el;
-		},
-		
 		getAttribute : function(el, attributeId) {
 			return el.getAttribute(attributeId);
+		},				
+		
+		/**
+		 * takes the args passed to an event function and returns you an object representing that which is being dragged.
+		 */
+		getDragObject : function(eventArgs) {
+			alert("YUI getDragObject not implemented yet");
 		},
 		
-		getSize : function(el) {
-			//TODO must be a proper way to get this?
-			var bcr = el._node.getBoundingClientRect();
-			return [ bcr.width, bcr.height ];
+		getElementObject : function(el) {
+			return typeof el == 'string' ? Y.one('#' + el) : Y.one(el);
 		},
 		
 		getOffset : function(el) {
@@ -38,39 +142,78 @@
 			return { left:bcr.left, top:bcr.top };
 		},
 		
-		setAttribute : function(el, attributeName, attributeValue) {
-			el.setAttribute(attributeName, attributeValue);
+		getScrollLeft : function(el) {
+			alert("YUI getScrollLeft not implemented yet");
 		},
 		
-		isDragSupported : function(el) { return true; },
-		isDropSupported : function(el) { return false; },
-		
-		initDraggable : function(el, options) {
-			var drag = function() {
-				console.log("drag");
-				options['drag:drag'](el);
-			};
-			new YUI().use('dd-drag', function(Y) {
-			    var dd = new Y.DD.Drag({
-			        node: '#' + el.getAttribute('id')
-			    });
-			    dd.on('drag:drag', drag);
-			});
+		getScrollTop : function(el) {
+			alert("YUI getScrollTop not implemented yet");
 		},
 		
-		addClass : function(el, clazz) {
-			el.addClass(clazz);
-		},
-		
-		removeClass : function(el, clazz) {
-			el.removeClass(clazz);
+		getSize : function(el) {
+			//TODO must be a proper way to get this?
+			var bcr = jsPlumb.CurrentLibrary.getElementObject(el)._node.getBoundingClientRect();
+			return [ bcr.width, bcr.height ];
 		},
 		
 		getUIPosition : function(args) {		
 			//TODO must be a proper way to get this? args was passed through from the drag function
 			// in initDraggable above - args[0] here is the element that was inited.
-			var bcr = args[0]._node.getBoundingClientRect();
+			var bcr = jsPlumb.CurrentLibrary.getElementObject(args[0])._node.getBoundingClientRect();
 			return { left:bcr.left, top:bcr.top };
-		}			
+		},
+				
+		initDraggable : function(el, options) {
+			var _opts = _getDDOptions(options);
+			_opts.node = "#" + el.getAttribute("id");
+			YUI().use("dd-drag", function(Y) {
+			    var dd = new Y.DD.Drag(_opts);
+			    _attachDDListeners(dd, options, el);			    
+			});
+		},
+		
+		initDroppable : function(el, options) {
+			var _opts = _getDDOptions(options);
+			_opts.node = "#" + el.getAttribute("id");
+			YUI().use("dd-drop", function(Y) {
+				var dd = new Y.DD.Drop(_opts);
+				_attachDDListeners(dd, options, el);
+			});			
+		},
+		
+		isDragSupported : function(el) { 
+			return true;
+		},
+		
+		isDropSupported : function(el) { 
+			return true;
+		},
+										
+		removeClass : function(el, clazz) {
+			el.removeClass(clazz);
+		},
+		
+		removeElement : function(el) {
+			alert("YUI removeElement not implemented yet");
+		},
+		
+		setAttribute : function(el, attributeName, attributeValue) {
+			el.setAttribute(attributeName, attributeValue);
+		},
+		
+		/**
+		 * sets the draggable state for the given element
+		 */
+		setDraggable : function(el, draggable) {
+			alert("YUI setDraggable not implemented yet");
+		},
+		
+		setOffset : function(el, o) {
+			el = jsPlumb.CurrentLibrary.getElementObject(el);
+			el.set("top", o.top);
+			el.set("left", o.left);
+		}
+		
+					
 	};		
 })();
