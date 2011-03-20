fd233cb629a506cb75215a26861dc683c0df570d
simon.porritt <simon.porritt@ebaf65f8-305e-11df-bfeb-7b4c664f286a>
2011-03-20 04:24:20 +0000

    added code for fix for issue 59: getting the UI position when dragging was incorrect if the UI object had margins applied.  this code looks like it will be useful in general. not sure yet if i need to replicate in the MooTools and YUI adapters.
    git-svn-id: https://jsplumb.googlecode.com/svn/trunk@801 ebaf65f8-305e-11df-bfeb-7b4c664f286a

--- js/1.2.6/jquery.jsPlumb-1.2.6-RC1.js
+++ js/1.2.6/jquery.jsPlumb-1.2.6-RC1.js
@@ -163,9 +163,12 @@
 		 * see getDragObject as well
 		 */
 		getUIPosition : function(eventArgs) {
-			var ui = eventArgs[1];
-			//return ui.absolutePosition || ui.offset;
-			return ui.offset || ui.absolutePosition;
+			var ui = eventArgs[1], _offset = ui.offset;
+			if (_offset) {
+				_offset.top = _offset.top + parseInt(ui.helper.css('margin-top').slice(0,-2));
+				_offset.left = _offset.left + parseInt(ui.helper.css('margin-left').slice(0,-2))
+			}
+			return _offset || ui.absolutePosition;
 		},		
 		
 		hasClass : function(el, clazz) {
