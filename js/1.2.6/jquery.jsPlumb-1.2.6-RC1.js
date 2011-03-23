47ac86b346d8d8c9f7bf2016aa86631c3c7a67ea
simon.porritt <simon.porritt@ebaf65f8-305e-11df-bfeb-7b4c664f286a>
2011-03-23 12:34:31 +0000

    backed out the fix for issu 59. breaks IE.
    git-svn-id: https://jsplumb.googlecode.com/svn/trunk@815 ebaf65f8-305e-11df-bfeb-7b4c664f286a

--- js/1.2.6/jquery.jsPlumb-1.2.6-RC1.js
+++ js/1.2.6/jquery.jsPlumb-1.2.6-RC1.js
@@ -164,10 +164,12 @@
 		 */
 		getUIPosition : function(eventArgs) {
 			var ui = eventArgs[1], _offset = ui.offset;
-			if (_offset) {
+		/*	if (_offset) {
 				_offset.top = _offset.top + parseInt(ui.helper.css('margin-top').slice(0,-2));
 				_offset.left = _offset.left + parseInt(ui.helper.css('margin-left').slice(0,-2))
 			}
+			fix for issue 59, but breaks in IE.
+			*/
 			return _offset || ui.absolutePosition;
 		},		
 		
