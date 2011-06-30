bf5a3e3d9a2adf0cd70a73639f38ee7fc53e5d57
simon.porritt <simon.porritt@ebaf65f8-305e-11df-bfeb-7b4c664f286a>
2011-06-30 06:04:35 +0000

    gradients were being swapped when elements moved in relation to each other, in svg.
    git-svn-id: https://jsplumb.googlecode.com/svn/trunk@1026 ebaf65f8-305e-11df-bfeb-7b4c664f286a

--- js/1.3.0/jsPlumb-renderers-svg-1.3.0-RC1.js
+++ js/1.3.0/jsPlumb-renderers-svg-1.3.0-RC1.js
@@ -75,7 +75,14 @@
 		// order to how canvas does it.  so we want to keep all the maths the same, but
 		// iterate the actual style declarations in reverse order, if the x indexes are not in order.
 		for (var i = 0; i < style.gradient.stops.length; i++) {
-			var styleToUse = dimensions[4] < dimensions[6] ? style.gradient.stops.length - 1 - i : i;
+			// Straight Connectors and Bezier connectors act slightly differently; this code is a bit of a kludge.  but next version of
+			// jsplumb will be replacing both Straight and Bezier to be generic instances of 'Connector', which has a list of segments.
+			// so, not too concerned about leaving this in for now.
+			var styleToUse = i;
+			if (dimensions.length == 8) 
+				styleToUse = dimensions[4] < dimensions[6] ? i: style.gradient.stops.length - 1 - i;
+			else
+				styleToUse = dimensions[4] < dimensions[6] ? style.gradient.stops.length - 1 - i : i;
 			var stopColor = _convertStyle(style.gradient.stops[styleToUse][1], true);
 			var s = _node("stop", {"offset":Math.floor(style.gradient.stops[i][0] * 100) + "%", "stop-color":stopColor});
 			g.appendChild(s);
