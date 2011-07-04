99c3c8e995362c93c7850423a1bceaa47c294e9d
simon.porritt <simon.porritt@ebaf65f8-305e-11df-bfeb-7b4c664f286a>
2011-07-04 06:43:10 +0000

    extracted common code for dragAnimDemo out into its own JS file.
    git-svn-id: https://jsplumb.googlecode.com/svn/trunk@1059 ebaf65f8-305e-11df-bfeb-7b4c664f286a

---
+++ demo/js/1.3.0/dragAnimDemo-mootools.js
@@ -0,0 +1,57 @@
+/**
+unavoidable separate file for jquery specific parts of the dragAnimDemo.
+*/
+;(function() {
+	
+	jsPlumbDemo.initClearButton = function() {
+		$("clear").addEvent('click', function() { jsPlumb.detachEverything(); });
+	};
+	
+	jsPlumbDemo.initAddButton = function() {
+		$("add").addEvent('click', function() {
+			jsPlumbDemo.addDisc();
+		});
+	};
+	
+	jsPlumbDemo.showDisc = function(id, div, endpoint) {
+	//	endpoint.canvas.style.display = 'none';
+//		$(div).set('opacity', '0');
+		//$(endpoint.canvas).set("opacity","0");
+		div.style.display = "block";
+		//endpoint.canvas.style.display = 'block';
+	//	$(div).tween('opacity', '1');
+		//$(endpoint.canvas).tween("opacity","1");
+	//	endpoint.repaint();
+	};
+	
+	jsPlumbDemo.initHover = function(elId) {
+		$(elId).addEvents({
+			'mouseenter' : function() { $(this).addClass("bigdot-hover"); },
+			'mouseleave' : function() { $(this).removeClass("bigdot-hover"); }
+		});
+	};
+	
+	jsPlumbDemo.initAnimation = function(elId) {
+		$(elId).addEvent('click', function(e, ui) {
+			if ($(this).hasClass("jsPlumb_dragged")) {
+				$(this).removeClass("jsPlumb_dragged");
+				return;
+			}
+			var o = $(this).getPosition();						
+			var s = $(this).getSize();
+			var w = s.x, h = s.y;
+			var c = [o.x + (w/2) - e.page.x, o.y + (h/2) - e.page.y];
+			var oo = [c[0] / w, c[1] / h];
+			var DIST = 450;
+			var dl = Math.abs(oo[0] * DIST);
+			var dt = Math.abs(oo[1] * DIST);
+			if (c[0] > 0) dl = dl * -1;
+			if (c[1] > 0) dt = dt * -1;
+			l = o.x + dl;
+			t = o.y + dt;
+			
+			jsPlumb.animate($(this), {'left':'' + l, 'top':'' + t }, {duration:900});
+		});
+	};
+
+})();
