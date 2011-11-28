8b21ce253b39bc5f5dbf5b66cff3225ca5e29f2f
simon.porritt@gmail.com <simon.porritt@gmail.com@ebaf65f8-305e-11df-bfeb-7b4c664f286a>
2011-11-28 01:40:12 +0000

    beginning work on state machine loopback
    git-svn-id: https://jsplumb.googlecode.com/svn/trunk@1282 ebaf65f8-305e-11df-bfeb-7b4c664f286a

--- js/1.3.4/jsPlumb-connectors-statemachine-1.3.4-RC1.js
+++ js/1.3.4/jsPlumb-connectors-statemachine-1.3.4-RC1.js
@@ -6,6 +6,52 @@ thanks to Brainstorm Mobile Solutions for supporting the development of these.
 */
 ;(function() {
 
+
+	var Line = function(x1, y1, x2, y2) {
+
+	this.m = (y2 - y1) / (x2 - x1);
+	this.b = -1 * ((this.m * x1) - y1);
+	
+	this.rectIntersect = function(x,y,w,h) {
+
+		// so y = b
+		var results = [];
+		// try top face
+		// the equation of the top face is y = (0 * x) + b; y = b.
+//		y = this.m * X + this.b
+//		y - this.b = this.m * X
+		var xInt = (y - this.b) / this.m;
+		// test that the X value is in the line's range.
+		if (xInt >= x && xInt <= (x + w)) results.push([ xInt, (this.m * xInt) + this.b ]);
+		
+		// try right face
+		var yInt = (this.m * (x + w)) + this.b;
+		if (yInt >= y && yInt <= (y + h)) results.push([ (yInt - this.b) / this.m, yInt ]);
+		
+		// bottom face
+		var xInt = ((y + h) - this.b) / this.m;
+		// test that the X value is in the line's range.
+		if (xInt >= x && xInt <= (x + w)) results.push([ xInt, (this.m * xInt) + this.b ]);
+		
+		// try left face
+		var yInt = (this.m * x) + this.b;
+		if (yInt >= y && yInt <= (y + h)) results.push([ (yInt - this.b) / this.m, yInt ]);
+
+		if (results.length == 2) {
+			var midx = (results[0][0] + results[1][0]) / 2, midy = (results[0][1] + results[1][1]) / 2;
+			results.push([ midx,midy ]);
+			// now calculate the segment inside the rectangle where the midpoint lies.
+			var xseg = midx <= x + (w / 2) ? -1 : 1,
+			yseg = midy <= y + (h / 2) ? -1 : 1;
+			results.push([xseg, yseg]);
+			return results;
+		}
+		
+		return null;
+
+	};
+};
+
 	/*
 		Function: StateMachine constructor
 		
@@ -30,95 +76,142 @@ thanks to Brainstorm Mobile Solutions for supporting the development of these.
 		params = params || {};		
 		
 		this.compute = function(sourcePos, targetPos, sourceEndpoint, targetEndpoint, sourceAnchor, targetAnchor, lineWidth, minWidth) {
-        	var w = Math.abs(sourcePos[0] - targetPos[0]),
-            h = Math.abs(sourcePos[1] - targetPos[1]),
-            // these are padding to ensure the whole connector line appears
-            xo = 0.45 * w, yo = 0.45 * h;
-            // these are padding to ensure the whole connector line appears
-            w *= 1.9; h *=1.9;
+		
+		//	if (sourceEndpoint.elementId != targetEndpoint.elementId) {
+				var w = Math.abs(sourcePos[0] - targetPos[0]),
+   	 	        	h = Math.abs(sourcePos[1] - targetPos[1]),
+	   	   	      	// these are padding to ensure the whole connector line appears
+   	   	      		xo = 0.45 * w, yo = 0.45 * h;
+   	         		// these are padding to ensure the whole connector line appears
+            		w *= 1.9; h *= 1.9,
             
-            var x = Math.min(sourcePos[0], targetPos[0]) - xo;
-            var y = Math.min(sourcePos[1], targetPos[1]) - yo;
+            		x = Math.min(sourcePos[0], targetPos[0]) - xo,
+            		y = Math.min(sourcePos[1], targetPos[1]) - yo,
             
-            // minimum size is 2 * line Width if minWidth was not given.
-            var calculatedMinWidth = Math.max(2 * lineWidth, minWidth);
+            		// minimum size is 2 * line Width if minWidth was not given.
+            		calculatedMinWidth = Math.max(2 * lineWidth, minWidth);
             
-            if (w < calculatedMinWidth) { 
-        		w = calculatedMinWidth; 
-        		x = sourcePos[0]  + ((targetPos[0] - sourcePos[0]) / 2) - (calculatedMinWidth / 2);
-        		xo = (w - Math.abs(sourcePos[0]-targetPos[0])) / 2;
-        	}
-            if (h < calculatedMinWidth) {         
-        		h = calculatedMinWidth; 
-        		y = sourcePos[1]  + ((targetPos[1] - sourcePos[1]) / 2) - (calculatedMinWidth / 2);
-        		yo = (h - Math.abs(sourcePos[1]-targetPos[1])) / 2;
-        	}
+            	if (w < calculatedMinWidth) { 
+        			w = calculatedMinWidth; 
+        			x = sourcePos[0]  + ((targetPos[0] - sourcePos[0]) / 2) - (calculatedMinWidth / 2);
+        			xo = (w - Math.abs(sourcePos[0]-targetPos[0])) / 2;
+        		}
+            	if (h < calculatedMinWidth) {         
+        			h = calculatedMinWidth; 
+        			y = sourcePos[1]  + ((targetPos[1] - sourcePos[1]) / 2) - (calculatedMinWidth / 2);
+        			yo = (h - Math.abs(sourcePos[1]-targetPos[1])) / 2;
+        		}
                             
-            _sx = sourcePos[0] < targetPos[0] ?  xo : w-xo;
-            _sy = sourcePos[1] < targetPos[1] ? yo:h-yo;
-            _tx = sourcePos[0] < targetPos[0] ? w-xo : xo;
-            _ty = sourcePos[1] < targetPos[1] ? h-yo : yo;
+        		_sx = sourcePos[0] < targetPos[0] ?  xo : w-xo;
+            	_sy = sourcePos[1] < targetPos[1] ? yo:h-yo;
+            	_tx = sourcePos[0] < targetPos[0] ? w-xo : xo;
+            	_ty = sourcePos[1] < targetPos[1] ? h-yo : yo;
             
-            // now adjust for the margin
-            if (sourcePos[2] == 0) _sx -= margin;
-            if (sourcePos[2] == 1) _sx += margin;
-            if (sourcePos[3] == 0) _sy -= margin;
-            if (sourcePos[3] == 1) _sy += margin;
-            if (targetPos[2] == 0) _tx -= margin;
-            if (targetPos[2] == 1) _tx += margin;
-            if (targetPos[3] == 0) _ty -= margin;
-            if (targetPos[3] == 1) _ty += margin;
+            	// now adjust for the margin
+            	if (sourcePos[2] == 0) _sx -= margin;
+            	if (sourcePos[2] == 1) _sx += margin;
+            	if (sourcePos[3] == 0) _sy -= margin;
+            	if (sourcePos[3] == 1) _sy += margin;
+            	if (targetPos[2] == 0) _tx -= margin;
+            	if (targetPos[2] == 1) _tx += margin;
+            	if (targetPos[3] == 0) _ty -= margin;
+            	if (targetPos[3] == 1) _ty += margin;
 
-            //
-            // these connectors are quadratic bezier curves, having a single control point. if both anchors 
-            // are located at 0.5 on their respective faces, the control point is set to the midpoint and you
-            // get a straight line.  this is also the case if the two anchors are within 'proximityLimit', since
-            // it seems to make good aesthetic sense to do that. outside of that, the control point is positioned 
-            // at 'curviness' pixels away along the normal to the straight line connecting the two anchors.
-            // 
-            // there may be two improvements to this.  firstly, we might actually support the notion of avoiding nodes
-            // in the UI, or at least making a good effort at doing so.  if a connection would pass underneath some node,
-            // for example, we might increase the distance the control point is away from the midpoint in a bid to
-            // steer it around that node.  this will work within limits, but i think those limits would also be the likely
-            // limits for, once again, aesthetic good sense in the layout of a chart using these connectors.
-            //
-            // the second possible change is actually two possible changes: firstly, it is possible we should gradually
-            // decrease the 'curviness' as the distance between the anchors decreases; start tailing it off to 0 at some
-            // point (which should be configurable).  secondly, we might slightly increase the 'curviness' for connectors
-            // with respect to how far their anchor is from the center of its respective face. this could either look cool,
-            // or stupid, and may indeed only work in a way that is so subtle as to have been a waste of time.
-            //
+            	//
+	            // these connectors are quadratic bezier curves, having a single control point. if both anchors 
+    	        // are located at 0.5 on their respective faces, the control point is set to the midpoint and you
+        	    // get a straight line.  this is also the case if the two anchors are within 'proximityLimit', since
+           	 	// it seems to make good aesthetic sense to do that. outside of that, the control point is positioned 
+           	 	// at 'curviness' pixels away along the normal to the straight line connecting the two anchors.
+	            // 
+   	        	// there may be two improvements to this.  firstly, we might actually support the notion of avoiding nodes
+            	// in the UI, or at least making a good effort at doing so.  if a connection would pass underneath some node,
+            	// for example, we might increase the distance the control point is away from the midpoint in a bid to
+            	// steer it around that node.  this will work within limits, but i think those limits would also be the likely
+            	// limits for, once again, aesthetic good sense in the layout of a chart using these connectors.
+            	//
+            	// the second possible change is actually two possible changes: firstly, it is possible we should gradually
+            	// decrease the 'curviness' as the distance between the anchors decreases; start tailing it off to 0 at some
+            	// point (which should be configurable).  secondly, we might slightly increase the 'curviness' for connectors
+            	// with respect to how far their anchor is from the center of its respective face. this could either look cool,
+            	// or stupid, and may indeed only work in a way that is so subtle as to have been a waste of time.
+            	//
 
-			var _segment = function(x1, y1, x2, y2) {
-				if (x1 < x2 && y2 < y1) return 1;
-				else if (x1 < x2 && y1 < y2) return 2;
-				else if (x2 < x1 && y2 > y1) return 3;
-				return 4;
-			};
-
-			var _findMultipliers = function(anchorX, anchorY, otherAnchorX, otherAnchorY, theta2, segment) {			
-				var m = 1, n = 1;
-				if (segment == 1 && otherAnchorY == 1) m = -1;
-				if (segment == 4 && otherAnchorY == 1) n = -1;				
-				if (segment == 3 && otherAnchorY == 0) n = -1;								
-				// TODO other cases?
-				return [ n * Math.cos(theta2), m * Math.sin(theta2) ];
-			},
-            _midx = (_sx + _tx) / 2, _midy = (_sy + _ty) / 2, 
-            m2 = (-1 * _midx) / _midy, theta2 = Math.atan(m2),            
-            dy =  Math.abs(curviness / 2 * Math.sin(m2)),
-			dx =  Math.abs(curviness / 2 * Math.cos(m2)),
-			segment = _segment(_sx, _sy, _tx, _ty),			
-			multz = _findMultipliers(sourcePos[2], sourcePos[3], targetPos[2], targetPos[3], theta2, segment),
-			distance = Math.sqrt(Math.pow(_tx - _sx, 2) + Math.pow(_ty - _sy, 2));
+				var _segment = function(x1, y1, x2, y2) {
+					if (x1 < x2 && y2 < y1) return 1;
+					else if (x1 < x2 && y1 < y2) return 2;
+					else if (x2 < x1 && y2 > y1) return 3;
+					return 4;
+				},
+				findMultz = function(sourceAnchorPos, targetAnchorPos, theta2) {
+					// find the pos that is marked as "1" or "0", meaning it lies on that edge. y = 0 is top,
+					// y = 1 is bottom, x=0 left, x=1 right.
+					var xm = 1, ym = 1, flip = false;
+					if (sourceAnchorPos[3] == 0) {
+						ym = -1, xm = sourceAnchorPos[2] < 0.5 ? -1 : sourceAnchorPos[2] == 0.5 ? 0 : 1, flip = (targetAnchorPos[3] != 0 && targetAnchorPos[3] != 1);
+					}
+					else if (sourceAnchorPos[3] == 1) {
+						ym = 1, xm = sourceAnchorPos[2] < 0.5 ? -1 : sourceAnchorPos[2] == 0.5 ? 0 : 1, flip = (targetAnchorPos[3] != 0 && targetAnchorPos[3] != 1);					
+					}
+					else if (sourceAnchorPos[2] == 0) {
+						xm = -1, ym = sourceAnchorPos[3] < 0.5 ? -1 : sourceAnchorPos[3] == 0.5 ? 0 : 1, flip = (targetAnchorPos[2] != 0 && targetAnchorPos[2] != 1);					
+					}
+					else if (sourceAnchorPos[2] == 1) {
+						xm = 1, ym = sourceAnchorPos[3] < 0.5 ? -1 : sourceAnchorPos[3] == 0.5 ? 0 : 1, flip = (targetAnchorPos[2] != 0 && targetAnchorPos[2] != 1);					
+					}
+					
+					return [xm, ym];
+				},
+            	_midx = (_sx + _tx) / 2, _midy = (_sy + _ty) / 2, 
+            	m2 = (-1 * _midx) / _midy, theta2 = Math.atan(m2),            
+            	dy =  Math.abs(curviness / 2 * Math.sin(m2)),
+				dx =  Math.abs(curviness / 2 * Math.cos(m2)),
+				segment = _segment(_sx, _sy, _tx, _ty),			
+				multz = findMultz(sourcePos, targetPos),
+				distance = Math.sqrt(Math.pow(_tx - _sx, 2) + Math.pow(_ty - _sy, 2));
 			
-            // calculate the control point.  this code will be where we'll put in a rudimentary element avoidance scheme; it
-            // will work by extending the control point to force the curve to be, um, curvier.
-            _controlX = _midx + ((distance > proximityLimit) ? (multz[0] * dx) : 0);
-			_controlY = _midy + ((distance > proximityLimit) ? (multz[1] * dy) : 0) ;
+            	// calculate the control point.  this code will be where we'll put in a rudimentary element avoidance scheme; it
+            	// will work by extending the control point to force the curve to be, um, curvier.
+            	if (sourceEndpoint.elementId != targetEndpoint.elementId) {
+	            	_controlX = _midx + ((distance > proximityLimit) ? (multz[0] * dx) : 0);
+					_controlY = _midy + ((distance > proximityLimit) ? (multz[1] * dy) : 0);
+				}
+				else {
+					// the loopback case.  needs work.
+					_controlY = _midy - 100;
+					_controlX = _midx;
+				}
+				
+				// now for a rudimentary avoidance scheme. TODO: how to set this in a cross-library way?  
+		/*		var testLine = new Line(sourcePos[0] + _sx,sourcePos[1] + _sy,sourcePos[0] + _tx,sourcePos[1] + _ty);
+				var sel = $(".w");
+				sel.each(function(i,e) {
+					var id = jsPlumb.getId(e);
+					if (id != sourceEndpoint.elementId && id != targetEndpoint.elementId) {
+						o = jsPlumb.getOffset(id), s = jsPlumb.getSize(id);
+
+						if (o && s) {
+							var collision = testLine.rectIntersect(o.left,o.top,s[0],s[1]);
+							if (collision) {
+								// set the control point to be a certain distance from the midpoint of the two points that
+								// the line crosses on the rectangle.
+								// TODO where will this 75 number come from?
+					//			_controlX = collision[2][0] + (75 * collision[3][0]);
+				//	/			_controlY = collision[2][1] + (75 * collision[3][1]);							
+							}
+						}
+					}
+				});*/
             
-            currentPoints = [ x, y, w, h, _sx, _sy, _tx, _ty, _controlX, _controlY ];                        
-                             
+            	currentPoints = [ x, y, w, h, _sx, _sy, _tx, _ty, _controlX, _controlY ];                        
+                
+     //       }
+      //      else {
+            	// a loopback connector.  draw an arc from one anchor to the other.
+            	// i guess we'll do this the same way as the others.  just the control point will be a fair distance away.
+            	
+        //    }
+                
             return currentPoints;
         };
         
@@ -172,16 +265,37 @@ thanks to Brainstorm Mobile Solutions for supporting the development of these.
 	/*
      * Canvas state machine renderer. 
      */
-    jsPlumb.Connectors.canvas.StateMachine = function() {   	 
-		var self = this;
+    jsPlumb.Connectors.canvas.StateMachine = function(params) {   	 
+    	params = params || {};
+		var self = this, drawGuideline = params.drawGuideline || true, avoidSelector = params.avoidSelector;
 		jsPlumb.Connectors.StateMachine.apply(this, arguments);
 		jsPlumb.CanvasConnector.apply(this, arguments);
-		this._paint = function(dimensions) {
+	
 		
-	        self.ctx.beginPath();
-			self.ctx.moveTo(dimensions[4], dimensions[5]);
-			self.ctx.quadraticCurveTo(dimensions[8], dimensions[9], dimensions[6], dimensions[7]);
-	        self.ctx.stroke();            
+		this._paint = function(dimensions) {
+			
+			if (dimensions.length == 10) {
+		        self.ctx.beginPath();
+				self.ctx.moveTo(dimensions[4], dimensions[5]);
+				self.ctx.quadraticCurveTo(dimensions[8], dimensions[9], dimensions[6], dimensions[7]);
+				self.ctx.stroke();            
+				
+				// draw the guideline
+				if (drawGuideline) {
+//					var midx = (dimensions[0] + dimensions[2]) / 2, midy = (dimensions[1] + dimensions[3]) / 2;
+					self.ctx.save();
+					self.ctx.beginPath();
+					self.ctx.strokeStyle = "silver";
+					self.ctx.lineWidth = 1;
+					self.ctx.moveTo(dimensions[4], dimensions[5]);
+					self.ctx.lineTo(dimensions[6], dimensions[7]);
+					self.ctx.stroke();            
+					self.ctx.restore();
+				}
+			}
+			else {
+				// a loopback connector
+			}
 	    };	    
 	    
 	    this.createGradient = function(dim, ctx) {
@@ -196,7 +310,13 @@ thanks to Brainstorm Mobile Solutions for supporting the development of these.
 		var self = this;
 		jsPlumb.Connectors.StateMachine.apply(this, arguments);
 		jsPlumb.SvgConnector.apply(this, arguments);
-		this.getPath = function(d) { return "M " + d[4] + " " + d[5] + " C " + d[8] + " " + d[9] + " " + d[8] + " " + d[9] + " " + d[6] + " " + d[7]; };	    	    
+		this.getPath = function(d) { 	
+			if (d.length == 10) 
+				return "M " + d[4] + " " + d[5] + " C " + d[8] + " " + d[9] + " " + d[8] + " " + d[9] + " " + d[6] + " " + d[7]; 
+			else {
+				// loopback
+			}
+		};	    	    
     };
     
     /*
@@ -206,9 +326,14 @@ thanks to Brainstorm Mobile Solutions for supporting the development of these.
 		jsPlumb.Connectors.StateMachine.apply(this, arguments);	
 		jsPlumb.VmlConnector.apply(this, arguments);
 		var _conv = jsPlumb.vml.convertValue;
-		this.getPath = function(d) {			
-			return "m" + _conv(d[4]) + "," + _conv(d[5]) + 
-				   " c" + _conv(d[8]) + "," + _conv(d[9]) + "," + _conv(d[8]) + "," + _conv(d[9]) + "," + _conv(d[6]) + "," + _conv(d[7]) + " e";
+		this.getPath = function(d) {	
+			if (d.length == 10) {
+				return "m" + _conv(d[4]) + "," + _conv(d[5]) + 
+					   " c" + _conv(d[8]) + "," + _conv(d[9]) + "," + _conv(d[8]) + "," + _conv(d[9]) + "," + _conv(d[6]) + "," + _conv(d[7]) + " e";
+			}
+			else {
+				// loopback
+			}
 		};
 	};
 
