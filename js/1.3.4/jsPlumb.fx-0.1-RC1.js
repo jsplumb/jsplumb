908e755a00a57a425832680a072ec0d2fc0b9c1f
simon.porritt@gmail.com <simon.porritt@gmail.com@ebaf65f8-305e-11df-bfeb-7b4c664f286a>
2011-12-01 01:21:04 +0000

    documented the fx plugin a little, and switched up the way it works to not use prototype inheritance; it overrides the Connection class now.
    git-svn-id: https://jsplumb.googlecode.com/svn/trunk@1283 ebaf65f8-305e-11df-bfeb-7b4c664f286a

--- js/1.3.4/jsPlumb.fx-0.1-RC1.js
+++ js/1.3.4/jsPlumb.fx-0.1-RC1.js
@@ -4,6 +4,59 @@
 	this is an experiment right now; it's neither finished nor guaranteed to get finished, and it's not supported either.
 	
 	but all comments and suggestions are welcome.
+	
+	This plugin offers (read: will offer. this list is aspirational in parts) the following functionality:
+	
+	- Connection.animateOverlay  (done)
+	
+		Animates an overlay along the path inscribed by the connector.
+		
+	- Connection.stopOverlayAnimation(overlayId)   (done)
+	
+		Stops an animating Overlay.  You can use this if you set loop:true on an animateOverlay call.
+		
+	- Connection.cycleGradient
+	
+		Cycles through 1..N different gradients.  This can be used for a "pulse" type effect.
+		
+	- Animation parameters in Overlay definitions (done)
+	
+		A new member will be supported in Overlay definitions, in jsPlumb.Defaults.Overlays, Endpoint's 'connectorOverlays'
+		parameter, and Connection's 'overlays' parameter.  Something like this:
+		
+			jsPlumb.connect({
+				source:d1,
+				target:d2,
+				overlays:[
+					["Label", {
+						animate: {
+							loop:true,
+							start:0,
+							end:0.5,
+							step:0.2
+						}
+					}]
+				]
+			});
+			
+			this animation should work when dragging a new connection too (not done yet)
+			
+	- "sprout" a Connection when drawing it
+	
+		This will animate a connection travelling along its path from the source to the target.  It will be called
+		something like this:
+		
+			jsPlumb.connect({
+				source:d1,
+				target:d2,
+				sprout:true,
+				sproutDuration:400   (ms)
+			});
+			
+		I'll also add jsPlumb.Defaults.Sprout and jsPlumb.Defaults.SproutDuration.
+			
+		Not sure yet whether to support this on existing connections.  Is there any point?
+	
 */
 ;(function() {
 
@@ -14,7 +67,11 @@
 
 /* ------------------ animate an overlay on a connection ----------------------------------- */
 
-	var animateOverlay = function(params) {
+	var animationTimers = {},
+	getAnimationKey = function(conn, overlayId) {
+		return conn.getId() + "_" + overlayId;
+	},
+	animateOverlay = function(params) {
 		params = params || {};
 		
 		Animation.apply(this, arguments);
@@ -22,6 +79,7 @@
 		var start = params.start || 0,
 		finish = params.finish || 1,
 		step = params.step || 0.1,
+		tick = params.tick || 100,
 		overlay = params.connection.getOverlay(params.overlayId),
 		timerHandle = null,
 		self = this;
@@ -30,9 +88,7 @@
 		overlay.setLocation(start);
 		params.connection.repaint();
 		
-	    // set a timer that animates the location of the arrow (note that 'loc' was
-   	 	// not exposed for this purpose, really, but it's kind of handy! i'm going
-    	// to add getLocation/setLocation in the next version of jsPlumb)
+	    // set a timer that animates the location of the arrow 
     	
 		timerHandle = window.setInterval(function() {
     		overlay.incrementLocation(step);
@@ -45,24 +101,52 @@
 	    		}
     		}
     		params.connection.repaint();
-		}, 100);
+		}, tick);
+		
+		animationTimers[getAnimationKey(params.connection, params.overlayId)] = timerHandle;
 	};
 	
-	jsPlumb.getDefaultConnectionType().prototype.animateOverlay = function(params) {
-		var o = jsPlumb.extend({connection:this}, params);
-		animateOverlay(o);
+	var ConnectionWithAnimation = function(params) {
+		jsPlumb.getDefaultConnectionType().apply(this, arguments);
+		var self = this;
+		this.animateOverlay = function(params) {
+			var handle = animationTimers[getAnimationKey(self, params.overlayId)];
+			if (!handle) {
+				var o = jsPlumb.extend({connection:self}, params);
+				animateOverlay(o);
+			}
+		};
+		this.stopAnimatingOverlay = function(overlayId) {
+			var animKey = getAnimationKey(self, overlayId), handle = animationTimers[animKey];
+			if (handle) {
+				window.clearInterval(handle);
+				delete animationTimers[animKey];
+			}
+		};
+		
+		// now see if we need to do anything about overlays
+		if (params && params.overlays) {
+			for (var i = 0; i < params.overlays.length; i++) {
+				if (params.overlays[i].constructor == Array && params.overlays[i][1].animate) {
+					var overlay = self.overlays[i];
+					self.animateOverlay(jsPlumb.extend(params.overlays[i][1].animate, {overlayId:overlay.id}));				
+				}
+			}
+		}
+
 	};
+	jsPlumb.Defaults.ConnectionType = ConnectionWithAnimation;
 	
 /* ---------------------  cycle through gradients on a connection ----------------------- */
 
-	var gradientCycle = function(params) {
+	var cycleGradient = function(params) {
 		
 	
 	};
 
-	jsPlumb.getDefaultConnectionType().prototype.gradientCycle = function(params) {
+	jsPlumb.getDefaultConnectionType().prototype.cycleGradient = function(params) {
 		var o = jsPlumb.extend({connection:this}, params);
-		gradientCycle(o);
+		cycleGradient(o);
 	};
 	
 		
