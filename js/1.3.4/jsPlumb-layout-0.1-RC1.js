0873834c3f081aca6acda5255c78c25f0d71b2a0
simon.porritt@gmail.com <simon.porritt@gmail.com@ebaf65f8-305e-11df-bfeb-7b4c664f286a>
2011-12-02 00:47:41 +0000

    committed first working version of kamada lawai...i think
    git-svn-id: https://jsplumb.googlecode.com/svn/trunk@1295 ebaf65f8-305e-11df-bfeb-7b4c664f286a

--- js/1.3.4/jsPlumb-layout-0.1-RC1.js
+++ js/1.3.4/jsPlumb-layout-0.1-RC1.js
@@ -4,6 +4,26 @@
 		var self = this;
 		params = params || {};
 		self.animate = params.animate;
+		self.width = params.width;
+		self.height = params.height;
+		self.updateUIPosition = function(params) {
+			boundingBox = params.boundingBox || getComputedBoundingBox();
+			margins = params.margins || [0,0];				
+			var id = jsPlumb.getId(params.fromEl),
+				el = jsPlumb.CurrentLibrary.getElementObject(params.fromEl),
+				s = jsPlumb.CurrentLibrary.getSize(el), p = self.getPosition(id),
+				clampedX = ( (p[0] - boundingBox[0][0]) / boundingBox[2]) * (self.width - (2 * margins[0])),
+				clampedY = ( (p[1] - boundingBox[0][1]) / boundingBox[3]) * (self.height - (2 * margins[1])),
+				newPos = {
+					left:margins[0] + clampedX - (s[0] / 2), 
+					top:margins[1] + clampedY - (s[1] / 2)
+				};
+				
+			if (self.animate && !params.doNotAnimate) 
+				jsPlumb.animate(el, newPos);
+			else
+				jsPlumb.CurrentLibrary.setOffset(el, newPos);
+		};
 	};
 
 	// TODO turn these into functions.
@@ -25,17 +45,27 @@
 				iterations		-	optional.  number of times to iterate in the quest for the perfect layout.  defaults to 50.
 		*/
 		"Fruchterman-Reingold" : function(params) {
-			AbstractLayout.apply(this, arguments);
-			var self = this,
-				w = params.width || 500,
-				h = params.height || 500,
-				margins = params.margins || [0,0],
+			AbstractLayout.apply(this, arguments);				
+			var self = this;
+			// TODO pull up into AbstractLayout; make Kamada Kawai use this.
+			self.getPosition = function(id) {
+				var p = positions[id];
+				if (!p) {
+					p = [Math.floor(Math.random()*(self.width+1)), Math.floor(Math.random()*(self.height + 1))];
+					positions[id] = p;
+				}
+				return p;
+			};
+
+			//	w = params.width || 500,
+			//	h = params.height || 500,
+			var	margins = params.margins || [0,0],
 				nodeSelector = params.nodeSelector,
-				maxDimension = Math.max(w, h),
+				maxDimension = Math.max(self.width, self.height),
 				attractionMultiplier = params.attractionMultiplier || 1000,
 				repulsionMultiplier = params.repulsionMultiplier || 50000,
-				forceConstant = Math.sqrt(w * h / nodeSelector.length),
-				temperature = w / 10,
+				forceConstant = Math.sqrt(self.width * self.height / nodeSelector.length),
+				temperature = self.width / 10,
 				attractionConstant = attractionMultiplier * forceConstant,
 				repulsionConstant = repulsionMultiplier * forceConstant,
 				EPSILON = 0.000001,
@@ -49,15 +79,7 @@
 					}
 					return l;
 				},
-				positions = {},
-				getPosition = function(id) {
-					var p = positions[id];
-					if (!p) {
-						p = [Math.floor(Math.random()*(w+1)), Math.floor(Math.random()*(h+1))];
-						positions[id] = p;
-					}
-					return p;
-				},
+				positions = {},				
 				iterations = params.iterations || 50,
 				currentIteration = 0,
 				cool = function() { temperature *= (1.0 - currentIteration / iterations); },
@@ -65,6 +87,7 @@
 				done = function() {
 					return currentIteration > iterations;// || temperature < 1.0 /  maxDimension;
 				},
+				// TODO move to AbstractLayout; make KK use this.
 				getComputedBoundingBox = function() {
 					var xmin = ymin = Infinity, xmax = ymax = -Infinity;
 					for (var i in positions) {
@@ -75,23 +98,6 @@
 					}
 					return [ [ xmin, ymin ], [ xmax, ymax ], Math.abs(xmin - xmax), Math.abs(ymin - ymax) ];
 				},
-				updateUIPosition = function(params) {
-					boundingBox = params.boundingBox || getComputedBoundingBox();
-					margins = params.margins || [0,0];
-					var id = jsPlumb.getId(params.fromEl),
-						el = jsPlumb.CurrentLibrary.getElementObject(params.fromEl),
-						s = jsPlumb.CurrentLibrary.getSize(el), p = getPosition(id),
-						clampedX = ( (p[0] - boundingBox[0][0]) / boundingBox[2]) * (w - (2 * margins[0])),
-						clampedY = ( (p[1] - boundingBox[0][1]) / boundingBox[3]) * (h - (2 * margins[1])),
-						newPos = {
-							left:margins[0] + clampedX - (s[0] / 2), 
-							top:margins[1] + clampedY - (s[1] / 2)
-						};
-					if (self.animate && !params.doNotAnimate) 
-						jsPlumb.animate(el, newPos);
-					else
-						jsPlumb.CurrentLibrary.setOffset(el, newPos);
-				},
 				_step = function() {
 					currentIteration++;
 					// repulsion
@@ -100,7 +106,7 @@
 						v1[0] = 0; v1[1] = 0;
 						for (var j = 0; j < nodeSelector.length; j++) {
 							var n2 = nodeSelector[j], id2 = jsPlumb.getId(n2),
-								p1 = getPosition(id), p2 = getPosition(id2),
+								p1 = self.getPosition(id), p2 = self.getPosition(id2),
 								xd = p1[0] - p2[0],
 								yd = p1[1] - p2[1],
 								d = Math.max(EPSILON, Math.sqrt((xd * xd) + (yd * yd))),
@@ -112,7 +118,7 @@
 					// attraction
 					var conns = jsPlumb.getConnections();
 					for (var i = 0; i < conns.length; i++) {
-						var p1 = getPosition(conns[i].sourceId), p2 = getPosition(conns[i].targetId),
+						var p1 = self.getPosition(conns[i].sourceId), p2 = self.getPosition(conns[i].targetId),
 							v1 = getLocation(conns[i].sourceId), v2 = getLocation(conns[i].targetId),
 							xd = p1[0] - p2[0], yd = p1[1] - p2[1],
 							d = Math.max(EPSILON, Math.sqrt((xd * xd) + (yd * yd))),
@@ -125,7 +131,7 @@
 					// positions
 					for (var i = 0; i < nodeSelector.length; i++) {
 						var id = jsPlumb.getId(nodeSelector[i]),
-							v = getLocation(id), p = getPosition(id),
+							v = getLocation(id), p = self.getPosition(id),
 							d = Math.max(EPSILON, Math.sqrt((v[0] * v[0]) + (v[1] * v[1]))),
 							xd = v[0] / d * Math.min(d, temperature),
 							yd = v[1] / d * Math.min(d, temperature);
@@ -134,9 +140,9 @@
 						p[1] += yd;				
 
 						// we could paint here, if we wanted to do a step-animate type thing.
-						if (step) {
-							updateUIPosition({fromEl:nodeSelector[i], boundingBox:computedBoundingBox, margins:margins, doNotAnimate:true});
-						}
+						//if (step) {
+						//	updateUIPosition({fromEl:nodeSelector[i], boundingBox:computedBoundingBox, margins:margins, doNotAnimate:true});
+						//}
 					}
 				};
 					
@@ -144,17 +150,22 @@
 				while (!done()) {
 					_step();						
 					//if (step) sleep(1);
-				}
+				}		
+				
 				// position ui components
 				var computedBoundingBox = getComputedBoundingBox();
 				
-				//for (var i = 0; i < nodeSelector.length; i++) 
-					//updateUIPosition({fromEl:nodeSelector[i], boundingBox:computedBoundingBox, margins:margins});
+				for (var i = 0; i < nodeSelector.length; i++) 
+					self.updateUIPosition({fromEl:nodeSelector[i], boundingBox:computedBoundingBox, margins:margins});
+					
 				// repaint jsPlumb
 				jsPlumb.repaintEverything();						
 			},
 			
 			"Kamada-Kawai":function(params) {
+				var self = this;
+				AbstractLayout.apply(this, arguments);
+				
 				var iterations = params.iterations || 200,
 					w = params.width || 500,
 					h = params.height || 500,
@@ -163,17 +174,203 @@
 					exchangeVertices = (params.exchangeVertices != null) ? params.exchangeVertices : true,
 					lengthFactor = params.lengthFactor || 0.9,
 					nodeSelector = params.nodeSelector,
+					nodeCount = nodeSelector.length,
 					disconnectedMultiplier = params.disconnectedMultiplier || 0.5,
+					calcEnergy = function() {
+						var energy = 0;
+						for (var i = 0; i < nodeCount - 1; i++) {
+							for (var j = i + 1; j < nodeCount; j++) {
+                				var dist = distanceMap[i][j],
+									l_ij = L * dist,
+								 	k_ij = K / (dist * dist),
+									dx = xyData[i].left - xyData[j].left,
+									dy = xyData[i].top - xyData[j].top,
+									d = Math.sqrt(dx * dx + dy * dy);
+
+								energy += k_ij / 2 * (dx * dx + dy * dy + l_ij * l_ij - 2 * l_ij * d);
+							}
+						}
+//						console.log(energy);
+						return energy;
+					},
+					calcDeltaM = function(m) {
+				 		var dEdxm = 0, dEdym = 0;
+						for (var i = 0; i < nodeCount; i++) {
+							if (i != m) {
+				                var dist = distanceMap[m][i], 
+				                	l_mi = L * dist, 
+				                	k_mi = K / (dist * dist),
+									dx = xyData[m].left - xyData[i].left,
+				 					dy = xyData[m].top - xyData[i].top,
+									d = Math.sqrt(dx * dx + dy * dy),
+									common = k_mi * (1 - l_mi / d);
+									
+								dEdxm += common * dx;
+								dEdym += common * dy;
+							}
+						}
+						return Math.sqrt(dEdxm * dEdxm + dEdym * dEdym);
+					},
+					calcDeltaXY = function(m) {
+						var dE_dxm = 0, dE_dym = 0, d2E_d2xm = 0, d2E_dxmdym = 0, d2E_dymdxm = 0, d2E_d2ym = 0;
+
+						for (var i = 0; i < nodeCount; i++) {	
+							if (i != m) {
+                
+                				var dist = distanceMap[m][i],
+									l_mi = L * dist,
+									k_mi = K / (dist * dist),
+									dx = xyData[m].left - xyData[i].left,
+									dy = xyData[m].top - xyData[i].top,
+									d = Math.sqrt(dx * dx + dy * dy),
+									ddd = d * d * d;
+
+								dE_dxm += k_mi * (1 - l_mi / d) * dx;
+								dE_dym += k_mi * (1 - l_mi / d) * dy;
+								d2E_d2xm += k_mi * (1 - l_mi * dy * dy / ddd);
+								d2E_dxmdym += k_mi * l_mi * dx * dy / ddd;
+								d2E_d2ym += k_mi * (1 - l_mi * dx * dx / ddd);
+							}
+						}
+						// d2E_dymdxm equals to d2E_dxmdym.
+						d2E_dymdxm = d2E_dxmdym;
+
+						var denomi = d2E_d2xm * d2E_d2ym - d2E_dxmdym * d2E_dymdxm,
+							deltaX = (d2E_dxmdym * dE_dym - d2E_d2ym * dE_dxm) / denomi,
+							deltaY = (d2E_dymdxm * dE_dxm - d2E_d2xm * dE_dym) / denomi;
+						return [ deltaX, deltaY ];
+					},
+					calcEnergyIfExchanged = function(p, q) {
+						if (p >= q)
+							throw "p should be < q";
+						var energy = 0;		// < 0
+						for (var i = 0; i < nodeCount - 1; i++) {
+							for (var j = i + 1; j < nodeCount; j++) {
+								var ii = i, jj = j;
+								if (i == p) ii = q;
+								if (j == q) jj = p;
+
+				                var dist = distanceMap[i][j],
+									l_ij = L * dist,
+									k_ij = K / (dist * dist),
+									dx = xyData[ii].left - xyData[jj].left,
+									dy = xyData[ii].top - xyData[jj].top,
+									d = Math.sqrt(dx * dx + dy * dy);
+				
+								energy += k_ij / 2 * (dx * dx + dy * dy + l_ij * l_ij - 2 * l_ij * d);
+							}
+						}
+						return energy;
+					},
+					adjustForGravity = function() {						
+						var gx = 0, gy = 0;
+						for (var i = 0; i < xyData.length; i++) {
+							gx += xyData[i].left;
+							gy += xyData[i].top;
+						}
+						gx /= xyData.length;
+						gy /= xyData.length;
+						var diffx = w / 2 - gx, diffy = h / 2 - gy;
+						for (var i = 0; i < xyData.length; i++) {
+            				xyData[i] = { left:xyData[i].left + diffx, top:xyData[i].top + diffy };
+						}
+					},
+					// TODO move to AbstractLayout; change format from JS object to Array like Fruchterman uses.
+					getComputedBoundingBox = function() {
+						var xmin = ymin = Infinity, xmax = ymax = -Infinity;
+						for (var i=0; i < xyData.length; i++) {
+							xmin = Math.min(xmin, xyData[i].left);
+							xmax = Math.max(xmax, xyData[i].left);
+							ymin = Math.min(ymin, xyData[i].top);
+							ymax = Math.max(ymax, xyData[i].top);								
+						}
+						return [ [ xmin, ymin ], [ xmax, ymax ], Math.abs(xmin - xmax), Math.abs(ymin - ymax) ];
+					},
+					updateUIPosition = function(params) {
+						var boundingBox = params.boundingBox || getComputedBoundingBox(),
+							margins = params.margins || [0,0],				
+							id = jsPlumb.getId(params.fromEl),
+							el = jsPlumb.CurrentLibrary.getElementObject(params.fromEl),
+							s = jsPlumb.CurrentLibrary.getSize(el), p = xyData[params.idx],//p = self.getPosition(),
+							clampedX = ( (p.left - boundingBox[0][0]) / boundingBox[2]) * (self.width - (2 * margins[0])),
+							clampedY = ( (p.top - boundingBox[0][1]) / boundingBox[3]) * (self.height - (2 * margins[1])),
+							newPos = {
+								left:margins[0] + clampedX - (s[0] / 2), 
+								top:margins[1] + clampedY - (s[1] / 2)
+							};
+				
+						if (self.animate && !params.doNotAnimate) 
+							jsPlumb.animate(el, newPos);
+						else
+							jsPlumb.CurrentLibrary.setOffset(el, newPos);
+					},
+	
 					done = function() { return currentIteration > iterations; },
-					step = function() {
+					_step = function() {
 						currentIteration++;
-					};
+						if (nodeCount == 0) return;						
+						var energy = calcEnergy();
+//			status = "Kamada-Kawai V=" + getGraph().getVertexCount()
+//			+ "(" + getGraph().getVertexCount() + ")"
+//			+ " IT: " + currentIteration
+//			+ " E=" + energy
+//			;
+
+						var maxDeltaM = 0, 
+							pm = -1;            // the node having max deltaM
+						for (var i = 0; i < nodeCount; i++) {
+//				if (isLocked(vertices[i]))
+//					continue;
+							var deltam = calcDeltaM(i);
+
+							if (maxDeltaM < deltam) {
+								maxDeltaM = deltam;
+								pm = i;
+							}
+						}
+						if (pm == -1) return;
+						
+						for (var i = 0; i < 100; i++) {
+							var dxy = calcDeltaXY(pm);
+							xyData[pm] = { left:xyData[pm].left+dxy[0], top:xyData[pm].top+dxy[1] };
+
+							var deltam = calcDeltaM(pm);
+							if (deltam < EPSILON)
+								break;
+						}
+						
+						if (adjustForGravity)
+							adjustForGravity();
+							
+						if (exchangeVertices && maxDeltaM < EPSILON) {
+							energy = calcEnergy();
+							for (var i = 0; i < nodeCount - 1; i++) {
+								//if (isLocked(vertices[i]))
+									//continue;
+								for (var j = i + 1; j < nodeCount; j++) {
+									//if (isLocked(vertices[j]))
+										//continue;
+									var xenergy = calcEnergyIfExchanged(i, j);
+									if (energy > xenergy) {
+										var sx = xyData[i].left,
+											sy = xyData[i].top;
+										xyData[i] = xyData[j];
+										xyData[j] = { left:sx, top:sy };
+										return;
+									}
+								}
+							}
+						}
+					},
+					distanceMap = [],
+					xyData = [];
 					
 				// create a graph and populate it
 				var graph = new Graph();
 				// first, vertices
 				for (var i = 0; i < nodeSelector.length; i++) {
 					var vertex = graph.addVertex(jsPlumb.getId(nodeSelector[i]));
+					nodeSelector[i].vertex = vertex;
 				}
 				// next, edges. Graph should be fixed to honour the bidirectional parameter.
 				var conns = jsPlumb.getConnections();
@@ -184,28 +381,40 @@
 				// now get the graph's diameter
 				var diameter = graph.diameter(),
 					L0 = Math.min(w, h),
-    				L = (L0 / diameter) * lengthFactor;
-
+    				L = (L0 / diameter) * lengthFactor,
+    				K = 1, 
+    				EPSILON = 0.1;
 					
-				// assign IDs (we probably dont need to do that; we have div ids)
 				// assign initial distances etc
-				for (var i = 0; i < nodeSelector.length - 2; i++) {
-    				for (var j = i + 1; j < nodeSelector.length; j++) {
-	    	/*			var d_ij = distance.getDistance(vertices[i], vertices[j]),
-    						d_ji = distance.getDistance(vertices[j], vertices[i]),
+				for (var i = 0; i < nodeCount - 1; i++) {
+					xyData[i] = {left:Math.floor(Math.random()*(w+1)), top:Math.floor(Math.random()*(h+1))};//jsPlumb.getOffset(jsPlumb.getId(nodeSelector[i]));
+    				for (var j = i + 1; j < nodeCount; j++) {
+    					var d_ij = graph.getDistance(nodeSelector[i].vertex, nodeSelector[j].vertex),
+    						d_ji = graph.getDistance(nodeSelector[j].vertex, nodeSelector[i].vertex),
     						dist = diameter * disconnectedMultiplier;
     					if (d_ij != null)
     						dist = Math.min(d_ij, dist);
     					if (d_ji != null)
     						dist = Math.min(d_ji, dist);
-	    				dm[i][j] = dm[j][i] = dist;*/
+    					if (!distanceMap[i]) distanceMap[i] = [];
+    					if (!distanceMap[j]) distanceMap[j] = [];    					
+	    				distanceMap[i][j] = distanceMap[j][i] = dist;
     				}
     			}
+    			// the last entry's offset
+    			//xyData[nodeCount - 1] = jsPlumb.getOffset(jsPlumb.getId(nodeSelector[nodeCount - 1]));
+    			xyData[nodeCount - 1] = { left:Math.floor(Math.random()*(w+1)), top:Math.floor(Math.random()*(h+1)) };
 				
 				//
-				while(!done()) step();
+				while(!done()) _step();
 					
-				throw "kamada kawai is not implemented yet";
+//alert( "kamada kawai is not implemented yet");
+				var bb = getComputedBoundingBox();
+				for (var i = 0; i < nodeCount; i++)
+					//jsPlumb.CurrentLibrary.setOffset(nodeSelector[i], xyData[i]);
+					updateUIPosition({fromEl:nodeSelector[i],idx:i, boundingBox:bb });
+					
+				jsPlumb.repaintEverything();
 			},
 				
 			"Tree":function(params) {
@@ -290,6 +499,10 @@
 			source = _getVertex(source);
 			target = _getVertex(target);
 			return Djikstra.compute({graph:self, source:source, target:target, edgeSelector:edgeSelector, vertexSelector:vertexSelector});
+		};
+		this.getDistance = function(source, target, edgeSelector, vertexSelector) {
+			var info = self.findPath(source, target, edgeSelector, vertexSelector);
+			return info.pathDistance;
 		};
 		this.getVertex = _getVertex;		
 		this.printPath = function(source, target) {
