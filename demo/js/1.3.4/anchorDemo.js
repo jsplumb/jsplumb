017cda716a2673c2643c1211cbdc00ce3b838101
simon.porritt@gmail.com <simon.porritt@gmail.com@ebaf65f8-305e-11df-bfeb-7b4c664f286a>
2012-01-04 23:59:02 +0000

    reinstated endpoint/anchor defs in sources and targets demo, just so its more obvious what's going on
    git-svn-id: https://jsplumb.googlecode.com/svn/trunk@1429 ebaf65f8-305e-11df-bfeb-7b4c664f286a

--- demo/js/1.3.4/anchorDemo.js
+++ demo/js/1.3.4/anchorDemo.js
@@ -25,38 +25,36 @@
 				[ 1, 1, 0, 1 ]				
 			];
 
-            // set the default anchors for jsplumb.
+            // set default anchors.  the 'connect' calls below will pick these up, and in fact setting these means
+            // that you do not need to supply anchor definitions to the makeSource or makeTarget functions. but
+            // i left those parameters in just to show what you can do.
             jsPlumb.Defaults.Anchors = [ sourceAnchors, "TopCenter" ];
 
-            // make 'window1' a connection source.  note the commented out endpoint declaration - we don't need it
-            // because we've set defaults.  but i left it in to show what you'd do if just setting up defaults
-            // wasn't going to cut it for you.
+            // make 'window1' a connection source.
 			jsPlumb.makeSource("window1", {
-				/*endpoint:{
+				endpoint:{
 					anchor:sourceAnchors
-				}*/
+				}
 			});
 
             // get the list of ".smallWindow" elements.  getSelector is just a helper method that abstracts out the underlying
             // library; you don't need to use it - you could use $(".smallWindow") in jquery, $$(".smallWindow") in
             // MooTools, or Y.all(".smallWindow") in YUI.
             //
-            // notice again here i've commented the endpoint declaration out, because we have achieved the result we need
-            // through using the defaults. but you can supply an 'endpoint' object, which has exactly the same form
-            // as one you would pass to jsPlumb.makeSource or jsPlumb.addEndpoint.
 			var smallWindows = jsPlumb.getSelector(".smallWindow");
 			for (var i = 0; i < smallWindows.length; i++) {	
 				jsPlumb.draggable(smallWindows[i]);
 				jsPlumb.makeTarget(smallWindows[i], {
-					/*endpoint:{
+					endpoint:{
 						paintStyle: { fillStyle:'#558822' },
 						anchor:"TopCenter"
-					},*/
+					},
 					dropOptions:{ hoverClass:"hover" }
 				});
 			}
 
-            // and finally connect a couple of small windows, just so the demo doesnt look broken when it loads ;)
+            // and finally connect a couple of small windows, just so the demo doesnt look broken when it loads.
+            // note that
             jsPlumb.connect({ source:"window1", target:"window5" });
             jsPlumb.connect({ source:"window1", target:"window2" });
 		}
