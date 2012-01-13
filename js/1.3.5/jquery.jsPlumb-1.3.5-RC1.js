37e3a4f44d95f5d14c5abd24c990e351bb9dd38c
simon.porritt@gmail.com <simon.porritt@gmail.com@ebaf65f8-305e-11df-bfeb-7b4c664f286a>
2012-01-13 04:00:06 +0000

    first pass at a version of 'offset' for jquery that takes the parent's scale transform into account.  not plugged in, and only kind of successful so far.
    git-svn-id: https://jsplumb.googlecode.com/svn/trunk@1481 ebaf65f8-305e-11df-bfeb-7b4c664f286a

--- js/1.3.5/jquery.jsPlumb-1.3.5-RC1.js
+++ js/1.3.5/jquery.jsPlumb-1.3.5-RC1.js
@@ -55,7 +55,7 @@
 (function($) {	
 	
 	//var getBoundingClientRectSupported = "getBoundingClientRect" in document.documentElement;
-	
+
 	jsPlumb.CurrentLibrary = {					        
 		
 		/**
@@ -208,18 +208,19 @@
 			// in the wrong offset if the element has a margin (it doesn't take the margin into account).  the getBoundingClientRect
 			// method, which is in pretty much all browsers now, reports the right numbers.  but it introduces a noticeable lag, which
 			// i don't like.
-			
+            
 			/*if ( getBoundingClientRectSupported ) {
 				var r = eventArgs[1].helper[0].getBoundingClientRect();
 				return { left : r.left, top: r.top };
 			} else {*/
 			if (eventArgs.length == 1) {
-				return { left: eventArgs[0].pageX, top:eventArgs[0].pageY };
+				ret = { left: eventArgs[0].pageX, top:eventArgs[0].pageY };
 			}
 			else {
 				var ui = eventArgs[1], _offset = ui.offset;			
-				return _offset || ui.absolutePosition;
+				ret = _offset || ui.absolutePosition;
 			}
+            return ret;
 		},		
 		
 		hasClass : function(el, clazz) {
@@ -340,3 +341,67 @@
 	$(document).ready(jsPlumb.init);
 	
 })(jQuery);
+
+
+jQuery.fn.offsetzz = function( options ) {
+		var elem = this[0], box;
+
+		if ( options ) {
+			return this.each(function( i ) {
+				jQuery.offset.setOffset( this, options, i );
+			});
+		}
+
+		if ( !elem || !elem.ownerDocument ) {
+			return null;
+		}
+
+		if ( elem === elem.ownerDocument.body ) {
+			return jQuery.offset.bodyOffset( elem );
+		}
+
+		try {
+			box = elem.getBoundingClientRect();
+		} catch(e) {}
+
+		var doc = elem.ownerDocument,
+			docElem = doc.documentElement;
+
+		// Make sure we're not dealing with a disconnected DOM node
+		if ( !box || !jQuery.contains( docElem, elem ) ) {
+			return box ? { top: box.top, left: box.left } : { top: 0, left: 0 };
+		}
+
+		var body = doc.body,
+			win = window,
+			clientTop  = docElem.clientTop  || body.clientTop  || 0,
+			clientLeft = docElem.clientLeft || body.clientLeft || 0,
+			scrollTop  = win.pageYOffset || jQuery.support.boxModel && docElem.scrollTop  || body.scrollTop,
+			scrollLeft = win.pageXOffset || jQuery.support.boxModel && docElem.scrollLeft || body.scrollLeft,
+			top  = box.top  + scrollTop  - clientTop,
+			left = box.left + scrollLeft - clientLeft;
+
+        var r = /scale\(([0-9]*\.?[0-9]*)\)/,
+            offsetParent = (elem.tagName.toLowerCase() === "svg" && elem.parentNode) ? elem.parentNode : elem.offsetParent,
+            t = offsetParent ? offsetParent.style.transform ||
+                               offsetParent.style.MozTransform ||
+                               offsetParent.style.OTransform : null,
+            scale = (offsetParent && t) ? t.match(r)[1] : null;
+
+		if (scale) {
+            var pBox = offsetParent.getBoundingClientRect(),
+                dl = left - pBox.left,
+                dt = top - pBox.top,
+                dls = dl / scale,
+                dts = dt / scale;
+
+            console.log("PARENT SCALE IS " + scale);
+            //top = pBox.top + dt;
+            //left = pBox.left + dl;
+            top = pBox.top + dts;
+            left = pBox.left + dls;
+        }
+
+
+		return { top: top, left: left };
+	};
