2dd654320b6c6537cc1efc0d5101630cde3a93f3
simon.porritt@gmail.com <simon.porritt@gmail.com@ebaf65f8-305e-11df-bfeb-7b4c664f286a>
2012-03-03 23:11:27 +0000

    updating for 1.3.7 release
    git-svn-id: https://jsplumb.googlecode.com/svn/trunk@1668 ebaf65f8-305e-11df-bfeb-7b4c664f286a

---
+++ build/1.3.7/demo/js/archive/1.3.3/demo-helper-yui3.js
@@ -0,0 +1,37 @@
+/*
+ *  This file contains the JS that handles the first init of each YUI3 demonstration, and also switching
+ *  between render modes.
+ */
+YUI().use("node", function(Y) {
+	
+	jsPlumb.bind("ready", function() {
+
+		// chrome fix.
+		document.onselectstart = function () { return false; };
+
+	     // render mode
+		var resetRenderMode = function(desiredMode) {
+			var newMode = jsPlumb.setRenderMode(desiredMode);
+			Y.all(".rmode").each(function(e) { e.removeClass("selected"); });
+			Y.all(".rmode[mode='" + newMode + "']").each(function(e) { e.addClass("selected"); });
+			var disableList = (newMode === jsPlumb.VML) ? ".rmode[mode='canvas'],.rmode[mode='svg']" : ".rmode[mode='vml']"; 
+			Y.all(disableList).each(function(e) { 
+				e.setAttribute("disabled", true) 
+			});				
+			
+			jsPlumbDemo.init();
+		};
+	     
+		Y.all(".rmode").each(function(r) {
+			r.on("click", function() {
+				var desiredMode = r.getAttribute("mode");
+				if (jsPlumbDemo.reset) jsPlumbDemo.reset();
+				jsPlumb.reset();
+				resetRenderMode(desiredMode);					
+			});
+		});
+	
+		resetRenderMode(jsPlumb.CANVAS);
+       
+	});
+});
---
+++ build/1.3.7/demo/js/demo-helper-jquery.js
@@ -0,0 +1,34 @@
+/*
+ *  This file contains the JS that handles the first init of each jQuery demonstration, and also switching
+ *  between render modes.
+ */
+jsPlumb.bind("ready", function() {
+
+	jsPlumb.DemoList.init();
+
+	// chrome fix.
+	document.onselectstart = function () { return false; };				
+
+    // render mode
+	var resetRenderMode = function(desiredMode) {
+		var newMode = jsPlumb.setRenderMode(desiredMode);
+		$(".rmode").removeClass("selected");
+		$(".rmode[mode='" + newMode + "']").addClass("selected");
+		var disableList = (newMode === jsPlumb.VML) ? ".rmode[mode='canvas'],.rmode[mode='svg']" : ".rmode[mode='vml']"; 
+		$(disableList).attr("disabled", true);				
+		jsPlumbDemo.init();
+	};
+     
+	$(".rmode").bind("click", function() {
+		var desiredMode = $(this).attr("mode");
+		if (jsPlumbDemo.reset) jsPlumbDemo.reset();
+		jsPlumb.reset();
+		resetRenderMode(desiredMode);					
+	});
+	
+	// explanation div is draggable
+	$("#explanation,.renderMode").draggable();
+
+	resetRenderMode(jsPlumb.SVG);
+       
+});
