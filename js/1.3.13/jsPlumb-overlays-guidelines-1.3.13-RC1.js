13bf91cb161fa5dae9ecd30174cac46802ecb0b5
sporritt <simon.porritt@gmail.com>
2012-08-16 03:12:58 +1000

    copied main guideline overlay routine into guideline js file (doesnt get packaged into released jsplumb)

--- js/1.3.13/jsPlumb-overlays-guidelines-1.3.13-RC1.js
+++ js/1.3.13/jsPlumb-overlays-guidelines-1.3.13-RC1.js
@@ -1,3 +1,29 @@
+    // this is really just a test overlay, so its undocumented and doesnt take any parameters. but i was loth to delete it.
+    jsPlumb.Overlays.GuideLines = function() {
+        var self = this;
+        self.length = 50;
+        self.lineWidth = 5;
+        this.type = "GuideLines";
+		AbstractOverlay.apply(this, arguments);
+        jsPlumb.jsPlumbUIComponent.apply(this, arguments);
+        this.draw = function(connector, currentConnectionPaintStyle, connectorDimensions) {
+
+            var head = connector.pointAlongPathFrom(self.loc, self.length / 2),
+                mid = connector.pointOnPath(self.loc),
+                tail = jsPlumbUtil.pointOnLine(head, mid, self.length),
+                tailLine = jsPlumbUtil.perpendicularLineTo(head, tail, 40),
+                headLine = jsPlumbUtil.perpendicularLineTo(tail, head, 20);
+
+            self.paint(connector, [head, tail, tailLine, headLine], self.lineWidth, "red", null, connectorDimensions);
+
+            return [Math.min(head.x, tail.x), Math.min(head.y, tail.y), Math.max(head.x, tail.x), Math.max(head.y,tail.y)];
+        };
+
+        this.computeMaxSize = function() { return 50; };
+
+    	this.cleanup = function() { };  // nothing to clean up for GuideLines
+    };
+
 // a test
     jsPlumb.Overlays.svg.GuideLines = function() {
         var path = null, self = this, path2 = null, p1_1, p1_2;
