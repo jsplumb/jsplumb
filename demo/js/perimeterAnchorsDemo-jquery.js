1eb790d74d245b8c7a3d97a4db0a6a8000156fe2
sporritt <simon.porritt@gmail.com>
2012-08-28 16:19:28 +1000

    perimeter anchors demo files

---
+++ demo/js/perimeterAnchorsDemo-jquery.js
@@ -0,0 +1,39 @@
+;(function() {
+	
+	window.jsPlumbDemo = {
+			
+		init : function() {
+			
+			jsPlumb.importDefaults({
+				Connector:"StateMachine",
+				PaintStyle:{ lineWidth:3, strokeStyle:"#ffa500", "dashstyle":"2 4" },
+				Endpoint:[ "Dot", { radius:5 } ],
+				EndpointStyle:{ fillStyle:"#ffa500" }
+			});
+			  
+			// NOTE here we are just using getSelector so we don't have to rewrite the code for each of the supported libraries.
+			// you can just use the approriate selector from the library you're using, if you want to. like $(".shape) on jquery, for example.
+			var shapes = jsPlumb.getSelector(".shape");
+				
+			// make everything draggable
+			jsPlumb.draggable(shapes);
+				
+			// loop through them and connect each one to each other one.
+			for (var i = 0; i < shapes.length; i++) {
+				for (var j = i + 1; j < shapes.length; j++) {						
+					jsPlumb.connect({
+						source:shapes[i],  // just pass in the current node in the selector for source 
+						target:shapes[j],
+						// here we supply a different anchor for source and for target, and we get the element's "data-shape"
+						// attribute to tell us what shape we should use.
+						anchors:[
+							[ "Perimeter", { shape:$(shapes[i]).attr("data-shape") }],
+							[ "Perimeter", { shape:$(shapes[j]).attr( "data-shape") }]
+						]
+					});				
+				}	
+			}
+    }    
+  }
+  
+})();
