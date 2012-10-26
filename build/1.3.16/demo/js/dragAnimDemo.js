31f07cea888abfb67af925bbeca4069c5d341799
sporritt <simon.porritt@gmail.com>
2012-10-26 21:43:36 +1100

    add 1.3.16 build dir

---
+++ build/1.3.16/demo/js/dragAnimDemo.js
@@ -0,0 +1,80 @@
+;(function() {
+	
+	// notice there are no dragOptions specified here, which is different from the
+	// draggableConnectors2 demo.  all connections on this page are therefore
+	// implicitly in the default scope.
+	var endpoint = {
+			anchor:[0.5,0.5,0,-1],
+			connectorStyle:{ lineWidth:7,strokeStyle:"rgba(198,89,30,0.7)" },
+			endpointsOnTop:true,
+			isSource:true,
+			maxConnections:10,
+			isTarget:true,
+			dropOptions:{ tolerance:"touch",hoverClass:"dropHover" }
+	},
+	
+	prepare = function(elId) {
+		jsPlumbDemo.initHover(elId);
+		jsPlumbDemo.initAnimation(elId);
+		
+		return jsPlumb.addEndpoint(elId, endpoint);
+	},
+	
+	discs = [];
+	
+
+	window.jsPlumbDemo = {
+		init : function() {
+
+			jsPlumb.importDefaults({
+				DragOptions : { cursor: 'wait', zIndex:20 },
+				Endpoint : [ "Image", { url:"../img/littledot.png" } ],
+				Connector : [ "Bezier", { curviness: 90 } ]
+			});				
+				
+			var e1 = prepare("bd1"),
+				e2 = prepare("bd2"),
+				e3 = prepare("bd3"),
+				e4 = prepare("bd4");
+	
+			jsPlumbDemo.initClearButton();
+	
+			jsPlumb.connect({ source:e1, target:e2 });
+			jsPlumb.connect({ source:e1, target:e3 });
+			jsPlumb.connect({ source:e1, target:e4 });
+	
+			jsPlumbDemo.initAddButton();
+		},
+		
+		// this is overridden by the YUI demo.
+		createDisc : function() {
+			var d = document.createElement("div");
+			d.className = "bigdot";
+			document.getElementById("demo").appendChild(d);
+			var id = '' + ((new Date().getTime())), _d = jsPlumb.CurrentLibrary.getElementObject(d);
+			jsPlumb.CurrentLibrary.setAttribute(_d, "id", id);
+			var w = screen.width - 162, h = screen.height - 162;
+			var x = (0.2 * w) + Math.floor(Math.random()*(0.5 * w));
+			var y = (0.2 * h) + Math.floor(Math.random()*(0.6 * h));
+			d.style.top= y + 'px';
+			d.style.left= x + 'px';
+			return {d:d, id:id};
+		},
+		
+		addDisc : function() {
+			var info = jsPlumbDemo.createDisc();
+			var e = prepare(info.id);	
+			jsPlumb.draggable(info.id);
+			discs.push(info.id);
+		},
+		
+		reset : function() {
+			for (var i = 0; i < discs.length; i++) {
+				var d = document.getElementById(discs[i]);
+				if (d) d.parentNode.removeChild(d);
+			}
+			discs = [];
+		}
+	};
+	
+})();
---
+++ build/1.3.16/demo/js/dynamicAnchorsDemo.js
@@ -0,0 +1,99 @@
+;(function() {
+	
+	window.jsPlumbDemo = {
+		init : function() {
+
+			var sourceAnchors = [[0.2, 0, 0, -1], [1, 0.2, 1, 0], [0.8, 1, 0, 1], [0, 0.8, -1, 0] ],
+				targetAnchors = [[0.6, 0, 0, -1], [1, 0.6, 1, 0], [0.4, 1, 0, 1], [0, 0.4, -1, 0] ],
+			exampleColor = '#00f',
+			exampleDropOptions = {
+					tolerance:'touch',
+					hoverClass:'dropHover',
+					activeClass:'dragActive'
+			}, 
+			connector = [ "Bezier", { cssClass:"connectorClass", hoverClass:"connectorHoverClass" } ],
+			connectorStyle = {
+				gradient:{stops:[[0, exampleColor], [0.5, '#09098e'], [1, exampleColor]]},
+				lineWidth:5,
+				strokeStyle:exampleColor
+			},
+			hoverStyle = {
+				strokeStyle:"#449999"
+			},
+			overlays = [ ["Diamond", { fillStyle:"#09098e", width:15, length:15 } ] ],
+			endpoint = ["Dot", { cssClass:"endpointClass", radius:10, hoverClass:"endpointHoverClass" } ],
+			endpointStyle = { fillStyle:exampleColor },
+			anEndpoint = {
+				endpoint:endpoint,
+				paintStyle:endpointStyle,
+				hoverPaintStyle:{ fillStyle:"#449999" },
+				isSource:true, 
+				isTarget:true, 
+				maxConnections:-1, 
+				connector:connector,
+				connectorStyle:connectorStyle,
+				connectorHoverStyle:hoverStyle,
+				connectorOverlays:overlays
+			};
+			
+			jsPlumb.Defaults.DragOptions = { cursor: 'pointer', zIndex:2000 };
+		
+			var connections = {
+				"window1":["window4"],
+				"window3":["window1"],
+				"window5":["window3"],
+				"window6":["window5"],
+				"window2":["window6"],
+				"window4":["window2"]
+				
+			},
+			endpoints = {},			
+			// ask jsPlumb for a selector for the window class
+			divsWithWindowClass = jsPlumb.CurrentLibrary.getSelector(".window");
+			
+			// add endpoints to all of these - one for source, and one for target, configured so they don't sit
+			// on top of each other.
+			for (var i = 0 ; i < divsWithWindowClass.length; i++) {
+				var id = jsPlumb.getId(divsWithWindowClass[i]);
+				endpoints[id] = [
+					// note the three-arg version of addEndpoint; lets you re-use some common settings easily.
+					jsPlumb.addEndpoint(id, anEndpoint, {anchor:sourceAnchors}),
+					jsPlumb.addEndpoint(id, anEndpoint, {anchor:targetAnchors})
+				];
+			}
+			// then connect everything using the connections map declared above.
+			for (var e in endpoints) {
+				if (connections[e]) {
+					for (var j = 0; j < connections[e].length; j++) {					
+						jsPlumb.connect({
+							source:endpoints[e][0],
+							target:endpoints[connections[e][j]][1]
+						});						
+					}
+				}	
+			}
+			
+			// bind click listener; delete connections on click			
+			jsPlumb.bind("click", function(conn) {
+				jsPlumb.detach(conn);
+			});
+			
+			// bind beforeDetach interceptor: will be fired when the click handler above calls detach, and the user
+			// will be prompted to confirm deletion.
+			jsPlumb.bind("beforeDetach", function(conn) {
+				return confirm("Delete connection?");
+			});
+			
+			//
+			// configure ".window" to be draggable. 'getSelector' is a jsPlumb convenience method that allows you to
+			// write library-agnostic selectors; you could use your library's selector instead, eg.
+			//
+			// $(".window")  		jquery
+			// $$(".window") 		mootools
+			// Y.all(".window")		yui3
+			//
+			jsPlumb.draggable(jsPlumb.getSelector(".window"));
+		}
+	};
+	
+})();
