10c1fe7dcdb26231454fb8203f25fe5aa9ae18fa
sporritt <simon.porritt@gmail.com>
2012-12-17 07:18:45 +1100

    issue 300 - typo in unmakeSource

--- js/1.4.0/jsPlumb-1.4.0-RC1.js
+++ js/1.4.0/jsPlumb-1.4.0-RC1.js
@@ -2982,7 +2982,7 @@ between this method and jsPlumb.reset).
 				mouseDownListener = _sourceTriggers[id];
 			
 			if (mouseDownListener) 
-				_currentInstance.unregisterListener(_el, "mousedown", mouseDownListener);
+				_currentInstance.unregisterListener(el, "mousedown", mouseDownListener);
 
 			if (!doNotClearArrays) {
 				delete _sourceEndpointDefinitions[id];
