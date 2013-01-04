663c67706ba3b82213ab5807f04f3a82e7b566f7
sporritt <simon.porritt@gmail.com>
2013-01-04 18:32:15 +1100

    svg arc renderer derives large angle flag from sweep of the underlying arc now

--- js/1.4.0/jsPlumb-renderers-svg-1.4.0-RC1.js
+++ js/1.4.0/jsPlumb-renderers-svg-1.4.0-RC1.js
@@ -360,7 +360,7 @@
 					},
 					"Arc":function(segment) {
 						var d = segment.params,
-							laf = 0,
+							laf = segment.sweep > Math.PI ? 1 : 0,
 							sf = segment.anticlockwise ? 0 : 1;
 							
 						return "M" + segment.x1 + " " + segment.y1 + " A " + segment.radius + " " + d.r + " 0 " + laf + "," + sf + " " + segment.x2 + " " + segment.y2;
