679f38bf9b7402cb7a0b06ad7d42af0eae149d89
sporritt <simon.porritt@gmail.com>
2013-01-25 13:14:47 +1100

    - support for user defined location in dynamic and continuous anchors - moved anchor definitions out into their own file

--- js/1.4.0/jsPlumb-1.4.0-RC1.js
+++ js/1.4.0/jsPlumb-1.4.0-RC1.js
@@ -3722,6 +3722,10 @@ between this method and jsPlumb.reset).
 
 			this.getCurrentLocation = function() { return lastReturnValue; };
             
+            this.getUserDefinedLocation = function() { 
+                return userDefinedLocation;
+            };
+            
             this.setUserDefinedLocation = function(l) {
                 userDefinedLocation = l;
             };
@@ -3738,6 +3742,8 @@ between this method and jsPlumb.reset).
 		 * TODO FloatingAnchor could totally be refactored to extend Anchor just slightly.
 		 */
 		var FloatingAnchor = function(params) {
+            
+            Anchor.apply(this, arguments);
 
 			// this is the anchor that this floating anchor is referenced to for
 			// purposes of calculating the orientation.
@@ -3816,6 +3822,8 @@ between this method and jsPlumb.reset).
 		 * 
 		 */
 		var DynamicAnchor = function(anchors, anchorSelector, elementId) {
+            Anchor.apply(this, arguments);
+            
 			this.isSelective = true;
 			this.isDynamic = true;			
 			var _anchors = [], self = this,
@@ -3861,6 +3869,15 @@ between this method and jsPlumb.reset).
 			
 			this.compute = function(params) {				
 				var xy = params.xy, wh = params.wh, timestamp = params.timestamp, txy = params.txy, twh = params.twh;				
+                
+                if(params.clearUserDefinedLocation)
+                    userDefinedLocation = null;
+                
+                var udl = self.getUserDefinedLocation();
+                if (udl != null) {
+                    return udl;
+                }
+                
 				// if anchor is locked or an opposite element was not given, we
 				// maintain our state. anchor will be locked
 				// if it is the source of a drag and drop.
@@ -3877,7 +3894,7 @@ between this method and jsPlumb.reset).
 			};
 
 			this.getCurrentLocation = function() {
-				return _curAnchor != null ? _curAnchor.getCurrentLocation() : null;
+				return self.getUserDefinedLocation() || (_curAnchor != null ? _curAnchor.getCurrentLocation() : null);
 			};
 
 			this.getOrientation = function(_endpoint) { return _curAnchor != null ? _curAnchor.getOrientation(_endpoint) : [ 0, 0 ]; };
@@ -3896,6 +3913,7 @@ between this method and jsPlumb.reset).
 	// be called as few times as possible.
 	var continuousAnchors = {},
         continuousAnchorLocations = {},
+        userDefinedContinuousAnchorLocations = {},        
 	    continuousAnchorOrientations = {},
 	    Orientation = { HORIZONTAL : "horizontal", VERTICAL : "vertical", DIAGONAL : "diagonal", IDENTITY:"identity" },
     
@@ -4040,7 +4058,6 @@ between this method and jsPlumb.reset).
 				ep = conn.endpoints,
                 doRegisterTarget = true,
 			    registerConnection = function(otherIndex, otherEndpoint, otherAnchor, elId, c) {
-
 					if ((sourceId == targetId) && otherAnchor.isContinuous){
                        // remove the target endpoint's canvas.  we dont need it.
                         jsPlumb.CurrentLibrary.removeElement(ep[1].canvas);
@@ -4054,8 +4071,8 @@ between this method and jsPlumb.reset).
             	registerConnection(1, ep[1], ep[1].anchor, sourceId, conn);
 		};
 		this.connectionDetached = function(connInfo) {
-			var connection = connInfo.connection || connInfo;
-			var sourceId = connection.sourceId,
+            var connection = connInfo.connection || connInfo,
+			    sourceId = connection.sourceId,
                 targetId = connection.targetId,
 				ep = connection.endpoints,
 				removeConnection = function(otherIndex, otherEndpoint, otherAnchor, elId, c) {
@@ -4320,16 +4337,22 @@ between this method and jsPlumb.reset).
 				existing = {
 					type:"Continuous",
 					compute : function(params) {
-						return continuousAnchorLocations[params.element.id] || [0,0];
+						return userDefinedContinuousAnchorLocations[params.element.id] || continuousAnchorLocations[params.element.id] || [0,0];
 					},
 					getCurrentLocation : function(endpoint) {
-						return continuousAnchorLocations[endpoint.id] || [0,0];
+						return userDefinedContinuousAnchorLocations[endpoint.id] || continuousAnchorLocations[endpoint.id] || [0,0];
 					},
 					getOrientation : function(endpoint) {
 						return continuousAnchorOrientations[endpoint.id] || [0,0];
 					},
 					isDynamic : true,
-					isContinuous : true
+					isContinuous : true,
+                    clearUserDefinedLocation:function() { 
+                        delete userDefinedContinuousAnchorLocations[params.elementId]; 
+                    },
+                    setUserDefinedLocation:function(loc) { 
+                        userDefinedContinuousAnchorLocations[params.elementId] = loc; 
+                    }
 				};
 				continuousAnchors[params.elementId] = existing;
 			}
@@ -4473,6 +4496,7 @@ between this method and jsPlumb.reset).
 				if (t.detachable != null) self.setDetachable(t.detachable);
 				if (t.reattach != null) self.setReattach(t.reattach);
 				if (t.scope) self.scope = t.scope;
+                editable = t.editable;
 				self.setConnector(t.connector, doNotRepaint);
 			};			
 // END TYPE
@@ -6369,227 +6393,4 @@ between this method and jsPlumb.reset).
 	
 // --------------------- end static instance + AMD registration -------------------------------------------		
 	
-// --------------------- anchors (would like to move these out of here) -------------------------------------------		
-	
-	var _curryAnchor = function(x, y, ox, oy, type, fnInit) {
-		return function(params) {
-			var a = params.jsPlumbInstance.makeAnchor([ x, y, ox, oy, 0, 0 ], params.elementId, params.jsPlumbInstance);
-			a.type = type;
-			if (fnInit) fnInit(a, params);
-			return a;
-		};
-	};
-	/*
-	 * Property: Anchors.TopCenter
-	 * An Anchor that is located at the top center of the element.
-	 */
-	jsPlumb.Anchors["TopCenter"] 		= _curryAnchor(0.5, 0, 0,-1, "TopCenter");
-	/*
-	 * Property: Anchors.BottomCenter
-	 * An Anchor that is located at the bottom center of the element.
-	 */
-	jsPlumb.Anchors["BottomCenter"] 	= _curryAnchor(0.5, 1, 0, 1, "BottomCenter");
-	/*
-	 * Property: Anchors.LeftMiddle
-	 * An Anchor that is located at the left middle of the element.
-	 */
-	jsPlumb.Anchors["LeftMiddle"] 		= _curryAnchor(0, 0.5, -1, 0, "LeftMiddle");
-	/*
-	 * Property: Anchors.RightMiddle
-	 * An Anchor that is located at the right middle of the element.
-	 */
-	jsPlumb.Anchors["RightMiddle"] 		= _curryAnchor(1, 0.5, 1, 0, "RightMiddle");
-	/*
-	 * Property: Anchors.Center
-	 * An Anchor that is located at the center of the element.
-	 */
-	jsPlumb.Anchors["Center"] 			= _curryAnchor(0.5, 0.5, 0, 0, "Center");
-	/*
-	 * Property: Anchors.TopRight
-	 * An Anchor that is located at the top right corner of the element.
-	 */
-	jsPlumb.Anchors["TopRight"] 		= _curryAnchor(1, 0, 0,-1, "TopRight");
-	/*
-	 * Property: Anchors.BottomRight
-	 * An Anchor that is located at the bottom right corner of the element.
-	 */
-	jsPlumb.Anchors["BottomRight"] 		= _curryAnchor(1, 1, 0, 1, "BottomRight");
-	/*
-	 * Property: Anchors.TopLeft
-	 * An Anchor that is located at the top left corner of the element.
-	 */
-	jsPlumb.Anchors["TopLeft"] 			= _curryAnchor(0, 0, 0, -1, "TopLeft");
-	/*
-	 * Property: Anchors.BottomLeft
-	 * An Anchor that is located at the bototm left corner of the element.
-	 */
-	jsPlumb.Anchors["BottomLeft"] 		= _curryAnchor(0, 1, 0, 1, "BottomLeft");
-			
-	jsPlumb.Defaults.DynamicAnchors = function(params) {
-		return params.jsPlumbInstance.makeAnchors(["TopCenter", "RightMiddle", "BottomCenter", "LeftMiddle"], params.elementId, params.jsPlumbInstance);
-	};
-	/*
-	 * Property: Anchors.AutoDefault
-	 * Default DynamicAnchors - chooses from TopCenter, RightMiddle, BottomCenter, LeftMiddle.
-	 */
-	jsPlumb.Anchors["AutoDefault"]  = function(params) { 
-		var a = params.jsPlumbInstance.makeDynamicAnchor(jsPlumb.Defaults.DynamicAnchors(params));
-		a.type = "AutoDefault";
-		return a;
-	};
-	
-	/*
-	 * Property: Anchors.Assign
-	 * An Anchor whose location is assigned at connection time, through an AnchorPositionFinder. Used in conjunction
-	 * with the 'makeTarget' function. jsPlumb has two of these - 'Fixed' and 'Grid', and you can also write your own.
-	 */
-	jsPlumb.Anchors["Assign"] = _curryAnchor(0,0,0,0,"Assign", function(anchor, params) {
-		// find what to use as the "position finder". the user may have supplied a String which represents
-		// the id of a position finder in jsPlumb.AnchorPositionFinders, or the user may have supplied the
-		// position finder as a function.  we find out what to use and then set it on the anchor.
-		var pf = params.position || "Fixed";
-		anchor.positionFinder = pf.constructor == String ? params.jsPlumbInstance.AnchorPositionFinders[pf] : pf;
-		// always set the constructor params; the position finder might need them later (the Grid one does,
-		// for example)
-		anchor.constructorParams = params;
-	});
-
-	// Continuous anchor is just curried through to the 'get' method of the continuous anchor
-	// factory.
-	/*
-	 * Property: Anchors.Continuous
-	 * An Anchor that tracks the other element in the connection, choosing the closest face.
-	 */
-	jsPlumb.Anchors["Continuous"] = function(params) {
-		return params.jsPlumbInstance.continuousAnchorFactory.get(params);
-	};
-
-    // these are the default anchor positions finders, which are used by the makeTarget function.  supplying
-    // a position finder argument to that function allows you to specify where the resulting anchor will
-    // be located
-	jsPlumb.AnchorPositionFinders = {
-		"Fixed": function(dp, ep, es, params) {
-			return [ (dp.left - ep.left) / es[0], (dp.top - ep.top) / es[1] ];	
-		},
-		"Grid":function(dp, ep, es, params) {
-			var dx = dp.left - ep.left, dy = dp.top - ep.top,
-				gx = es[0] / (params.grid[0]), gy = es[1] / (params.grid[1]),
-				mx = Math.floor(dx / gx), my = Math.floor(dy / gy);
-			return [ ((mx * gx) + (gx / 2)) / es[0], ((my * gy) + (gy / 2)) / es[1] ];
-		}
-	};
-	
-	/*
-	 * Property: Anchors.Perimeter
-	 * An Anchor that tracks the perimeter of some shape, approximating it with a given number of dynamically
-	 * positioned locations.
-	 *
-	 * Parameters:
-	 *
-	 * anchorCount  -   optional number of anchors to use to approximate the perimeter. default is 60.
-	 * shape        -   required. the name of the shape. valid values are 'rectangle', 'square', 'ellipse', 'circle', 'triangle' and 'diamond'
-	 * rotation     -   optional rotation, in degrees, to apply. 
-	 */
-	jsPlumb.Anchors["Perimeter"] = function(params) {
-		params = params || {};
-		var anchorCount = params.anchorCount || 60,
-			shape = params.shape;
-		
-		if (!shape) throw new Error("no shape supplied to Perimeter Anchor type");		
-		
-		var _circle = function() {
-                var r = 0.5, step = Math.PI * 2 / anchorCount, current = 0, a = [];
-                for (var i = 0; i < anchorCount; i++) {
-                    var x = r + (r * Math.sin(current)),
-                        y = r + (r * Math.cos(current));                                
-                    a.push( [ x, y, 0, 0 ] );
-                    current += step;
-                }
-                return a;	
-            },
-            _path = function(segments) {
-                var anchorsPerFace = anchorCount / segments.length, a = [],
-                    _computeFace = function(x1, y1, x2, y2, fractionalLength) {
-                        anchorsPerFace = anchorCount * fractionalLength;
-                        var dx = (x2 - x1) / anchorsPerFace, dy = (y2 - y1) / anchorsPerFace;
-                        for (var i = 0; i < anchorsPerFace; i++) {
-                            a.push( [
-                                x1 + (dx * i),
-                                y1 + (dy * i),
-                                0,
-                                0
-                            ]);
-                        }
-                    };
-								
-                for (var i = 0; i < segments.length; i++)
-                    _computeFace.apply(null, segments[i]);
-														
-                return a;					
-            },
-			_shape = function(faces) {												
-                var s = [];
-                for (var i = 0; i < faces.length; i++) {
-                    s.push([faces[i][0], faces[i][1], faces[i][2], faces[i][3], 1 / faces.length]);
-                }
-                return _path(s);
-			},
-			_rectangle = function() {
-				return _shape([
-					[ 0, 0, 1, 0 ], [ 1, 0, 1, 1 ], [ 1, 1, 0, 1 ], [ 0, 1, 0, 0 ]
-				]);		
-			};
-		
-		var _shapes = {
-			"Circle":_circle,
-			"Ellipse":_circle,
-			"Diamond":function() {
-				return _shape([
-						[ 0.5, 0, 1, 0.5 ], [ 1, 0.5, 0.5, 1 ], [ 0.5, 1, 0, 0.5 ], [ 0, 0.5, 0.5, 0 ]
-				]);
-			},
-			"Rectangle":_rectangle,
-			"Square":_rectangle,
-			"Triangle":function() {
-				return _shape([
-						[ 0.5, 0, 1, 1 ], [ 1, 1, 0, 1 ], [ 0, 1, 0.5, 0]
-				]);	
-			},
-			"Path":function(params) {
-                var points = params.points, p = [], tl = 0;
-				for (var i = 0; i < points.length - 1; i++) {
-                    var l = Math.sqrt(Math.pow(points[i][2] - points[i][0]) + Math.pow(points[i][3] - points[i][1]));
-                    tl += l;
-					p.push([points[i][0], points[i][1], points[i+1][0], points[i+1][1], l]);						
-				}
-                for (var i = 0; i < p.length; i++) {
-                    p[i][4] = p[i][4] / tl;
-                }
-				return _path(p);
-			}
-		},
-        _rotate = function(points, amountInDegrees) {
-            var o = [], theta = amountInDegrees / 180 * Math.PI ;
-            for (var i = 0; i < points.length; i++) {
-                var _x = points[i][0] - 0.5,
-                    _y = points[i][1] - 0.5;
-                    
-                o.push([
-                    0.5 + ((_x * Math.cos(theta)) - (_y * Math.sin(theta))),
-                    0.5 + ((_x * Math.sin(theta)) + (_y * Math.cos(theta))),
-                    points[i][2],
-                    points[i][3]
-                ]);
-            }
-            return o;
-        };
-		
-		if (!_shapes[shape]) throw new Error("Shape [" + shape + "] is unknown by Perimeter Anchor type");
-		
-		var da = _shapes[shape](params);
-        if (params.rotation) da = _rotate(da, params.rotation);
-        var a = params.jsPlumbInstance.makeDynamicAnchor(da);
-		a.type = "Perimeter";
-		return a;
-	};
 })();
