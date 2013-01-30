da6ac5979e1a34a11a6b00d895ca96909348d90d
sporritt <simon.porritt@gmail.com>
2013-01-30 20:51:26 +1100

    added functionChain method to util

--- js/1.4.0/jsPlumb-util-1.4.0-RC1.js
+++ js/1.4.0/jsPlumb-util-1.4.0-RC1.js
@@ -86,6 +86,19 @@
             }
             return c;
         },
+        //
+        // chain a list of functions, supplied by [ object, method name, args ], and return on the first
+        // one that returns the failValue. if none return the failValue, return the successValue.
+        //
+        functionChain : function(successValue, failValue, fns) {        
+            for (var i = 0; i < fns.length; i++) {
+                var o = fns[i][0][fns[i][1]].apply(fns[i][0], fns[i][2]);
+                if (o === failValue) {
+                    return o;
+                }
+            }                
+            return successValue;
+        },
         // take the given model and expand out any parameters.
         populate : function(model, values) {		
             // for a string, see if it has parameter matches, and if so, try to make the substitutions.
