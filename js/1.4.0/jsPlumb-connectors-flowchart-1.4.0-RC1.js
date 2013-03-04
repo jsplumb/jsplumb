7a474b60d26fb91dfb13f424e988489635928af2
sporritt <simon.porritt@gmail.com>
2013-03-04 22:43:58 +1100

    flowchart connector refactor

--- js/1.4.0/jsPlumb-connectors-flowchart-1.4.0-RC1.js
+++ js/1.4.0/jsPlumb-connectors-flowchart-1.4.0-RC1.js
@@ -37,7 +37,7 @@
             points = [], segments = [],
             grid = params.grid,
             userSuppliedSegments = null,
-            lastx = -1, lasty = -1, lastOrientation,	
+            lastx = null, lasty = null, lastOrientation,	
             cornerRadius = params.cornerRadius != null ? params.cornerRadius : 10,	
             sgn = function(n) { return n < 0 ? -1 : n == 0 ? 0 : 1; },            
             /**
@@ -47,8 +47,8 @@
                 // if segment would have length zero, dont add it.
                 if (sx == lastx && sy == lasty) return;
                 
-                var lx = lastx == -1 ? sx : lastx,
-                    ly = lasty == -1 ? sy : lasty,
+                var lx = lastx == null ? sx : lastx,
+                    ly = lasty == null ? sy : lasty,
                     o = lx == x ? "v" : "h",
                     sgnx = sgn(x - lx),
                     sgny = sgn(y - ly);
@@ -61,11 +61,11 @@
                 return Math.sqrt(Math.pow(s[0] - s[2], 2) + Math.pow(s[1] - s[3], 2));    
             },
             _cloneArray = function(a) { var _a = []; _a.push.apply(_a, a); return _a;},
-            updateMinMax = function(a1, a2) {
-                self.bounds.minX = Math.min(self.bounds.minX, a1[2], a2[0]);
-                self.bounds.maxX = Math.max(self.bounds.maxX, a1[2], a2[0]);
-                self.bounds.minY = Math.min(self.bounds.minY, a1[3], a2[1]);
-                self.bounds.maxY = Math.max(self.bounds.maxY, a1[3], a2[1]);    
+            updateMinMax = function(a1) {
+                self.bounds.minX = Math.min(self.bounds.minX, a1[2]);
+                self.bounds.maxX = Math.max(self.bounds.maxX, a1[2]);
+                self.bounds.minY = Math.min(self.bounds.minY, a1[3]);
+                self.bounds.maxY = Math.max(self.bounds.maxY, a1[3]);    
             },
             writeSegments = function(segments, paintInfo) {
                 console.log("writing segments. x,y,w,h is ", paintInfo.points);
@@ -104,17 +104,14 @@
                             cx:cx,
                             cy:cy,
                             ac:ac
-                        });	
-                        
-                        updateMinMax(current, next);
+                        });	                                            
                     }
                     else {
                         _super.addSegment("Straight", {
                             x1:current[0], y1:current[1], x2:current[2], y2:current[3]
                         });
-                        
-                        updateMinMax(current, current);
                     }
+                    //updateMinMax(current);
                     current = next;
                 }
                 // last segment
@@ -122,7 +119,7 @@
                     x1:next[0], y1:next[1], x2:next[2], y2:next[3]
                 });
                 
-                updateMinMax(next, next);    
+                //updateMinMax(next);    
                 console.log("bounds of segments", self.bounds.minX, self.bounds.minY, self.bounds.maxX, self.bounds.maxY);                
             };
         
@@ -152,7 +149,7 @@
             }
             
             segments = [];
-            lastx = -1; lasty = -1;
+            lastx = null; lasty = null;
             lastOrientation = null;          
             
             var midx = paintInfo.startStubX + ((paintInfo.endStubX - paintInfo.startStubX) * midpoint),
@@ -165,117 +162,110 @@
                     return start + (mult * (( 1 - anchorPos) * dimension) + _super.maxStub);
                 },
                 orientations = { x:[ 0, 1 ], y:[ 1, 0 ] },
-                perpendicular = function(axis) {
-                    with (paintInfo) {
-                        var sis = {
-                            x:[ [ [ 1,2,3,4 ], null, [ 2,1,4,3 ] ], null, [ [ 4,3,2,1 ], null, [ 3,4,1,2 ] ] ],
-                            y:[ [ [ 3,2,1,4 ], null, [ 2,3,4,1 ] ], null, [ [ 4,1,2,3 ], null, [ 1,4,3,2 ] ] ]
-                        },
-                        stubs = { 
-                            x:[ [ startStubX, endStubX ] , null, [ endStubX, startStubX ] ],
-                            y:[ [ startStubY, endStubY ] , null, [ endStubY, startStubY ] ]
-                        },
-                        midLines = {
-                            x:[ [ midx, startStubY ], [ midx, endStubY ] ],
-                            y:[ [ startStubX, midy ], [ endStubX, midy ] ]
-                        },
-                        linesToEnd = {
-                            x:[ [ endStubX, startStubY ] ],
-                            y:[ [ startStubX, endStubY ] ]
-                        },
-                        startToEnd = {
-                            x:[ [ startStubX, endStubY ], [ endStubX, endStubY ] ],        
-                            y:[ [ endStubX, startStubY ], [ endStubX, endStubY ] ]
-                        },
-                        startToMidToEnd = {
-                            x:[ [ startStubX, midy ], [ endStubX, midy ], [ endStubX, endStubY ] ],
-                            y:[ [ midx, startStubY ], [ midx, endStubY ], [ endStubX, endStubY ] ]
-                        },
-                        otherStubs = {
-                            x:[ startStubY, endStubY ],
-                            y:[ startStubX, endStubX ]                                    
-                        },
-                                    
-                        soIdx = orientations[axis][0], toIdx = orientations[axis][1],
-                        _so = so[soIdx] + 1,
-                        _to = to[toIdx] + 1,
-                        otherFlipped = (to[toIdx] == -1 && (otherStubs[axis][1] < otherStubs[axis][0])) || (to[toIdx] == 1 && (otherStubs[axis][1] > otherStubs[axis][0])),
-                        stub1 = stubs[axis][_so][0],
-                        stub2 = stubs[axis][_so][1],
-                        segmentIndexes = sis[axis][_so][_to];
-                        
-                        if (segment == segmentIndexes[3] || (segment == segmentIndexes[2] && otherFlipped)) {
-                            return midLines[axis];       
-                        }
-                        else if (segment == segmentIndexes[2] && stub2 < stub1) {
-                            return linesToEnd[axis];
-                        }
-                        else if ((segment == segmentIndexes[2] && stub2 >= stub1) || (segment == segmentIndexes[1] && !otherFlipped)) {
-                            return startToMidToEnd[axis];
-                        }
-                        else if (segment == segmentIndexes[0] || (segment == segmentIndexes[1] && otherFlipped)) {
-                            return startToEnd[axis];  
-                        }                                
-                    }                                
-                },
-                orthogonal = function(axis) {                    
-                    var pi = paintInfo,                                            
-                        extent = {
-                            "x":pi.so[0] == -1 ? Math.min(pi.startStubX, pi.endStubX) : Math.max(pi.startStubX, pi.endStubX),
-                            "y":pi.so[1] == -1 ? Math.min(pi.startStubY, pi.endStubY) : Math.max(pi.startStubY, pi.endStubY)
-                        }[axis];
-                                            
-                    return {
-                        "x":[ [ extent, pi.startStubY ],[ extent, pi.endStubY ], [ pi.endStubX, pi.endStubY ] ],
-                        "y":[ [ pi.startStubX, extent ], [ pi.endStubX, extent ],[ pi.endStubX, pi.endStubY ] ]
-                    }[axis];                    
-                },
                 lineCalculators = {
-                    oppositex : function() {
-                        with (paintInfo) {                                
-                            if (params.sourceEndpoint.elementId == params.targetEndpoint.elementId) {
-                                var _y = startStubY + ((1 - params.sourceAnchor.y) * params.sourceInfo.height) + _super.maxStub;
-                                return [ [ startStubX, _y ], [ endStubX, _y ]];
-                            }                                                        
-                            else if (!isXGreaterThanStubTimes2 || (so[0] == 1 && startStubX > endStubX)
-                               || (so[0] == -1 && startStubX < endStubX)) {
-                                return [[ startStubX, midy ], [ endStubX, midy ]];                                            
-                            }
-                            else if ((so[0] == 1 && startStubX < endStubX) || (so[0] == -1 && startStubX > endStubX)) {
-                                return [[ midx, sy ], [ midx, ty ]];
-                            }
-                        }
-                    },
-                    orthogonalx : function() {
-                        return orthogonal("x");
-                    },
-                    perpendicularx : function() { 
-                        return perpendicular("x");                                                   
-                    },
-                    oppositey : function() {
+                    perpendicular : function(axis) {
                         with (paintInfo) {
-                            if (params.sourceEndpoint.elementId == params.targetEndpoint.elementId) {
-                                var _x = startStubX + ((1 - params.sourceAnchor.x) * params.sourceInfo.width) + _super.maxStub;
-                                return [ [ _x, startStubY ], [ _x, endStubY ]];
+                            var sis = {
+                                x:[ [ [ 1,2,3,4 ], null, [ 2,1,4,3 ] ], null, [ [ 4,3,2,1 ], null, [ 3,4,1,2 ] ] ],
+                                y:[ [ [ 3,2,1,4 ], null, [ 2,3,4,1 ] ], null, [ [ 4,1,2,3 ], null, [ 1,4,3,2 ] ] ]
+                            },
+                            stubs = { 
+                                x:[ [ startStubX, endStubX ] , null, [ endStubX, startStubX ] ],
+                                y:[ [ startStubY, endStubY ] , null, [ endStubY, startStubY ] ]
+                            },
+                            midLines = {
+                                x:[ [ midx, startStubY ], [ midx, endStubY ] ],
+                                y:[ [ startStubX, midy ], [ endStubX, midy ] ]
+                            },
+                            linesToEnd = {
+                                x:[ [ endStubX, startStubY ] ],
+                                y:[ [ startStubX, endStubY ] ]
+                            },
+                            startToEnd = {
+                                x:[ [ startStubX, endStubY ], [ endStubX, endStubY ] ],        
+                                y:[ [ endStubX, startStubY ], [ endStubX, endStubY ] ]
+                            },
+                            startToMidToEnd = {
+                                x:[ [ startStubX, midy ], [ endStubX, midy ], [ endStubX, endStubY ] ],
+                                y:[ [ midx, startStubY ], [ midx, endStubY ], [ endStubX, endStubY ] ]
+                            },
+                            otherStubs = {
+                                x:[ startStubY, endStubY ],
+                                y:[ startStubX, endStubX ]                                    
+                            },
+                                        
+                            soIdx = orientations[axis][0], toIdx = orientations[axis][1],
+                            _so = so[soIdx] + 1,
+                            _to = to[toIdx] + 1,
+                            otherFlipped = (to[toIdx] == -1 && (otherStubs[axis][1] < otherStubs[axis][0])) || (to[toIdx] == 1 && (otherStubs[axis][1] > otherStubs[axis][0])),
+                            stub1 = stubs[axis][_so][0],
+                            stub2 = stubs[axis][_so][1],
+                            segmentIndexes = sis[axis][_so][_to];
+                            
+                            if (segment == segmentIndexes[3] || (segment == segmentIndexes[2] && otherFlipped)) {
+                                return midLines[axis];       
                             }
-                            else if (!isYGreaterThanStubTimes2 || (so[1] == 1 && startStubY > endStubY)
-                               || (so[1] == -1 && startStubY < endStubY)) {
-                                return [[ midx, startStubY ], [ midx, endStubY ]];                                            
+                            else if (segment == segmentIndexes[2] && stub2 < stub1) {
+                                return linesToEnd[axis];
                             }
-                            else if ((so[1] == 1 && startStubY < endStubY) || (so[1] == -1 && startStubY > endStubY)) {
-                                return [[ sx, midy ], [ tx, midy ]];
+                            else if ((segment == segmentIndexes[2] && stub2 >= stub1) || (segment == segmentIndexes[1] && !otherFlipped)) {
+                                return startToMidToEnd[axis];
                             }
+                            else if (segment == segmentIndexes[0] || (segment == segmentIndexes[1] && otherFlipped)) {
+                                return startToEnd[axis];  
+                            }                                
+                        }                                
+                    },
+                    orthogonal : function(axis) {                    
+                        var pi = paintInfo,                                            
+                            extent = {
+                                "x":pi.so[0] == -1 ? Math.min(pi.startStubX, pi.endStubX) : Math.max(pi.startStubX, pi.endStubX),
+                                "y":pi.so[1] == -1 ? Math.min(pi.startStubY, pi.endStubY) : Math.max(pi.startStubY, pi.endStubY)
+                            }[axis];
+                                                
+                        return {
+                            "x":[ [ extent, pi.startStubY ],[ extent, pi.endStubY ], [ pi.endStubX, pi.endStubY ] ],
+                            "y":[ [ pi.startStubX, extent ], [ pi.endStubX, extent ],[ pi.endStubX, pi.endStubY ] ]
+                        }[axis];                    
+                    },
+                    opposite : function(axis) {                                                
+                        var pi = paintInfo,
+                            otherAxis = {"x":"y","y":"x"}[axis], 
+                            stub = "Stub" + axis.toUpperCase(),
+                            otherStub = "Stub" + otherAxis.toUpperCase(),
+                            otherStartStub = pi["start" + otherStub],
+                            startStub = pi["start" + stub],
+                            otherEndStub = pi["end" + otherStub],
+                            endStub = pi["end" + stub],
+                            dim = {"x":"height","y":"width"}[axis],
+                            comparator = pi["is" + axis.toUpperCase() + "GreaterThanStubTimes2"],
+                            idx = axis == "x" ? 0 : 1;
+
+                        if (params.sourceEndpoint.elementId == params.targetEndpoint.elementId) {
+                            var _val = otherStartStub + ((1 - params.sourceAnchor[otherAxis]) * params.sourceInfo[dim]) + _super.maxStub;
+                            return {
+                                "x":[ [ startStub, _val ], [ endStub, _val ] ],
+                                "y":[ [ _val, startStub ], [ _val, endStub ] ]
+                            }[axis];
+                            
+                        }                                                        
+                        else if (!comparator || (pi.so[idx] == 1 && startStub > endStub)
+                           || (pi.so[idx] == -1 && startStub < endStub)) {
+                            return {
+                                "x":[[ startStub, midy ], [ endStub, midy ]],
+                                "y":[[ midx, startStub ], [ midx, endStub ]]
+                            }[axis];
                         }
-                    },
-                    orthogonaly : function() {
-                        return orthogonal("y");
-                    },
-                    perpendiculary : function() {    
-                        return perpendicular("y");
+                        else if ((pi.so[idx] == 1 && startStub < endStub) || (pi.so[idx] == -1 && startStub > endStub)) {
+                            return {
+                                "x":[[ midx, pi.sy ], [ midx, pi.ty ]],
+                                "y":[[ pi.sx, midy ], [ pi.tx, midy ]]
+                            }[axis];
+                        }                        
                     }
-                };       
-                       
-            var p = lineCalculators[paintInfo.anchorOrientation + paintInfo.sourceAxis]();
+                },
+                p = lineCalculators[paintInfo.anchorOrientation](paintInfo.sourceAxis);
+                
             if (p) {
                 for (var i = 0; i < p.length; i++) {                	
                     addSegment(segments, p[i][0], p[i][1]);
