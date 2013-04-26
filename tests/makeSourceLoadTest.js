a1094cd32b0b5aadc01238a84322f6301e54c118
sporritt <simon.porritt@gmail.com>
2013-04-26 13:18:29 +1000

    added test page for make source/make target

---
+++ tests/makeSourceLoadTest.js
@@ -0,0 +1,105 @@
+;(function() {
+    var elements = [];
+
+    window.jsPlumbLoadTest = {
+        spacing:100,
+        endpoint:{
+            endpoint: [ "Dot", { radius:10 } ],
+            paintStyle:{ fillStyle:"#456", outlineColor:"black", outlineWidth:2 },
+            connectorPaintStyle:{lineWidth:1, strokeStyle:"red"},
+            connectorHoverStyle:{strokeStyle:"#943"},
+            isSource:true,
+            isTarget:true,
+            maxConnections:-1
+        },
+
+        reset : function(thenRun) {
+            $("#numConnections").html("");
+            $("#totalCreateTime").html("");
+            $("#averageCreateTime").html("");
+            $("#repaintTime").html("");
+            var t = new Date().getTime();
+            jsPlumb.reset();
+            var t2 = new Date().getTime();
+            $("#resetTime").html(t2 - t);
+            $("#demo").empty();
+
+            if (thenRun !== false) window.setTimeout(jsPlumbLoadTest.run, 250);
+        },
+
+        run : function() {
+
+            var numElements = $("#txtElements").val(),
+                makeSource = $("#chkMakeSource")[0].checked,
+                makeTarget = $("#chkMakeTarget")[0].checked,
+                suspend = $("input[name='chkSuspend']:checked").val() === "yes",
+                setLabel = $("input[name='chkLabel']:checked").val() === "yes";
+
+            jsPlumb.importDefaults({
+                Container: $("#demo"),
+                Overlays:[ "Arrow" ]
+            });
+                    
+
+            // for bulk drawing operations this is recommended.
+            if (suspend) jsPlumb.setSuspendDrawing(true);
+
+            var st = (new Date()).getTime(),
+                ww = $(window).width(),
+                x = 0, y = 0;
+            
+            for (var i = 0; i < numElements; i++) {
+                var div = document.createElement("div");
+                div.style.left = x + "px";
+                div.style.top = y + "px";
+                div.style.zIndex = 100;
+                div.className = "jspLoad";
+                div.setAttribute("id", "div-" + i);
+                x += jsPlumbLoadTest.spacing;
+                if (x > ww) {
+                    x = 0;
+                    y += jsPlumbLoadTest.spacing;
+                }
+                div.style.backgroundColor = "#123";
+                $("#demo").append(div);
+                elements.push(div);
+                if (makeSource) jsPlumb.makeSource(div);
+                if (makeTarget) jsPlumb.makeTarget(div);
+            }
+
+
+            var connCount = 0, time = 0;
+/*            for (var i = 0; i < numElements; i++) {
+                for (var j = 0; j < numElements; j++) {
+                    if (i != j) {                       
+                        jsPlumb.connect({source:elements[i], target:elements[j]});
+                        connCount++;
+                    }
+                }
+            }
+*/            
+
+            var t = (new Date()).getTime();
+
+            jsPlumb.draggable($(".jspLoad"));
+
+            var st2 = (new Date()).getTime();
+            // instruct jsplumb to unsuspend drawing, and to do a repaint.
+            if (suspend) jsPlumb.setSuspendDrawing(false, true);
+            var t2 = (new Date()).getTime();
+
+            $("#numConnections").html(connCount);
+            $("#totalCreateTime").html((t-st) + (t2-st2));
+            $("#createTime").html(t-st);
+            $("#averageCreateTime").html((t-st)/connCount);
+            $("#repaintTime").html(t2-st2);
+            $("#averageRepaintTime").html((t2-st2)/connCount);
+            
+        }
+    };
+
+
+    $(function() {
+        $("#btnTest").bind("click", jsPlumbLoadTest.reset);
+    });
+})();
