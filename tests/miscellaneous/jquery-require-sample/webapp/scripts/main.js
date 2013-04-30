315b75184ba0977562f9a49b51589b3ed3fda9d4
sporritt <simon.porritt@gmail.com>
2013-05-01 09:22:42 +1000

    moved require demo from tests into demo, added natural docs housekeeping files

--- tests/miscellaneous/jquery-require-sample/webapp/scripts/main.js
+++
@@ -1,6 +0,0 @@
-require(["jquery", "jquery.alpha", "jquery.beta"], function($) {
-    //the jquery.alpha.js and jquery.beta.js plugins have been loaded.
-    $(function() {
-        $('body').alpha().beta();
-    });
-});
