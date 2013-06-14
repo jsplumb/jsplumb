d3ce74fa42ec90bd33862b44897dad914d423b8e
sporritt <simon.porritt@gmail.com>
2013-06-14 15:23:28 +0200

    added uuid function to jsplumb util

--- src/jsPlumb-util.js
+++ src/jsPlumb-util.js
@@ -396,6 +396,12 @@
 
             return child;
         },
+        uuid : function() {
+            return ('xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+                var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+                return v.toString(16);
+            }));
+        },
         logEnabled : true,
         log : function() {
             if (jsPlumbUtil.logEnabled && typeof console != "undefined") {
