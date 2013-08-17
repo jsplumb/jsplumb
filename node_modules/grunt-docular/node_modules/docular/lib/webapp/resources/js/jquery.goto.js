5828737c885d4a82f035464fc864df12eb5f9eab
Oliver Kopp <koppdev@googlemail.com>
2013-08-17 18:19:13 +0200

    Removes node_modules due to issues on Win32 and maximum path length

--- node_modules/grunt-docular/node_modules/docular/lib/webapp/resources/js/jquery.goto.js
+++
@@ -1,8 +0,0 @@
-(function($) {
-    $.fn.goTo = function() {
-        $('html, body').animate({
-            scrollTop: $(this).offset().top - 128 + 'px'
-        }, 500);
-        return this; // for chaining...
-    };
-})(jQuery);
