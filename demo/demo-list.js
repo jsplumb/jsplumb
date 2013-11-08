4dce937fb08cff8a5876b676ea7a058ccc0a6dae
sporritt <simon.porritt@gmail.com>
2013-11-08 15:05:01 +1100

    udpates to yui and mootools demo pages

--- demo/demo-list.js
+++ demo/demo-list.js
@@ -15,6 +15,12 @@
 		[ "animation", "Animation" ]
 	];
 
+	var libs = [
+		[ "jquery", "jQuery"],
+		[ "mootools", "MooTools" ],
+		[ "yui", "YUI3" ]
+	];
+
 	jsPlumb.bind("ready", function() {
 		var current = document.body.getAttribute("data-demo-id"),
 			library = document.body.getAttribute("data-library");
@@ -40,24 +46,39 @@
 					return d;
 				};
 
+			// next/previous links
 			var d = _d("div",  "demo-links", document.body),
 				dp = _d("div",  "", d, "<a href='" + list[prev][0] + "/" + library + ".html'>" + list[prev][1] + "<i class='fa fa-arrow-left'></i></a>"),
 				dc = _d("div",  "", d, list[idx][1]),
 				dn = _d("div",  "", d, "<a href='" + list[next][0] + "/" + library + ".html'><i class='fa fa-arrow-right'></i>" + list[next][1] + "</a>");
 
+			if (typeof jQuery != "undefined") {
+				// dropdown menu
+				var m = document.getElementsByClassName("menu")[0],
+					dd = _d("div", "dropdown", m, null, null, true),
+					a = _d("a", "", dd, "DEMOS", {href:"#", "data-toggle":"dropdown"}),
+					ul = _d("ul", "dropdown-menu", dd, null, { role:"menu" });
 
-		// dropdown menu
-			var m = document.getElementsByClassName("menu")[0],
-				dd = _d("div", "dropdown", m, null, null, true),
-				a = _d("a", "", dd, "DEMOS", {href:"#", "data-toggle":"dropdown"}),
-				ul = _d("ul", "dropdown-menu", dd, null, { role:"menu" });
+				for (var i = 0; i < list.length; i++) {
+					var li = _d("li", "", ul, null, {role:"presentation"}),
+						aa = _d("a", "", li, list[i][1], { role:"menuitem", tabindex:"-1", href:list[i][0] +"/" + library + ".html"});
+				}
 
-			for (var i = 0; i < list.length; i++) {
-				var li = _d("li", "", ul, null, {role:"presentation"}),
-					aa = _d("a", "", li, list[i][1], { role:"menuitem", tabindex:"-1", href:list[i][0] +"/" + library + ".html"});
+				$(a).dropdown();
+			}
+			else {
+				// make a drop down.
 			}
 
-			$(a).dropdown();
+			// library links
+			var lidx = jsPlumbUtil.findWithFunction(libs, function(l) { return l[0] == library; }),
+				ld = _d("div", "library-links", document.body);
+
+			for (var i = 0; i <libs.length; i++) {
+				_d("a", libs[i][0] == library ? "current-library" : "", ld, libs[i][1], {href:current + "/" + libs[i][0] + ".html"});
+			}
+
+
 		}
 
 			
