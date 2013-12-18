fa300b3fd114e5528a1de83cdc5fe1b3633a9683
sporritt <simon.porritt@gmail.com>
2013-12-18 21:41:00 +1100

    updates to dom jsplumb, event support

--- src/dom.jsPlumb.js
+++ src/dom.jsPlumb.js
@@ -15,17 +15,16 @@
      */
 	 
 	 var _getDragManager = function(instance) {
-		 var k = instance._katavorio,
-		 	e = _getEventManager(instance);
+		var k = instance._katavorio,
+			e = _getEventManager(instance);
 			
-		 if (!k) {
-			 k = instance._katavorio = new Katavorio( {
+		if (!k) {
+			k = instance._katavorio = new Katavorio( {
 				bind:e.bind,
 				unbind:e.unbind,
 				getSize:jsPlumb.getSize,
 				getPosition:function(el) {
-					var o = jsPlumbAdapter.getOffset(el);
-					return [o.left, o.top];
+					return [el.offsetLeft, el.offsetTop];
 				},
 				setPosition:function(el, xy) {
 					el.style.left = xy[0] + "px";
@@ -33,24 +32,21 @@
 				},
 				addClass:jsPlumbAdapter.addClass,
 				removeClass:jsPlumbAdapter.removeClass,
-				fireEvent:function() {
-					console.log(arguments);
-				},
 				intersects:Biltong.intersects
-			 });
-		 }
-		 return k;
-	 };
-	 
+			});
+		}
+		return k;
+	};
+
 	 var _getEventManager = function(instance) {
 		 var e = instance._evensi;
 		 if (!e) {
 			 e = instance._evensi = new TouchAdapter({
-				 
+
 			 });
 		 }
 		 return e;
-	 };       
+	 };
 
 	jsPlumb.extend(jsPlumbInstance.prototype, {		
 	
@@ -62,8 +58,14 @@
 		
 		
 		// DRAG/DROP
-		destroyDraggable:function(el) {},
-		destroyDroppable:function(el) {},
+		destroyDraggable:function(el) {
+			// TODO add destroy methods to Katavorio
+			//_getDragManager(this).destroyDraggable(el);
+		},
+		destroyDroppable:function(el) {
+			// TODO add destroy methods to Katavorio
+			//_getDragManager(this).destroyDroppable(el);
+		},
 		initDraggable : function(el, options, isPlumbedComponent, _jsPlumb) {
 			_getDragManager(this).draggable(el, options);
 		},
@@ -73,11 +75,16 @@
 		isAlreadyDraggable : function(el) { return el._katavorioDrag != null; },
 		isDragSupported : function(el, options) { return true; },
 		isDropSupported : function(el, options) { return true; },
-		getDragObject : function(eventArgs) { },
+		getDragObject : function(eventArgs) { return eventArgs[0].drag.el; },
 		getDragScope : function(el) { },
-		getDropEvent : function(args) { },
+		getDropEvent : function(args) { return args[0].event; },
 		getDropScope : function(el) { },
-		getUIPosition : function(eventArgs, zoom) { },
+		getUIPosition : function(eventArgs, zoom) {
+			return {
+				left:eventArgs[0].pos[0],
+				top:eventArgs[0].pos[1]
+			};
+		},
 		setDragFilter : function(el, filter) { },
 		setElementDraggable : function(el, draggable) { },
 		setDragScope : function(el, scope) { },
@@ -87,36 +94,22 @@
 		},
 		trigger : function(el, event, originalEvent) { },
 		getOriginalEvent : function(e) { return e; },
-		/**
-		 * event binding wrapper.  it just so happens that jQuery uses 'bind' also.  yui3, for example,
-		 * uses 'on'.
-		 */
-		 
-		 // TODO rename to 'on'
 		on : function(el, event, callback) {
-			_getEventManager(this).bind(event, callback);
-		},				
-		
-		// TODO rename to 'off'
+			_getEventManager(this).bind(el, event, callback);
+		},
 		off : function(el, event, callback) {
-			_getEventManager(this).unbind(event, callback);
+			_getEventManager(this).unbind(el, event, callback);
 		}
-		
-		
 	});
-	
-	jsPlumb.CurrentLibrary = {					        
-																															
-				
-		// TODO remove library dependency on a removeElement method.
-		removeElement : function(element) {			
-			_getElementObject(element).remove();
+
+	jsPlumb.CurrentLibrary = {
+		// TODO use the touch adapter's remove method; it unregisters
+		// event listeners properly.
+		removeElement : function(element) {
+			(element && element.parentNode) && element.parentNode.removeChild(element);
 		}
-		
-		
 	};
-	
-	//$(document).ready(jsPlumb.init);
+
 	var ready = function (f) {
         (/complete|loaded|interactive/.test(document.readyState)) ?
             f() :
