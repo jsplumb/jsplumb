df1ea465cc473cba68d36462414cd4a1762dca6e
sporritt <simon.porritt@gmail.com>
2014-04-24 14:01:02 +1000

    alter path to image in home demo

--- dist/demo/home/demo.js
+++ dist/demo/home/demo.js
@@ -158,7 +158,7 @@
 					joinstyle:"miter"
 				},
 				hoverPaintStyle:hoverPaintStyle, 
-				endpoint:["Image", {url:"../../img/endpointTest1.png"}], 
+				endpoint:["Image", {url:"endpointTest1.png"}], 
 				connector:"Straight", 
 				endpointsOnTop:true,
 				overlays:[ ["Label", {
