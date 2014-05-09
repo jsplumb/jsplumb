d7a9210ae6192bbd6c8e54c3b1a447c1f7ff1094
sporritt <simon.porritt@gmail.com>
2014-05-09 10:02:14 +1000

    final demo cleanup for 1.6.1

--- demo/sourcesAndTargets/demo.js
+++ demo/sourcesAndTargets/demo.js
@@ -73,27 +73,5 @@
 	        instance.connect({ source:"sourceWindow1", target:"targetWindow5" });
 	        instance.connect({ source:"sourceWindow1", target:"targetWindow2" });	
 		});
-		
-		window.setZoom = function(zoom, instance, transformOrigin, el) {
-  transformOrigin = transformOrigin || [ 0.5, 0.5 ];
-  el = el || instance.Defaults.Container;
-  var p = [ "webkit", "moz", "ms", "o" ],
-      s = "scale(" + zoom + ")",
-      oString = (transformOrigin[0] * 100) + "% " + (transformOrigin[1] * 100) + "%";
-
-  for (var i = 0; i < p.length; i++) {
-    el.style[p[i] + "Transform"] = s;
-    el.style[p[i] + "TransformOrigin"] = oString;
-  }
-
-  el.style["transform"] = s;
-  el.style["transformOrigin"] = oString;
-
-  instance.setZoom(zoom);    
-};
-
-	setZoom(0.5, instance, [0.7,0.7])
-	
-
 	});	
 })();
