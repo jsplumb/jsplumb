f222fbda929873af2494924938bfca9c61d867a2
sporritt <simon.porritt@gmail.com>
2014-09-05 13:46:13 +1000

    fix issue 214, disable right muoue button for makeSource

--- tests/test-support-mootools.js
+++
@@ -1,6 +0,0 @@
-jsPlumbTestSupport = {
-    getAttribute:function(el, att) {
-        return el.getAttribute(att);
-    },
-    droppableClass:"jsplumb-mootools-droppable"
-};
--- tests/test-support-yui.js
+++
@@ -1,6 +0,0 @@
-jsPlumbTestSupport = {
-    getAttribute:function(el, att) {
-        return el.getAttribute(att);
-    },
-    droppableClass:"yui3-dd-drop"
-};
