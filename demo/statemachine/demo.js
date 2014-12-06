388e3423459dd9b9185812c59cf0b722e72649db
sporritt <simon.porritt@gmail.com>
2014-12-06 15:33:33 +1100

    formatting

--- demo/statemachine/demo.js
+++ demo/statemachine/demo.js
@@ -1,70 +1,70 @@
-jsPlumb.ready(function() {
+jsPlumb.ready(function () {
 
-	// setup some defaults for jsPlumb.
-	var instance = jsPlumb.getInstance({
-		Endpoint : ["Dot", {radius:2}],
-		HoverPaintStyle : {strokeStyle:"#1e8151", lineWidth:2 },
-		ConnectionOverlays : [
-			[ "Arrow", {
-				location:1,
-				id:"arrow",
-                length:14,
-                foldback:0.8
-			} ],
-            [ "Label", { label:"FOO", id:"label", cssClass:"aLabel" }]
-		],
-		Container:"statemachine-demo"
-	});
+    // setup some defaults for jsPlumb.
+    var instance = jsPlumb.getInstance({
+        Endpoint: ["Dot", {radius: 2}],
+        HoverPaintStyle: {strokeStyle: "#1e8151", lineWidth: 2 },
+        ConnectionOverlays: [
+            [ "Arrow", {
+                location: 1,
+                id: "arrow",
+                length: 14,
+                foldback: 0.8
+            } ],
+            [ "Label", { label: "FOO", id: "label", cssClass: "aLabel" }]
+        ],
+        Container: "statemachine-demo"
+    });
 
     window.jsp = instance;
 
-	var windows = jsPlumb.getSelector(".statemachine-demo .w");
+    var windows = jsPlumb.getSelector(".statemachine-demo .w");
 
     // initialise draggable elements.
-	instance.draggable(windows);
+    instance.draggable(windows);
 
     // bind a click listener to each connection; the connection is deleted. you could of course
-	// just do this: jsPlumb.bind("click", jsPlumb.detach), but I wanted to make it clear what was
-	// happening.
-	instance.bind("click", function(c) {
-		instance.detach(c);
-	});
+    // just do this: jsPlumb.bind("click", jsPlumb.detach), but I wanted to make it clear what was
+    // happening.
+    instance.bind("click", function (c) {
+        instance.detach(c);
+    });
 
-	// bind a connection listener. note that the parameter passed to this function contains more than
-	// just the new connection - see the documentation for a full list of what is included in 'info'.
-	// this listener sets the connection's internal
-	// id as the label overlay's text.
-    instance.bind("connection", function(info) {
-		info.connection.getOverlay("label").setLabel(info.connection.id);
+    // bind a connection listener. note that the parameter passed to this function contains more than
+    // just the new connection - see the documentation for a full list of what is included in 'info'.
+    // this listener sets the connection's internal
+    // id as the label overlay's text.
+    instance.bind("connection", function (info) {
+        info.connection.getOverlay("label").setLabel(info.connection.id);
     });
 
 
-	// suspend drawing and initialise.
-	instance.doWhileSuspended(function() {
+    // suspend drawing and initialise.
+    instance.doWhileSuspended(function () {
         instance.makeSource(windows, {
-            filter:".ep",
-            anchor:"Continuous",
-            connector:[ "StateMachine", { curviness:20 } ],
-            connectorStyle:{ strokeStyle:"#5c96bc", lineWidth:2, outlineColor:"transparent", outlineWidth:4 },
-            maxConnections:5,
-            onMaxConnections:function(info, e) {
+            filter: ".ep",
+            anchor: "Continuous",
+            connector: [ "StateMachine", { curviness: 20 } ],
+            connectorStyle: { strokeStyle: "#5c96bc", lineWidth: 2, outlineColor: "transparent", outlineWidth: 4 },
+            maxConnections: 5,
+            onMaxConnections: function (info, e) {
                 alert("Maximum connections (" + info.maxConnections + ") reached");
             }
         });
-	});
+    });
 
-	// initialise all '.w' elements as connection targets.
-	instance.makeTarget(windows, {
-		dropOptions:{ hoverClass:"dragHover" },
-		anchor:"Continuous",
-		allowLoopback:true
-	});
+    // initialise all '.w' elements as connection targets.
+    instance.makeTarget(windows, {
+        dropOptions: { hoverClass: "dragHover" },
+        anchor: "Continuous",
+        allowLoopback: true
+    });
 
-	// and finally, make a couple of connections
-	instance.connect({ source:"opened", target:"phone1" });
-	instance.connect({ source:"phone1", target:"phone1" });
-	instance.connect({ source:"phone1", target:"inperson" });
+    // and finally, make a couple of connections
+    instance.connect({ source: "opened", target: "phone1" });
+    instance.connect({ source: "phone1", target: "phone1" });
+    instance.connect({ source: "phone1", target: "inperson" });
 
-	jsPlumb.fire("jsPlumbDemoLoaded", instance);
+    jsPlumb.fire("jsPlumbDemoLoaded", instance);
 
 });
