ee10c5c4735bb0e960147aa39efa7c2bb0a3c24a
sporritt <simon.porritt@gmail.com>
2015-09-09 20:20:50 +1000

    remove indexOf function and Function.prototype.bind polyfill; these were needed only for IE8

--- src/util.js
+++ src/util.js
@@ -223,18 +223,13 @@
                 for (var i = 0; i < a.length; i++) if (f(a[i])) return i;
             return -1;
         },
-        indexOf: function (l, v) {
-            return l.indexOf ? l.indexOf(v) : exports.findWithFunction(l, function (_v) {
-                return _v == v;
-            });
-        },
         removeWithFunction: function (a, f) {
             var idx = exports.findWithFunction(a, f);
             if (idx > -1) a.splice(idx, 1);
             return idx != -1;
         },
         remove: function (l, v) {
-            var idx = exports.indexOf(l, v);
+            var idx = l.indexOf(v);
             if (idx > -1) l.splice(idx, 1);
             return idx != -1;
         },
@@ -447,30 +442,5 @@
             this.cleanupListeners();
         }
     };
-
-    // thanks MDC
-    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function/bind?redirectlocale=en-US&redirectslug=JavaScript%2FReference%2FGlobal_Objects%2FFunction%2Fbind
-    if (!Function.prototype.bind) {
-        Function.prototype.bind = function (oThis) {
-            if (typeof this !== "function") {
-                // closest thing possible to the ECMAScript 5 internal IsCallable function
-                throw new TypeError("Function.prototype.bind - what is trying to be bound is not callable");
-            }
-
-            var aArgs = Array.prototype.slice.call(arguments, 1),
-                fToBind = this,
-                fNOP = function () {
-                },
-                fBound = function () {
-                    return fToBind.apply(this instanceof fNOP && oThis ? this : oThis,
-                        aArgs.concat(Array.prototype.slice.call(arguments)));
-                };
-
-            fNOP.prototype = this.prototype;
-            fBound.prototype = new fNOP();
-
-            return fBound;
-        };
-    }
 
 }).call(this);
