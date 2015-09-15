74bebef27acf2e00df1983ebf84541eb70bbe8de
sporritt <simon.porritt@gmail.com>
2015-09-16 06:05:35 +1000

    makeSource and makeTarget require explicit anchor/endpoint definitions now and do not source them from the Defaults.

--- demo/sourcesAndTargets/demo.js
+++ demo/sourcesAndTargets/demo.js
@@ -10,22 +10,9 @@ jsPlumb.ready(function () {
     ];
 
     var instance = window.instance = jsPlumb.getInstance({
-        // set default anchors.  the 'connect' calls below will pick these up, and in fact setting these means
-        // that you also do not need to supply anchor definitions to the makeSource or makeTarget functions.
-        Anchors: [ sourceAnchors, "TopCenter" ],
         // drag options
         DragOptions: { cursor: "pointer", zIndex: 2000 },
-        // default to blue at source and green at target
-        EndpointStyles: [
-            { fillStyle: "#0d78bc" },
-            { fillStyle: "#558822" }
-        ],
-        // blue endpoints 7 px; green endpoints 11.
-        Endpoints: [
-            ["Dot", { radius: 7 } ],
-            [ "Dot", { radius: 11 } ]
-        ],
-        // default to a gradient stroke from blue to green.  for IE provide all green fallback.
+        // default to a gradient stroke from blue to green.
         PaintStyle: {
             gradient: { stops: [
                 [ 0, "#0d78bc" ],
@@ -58,21 +45,21 @@ jsPlumb.ready(function () {
     // suspend drawing and initialise.
     instance.batch(function () {
 
-        // make 'window1' a connection source. notice the filter parameter: it tells jsPlumb to ignore drags
+        // make 'window1' a connection source. notice the filter and filterExclude parameters: they tell jsPlumb to ignore drags
         // that started on the 'enable/disable' link on the blue window.
         instance.makeSource("sourceWindow1", {
-            //anchor:sourceAnchors,		// you could supply this if you want, but it was set in the defaults above.
-            filter: function (evt, el) {
-                var t = evt.target || evt.srcElement;
-                return t.tagName !== "A";
-            },
-            maxConnections: -1
+            filter:"a",
+            filterExclude:true,
+            maxConnections: -1,
+            endpoint:[ "Dot", { radius: 7, cssClass:"small-blue" } ],
+            anchor:sourceAnchors
         });
 
         // configure the .smallWindows as targets.
         instance.makeTarget(smallWindows, {
-            //anchor:"TopCenter",				// you could supply this if you want, but it was set in the defaults above.
-            dropOptions: { hoverClass: "hover" }
+            dropOptions: { hoverClass: "hover" },
+            anchor:"Top",
+            endpoint:[ "Dot", { radius: 11, cssClass:"large-green" } ]
         });
 
         // and finally connect a couple of small windows, just so its obvious what's going on when this demo loads.           
