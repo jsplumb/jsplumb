3262a6b6232072123fccf98a57eb7f474cfbf583
sporritt <simon.porritt@gmail.com>
2015-11-02 07:07:30 +1100

    set connector geometry if passed in to connection constructor

--- src/connection.js
+++ src/connection.js
@@ -222,6 +222,9 @@
 // PAINTING
 
         this.setConnector(this.endpoints[0].connector || this.endpoints[1].connector || params.connector || _jsPlumb.Defaults.Connector || _jp.Defaults.Connector, true);
+        if (params.geometry) {
+            this.connector.setGeometry(params.geometry);
+        }
         var data = params.data == null || !jsPlumbUtil.isObject(params.data) ? {} : params.data;
         this.getData = function() { return data; };
         this.setData = function(d) { data = d || {}; };
