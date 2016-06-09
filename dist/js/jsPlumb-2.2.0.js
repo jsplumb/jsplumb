47ae9c1360274ead7fd3343ae01e5f9ac9d42ca9
sporritt <simon.porritt@gmail.com>
2016-06-09 21:34:53 +1000

    2.2.0 alpha

--- dist/js/jsPlumb-2.2.0.js
+++ dist/js/jsPlumb-2.2.0.js
@@ -3830,6 +3830,7 @@
                             var newEndpoint = tep.endpoint != null && tep.endpoint._jsPlumb ? tep.endpoint : _addEndpoint(_p[type], tep.def, idx);
                             if (newEndpoint.isFull()) return false;
                             _p[type + "Endpoint"] = newEndpoint;
+                            if (!_p.scope && tep.def.scope) _p.scope = tep.def.scope; // provide scope if not already provided and endpoint def has one.
                             newEndpoint._doNotDeleteOnDetach = false; // reset.
                             newEndpoint._deleteOnDetach = true;
                             if (tep.uniqueEndpoint) {
@@ -6059,9 +6060,9 @@
         define("jsplumb", [], function () {
             return jsPlumb;
         });
-        define("jsplumbinstance", [], function () {
-            return jsPlumb.getInstance();
-        });
+//        define("jsplumbinstance", [], function () {
+//            return jsPlumb.getInstance();
+//        });
     }
     // CommonJS
     if (typeof exports !== 'undefined') {
@@ -11882,13 +11883,18 @@
                     _jsPlumb.fire(EVT_CHILD_ADDED, {group: self, el: __el});
                 }
             });
+
+            _jsPlumb.getGroupManager().updateConnectionsForGroup(self);
         };
-        this.remove = function(el, manipulateDOM, doNotFireEvent) {
+
+        this.remove = function(el, manipulateDOM, doNotFireEvent, doNotUpdateConnections) {
+
             _each(el, function(__el) {
                 delete __el._jsPlumbGroup;
                 _ju.removeWithFunction(elements, function(e) {
                     return e === __el;
                 });
+
                 if (manipulateDOM) {
                     try { self.getEl().removeChild(__el); }
                     catch (e) {
@@ -11900,12 +11906,16 @@
                     _jsPlumb.fire(EVT_CHILD_REMOVED, {group: self, el: __el});
                 }
             });
+            if (!doNotUpdateConnections) {
+                _jsPlumb.getGroupManager().updateConnectionsForGroup(self);
+            }
         };
-        this.removeAll = function() {
-            for (var i = 0; i < elements.length; i++) {
-                _jsPlumb.remove(elements[i]);
+        this.removeAll = function(manipulateDOM, doNotFireEvent) {
+            for (var i = 0, l = elements.length; i < l; i++) {
+                self.remove(elements[0], manipulateDOM, doNotFireEvent, true);
             }
             elements.length = 0;
+            _jsPlumb.getGroupManager().updateConnectionsForGroup(self);
         };
         this.orphanAll = function() {
             for (var i = 0; i < elements.length; i++) {
@@ -11981,6 +11991,7 @@
         // unbind the group specific drag/revert handlers.
         //
         function _unbindDragHandlers(_el) {
+            if (!_el._katavorioDrag) return;
             if (prune || orphan) {
                 _el._katavorioDrag.off(STOP, _pruneOrOrphan);
             }
@@ -11991,6 +12002,7 @@
         }
 
         function _bindDragHandlers(_el) {
+            if (!_el._katavorioDrag) return;
             if (prune || orphan) {
                 _el._katavorioDrag.on(STOP, _pruneOrOrphan);
             }
