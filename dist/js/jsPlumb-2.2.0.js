c6ce7599e3dc0908abbabe82a70b15f6db56cb67
sporritt <simon.porritt@gmail.com>
2016-08-08 20:51:49 +1000

    2.2.0 alpha

--- dist/js/jsPlumb-2.2.0.js
+++ dist/js/jsPlumb-2.2.0.js
@@ -2230,6 +2230,13 @@
         var _destroy = function(el, type, map) {
             el = _gel(el);
             if (el[type]) {
+
+                // remove from selection, if present.
+                var selIdx = _selection.indexOf(el[type]);
+                if (selIdx >= 0) {
+                    _selection.splice(selIdx, 1);
+                }
+
                 if (_unreg(el[type], map)) {
                     _each(el[type], function(kObj) { kObj.destroy() });
                 }
@@ -3681,19 +3688,17 @@
                                 });
                                 options[stopEvent] = _ju.wrap(options[stopEvent], function () {
                                     var elements = arguments[0].selection;
-                                    var uip = _currentInstance.getUIPosition(arguments);
-
-                                    // this is one element
                                     var _one = function (_e) {
-                                        if (uip != null) _draw(_e[0], uip);
+                                        if (_e[1] != null) _draw(_e[0], _e[1]);
                                         _currentInstance.removeClass(_e[0], "jsplumb-dragged");
                                         _currentInstance.select({source: _e[0]}).removeClass(_currentInstance.elementDraggingClass + " " + _currentInstance.sourceElementDraggingClass, true);
                                         _currentInstance.select({target: _e[0]}).removeClass(_currentInstance.elementDraggingClass + " " + _currentInstance.targetElementDraggingClass, true);
                                         _currentInstance.getDragManager().dragEnded(_e[0]);
                                     };
 
-                                    for (var i = 0; i < elements.length; i++)
+                                    for (var i = 0; i < elements.length; i++) {
                                         _one(elements[i]);
+                                    }
 
                                     _started = false;
                                     _currentInstance.setHoverSuspended(false);
@@ -5747,6 +5752,7 @@
 
         this.reset = function () {
             _currentInstance.silently(function() {
+                _hoverSuspended = false;
                 _currentInstance.removeAllGroups();
                 _currentInstance.removeGroupManager();
                 _currentInstance.deleteEveryEndpoint();
@@ -6076,23 +6082,14 @@
 
     var root = this, _ju = root.jsPlumbUtil;
 
-    var svgAvailable = !!window.SVGAngle || document.implementation.hasFeature("http://www.w3.org/TR/SVG11/feature#BasicStructure", "1.1"),
-
-        _genLoc = function (e, prefix) {
+    var _genLoc = function (prefix, e) {
             if (e == null) return [ 0, 0 ];
             var ts = _touches(e), t = _getTouch(ts, 0);
             return [t[prefix + "X"], t[prefix + "Y"]];
         },
-        _pageLocation = function (e) {
-            if (e == null) return [ 0, 0 ];
-            return _genLoc(e, "page");
-        },
-        _screenLocation = function (e) {
-            return _genLoc(e, "screen");
-        },
-        _clientLocation = function (e) {
-            return _genLoc(e, "client");
-        },
+        _pageLocation = _genLoc.bind(this, "page"),
+        _screenLocation = _genLoc.bind(this, "screen"),
+        _clientLocation = _genLoc.bind(this, "client"),
         _getTouch = function (touches, idx) {
             return touches.item ? touches.item(idx) : touches[idx];
         },
@@ -11476,7 +11473,7 @@
         });
 
         this.addGroup = function(group) {
-            _jsPlumb.addClass(group.el, GROUP_EXPANDED_CLASS);
+            _jsPlumb.addClass(group.getEl(), GROUP_EXPANDED_CLASS);
             _managedGroups[group.id] = group;
             group.manager = this;
             _updateConnectionsForGroup(group);
@@ -11746,8 +11743,12 @@
         this.getEl = function() { return el; };
         this.id = params.id || _ju.uuid();
         el._isJsPlumbGroup = true;
-        var da = _jsPlumb.getSelector(el, GROUP_CONTAINER_SELECTOR);
-        var dragArea = da && da.length > 0 ? da[0] : el;
+
+        var getDragArea = this.getDragArea = function() {
+            var da = _jsPlumb.getSelector(el, GROUP_CONTAINER_SELECTOR);
+            return da && da.length > 0 ? da[0] : el;
+        };
+
         var ghost = params.ghost === true;
         var constrain = ghost || (params.constrain === true);
         var revert = params.revert !== false;
@@ -11791,7 +11792,7 @@
                     // if already a member of this group, do nothing
                     if (currentGroup !== self) {
                         var elpos = _jsPlumb.getOffset(_el, true);
-                        var cpos = self.collapsed ? _jsPlumb.getOffset(el, true) : _jsPlumb.getOffset(dragArea, true);
+                        var cpos = self.collapsed ? _jsPlumb.getOffset(el, true) : _jsPlumb.getOffset(getDragArea(), true);
 
                         // otherwise, transfer to this group.
                         if (currentGroup != null) {
@@ -11849,6 +11850,7 @@
         };
 
         this.add = function(_el, doNotFireEvent) {
+            var dragArea = getDragArea();
             _each(_el, function(__el) {
                 __el._jsPlumbGroup = self;
                 elements.push(__el);
@@ -11878,7 +11880,7 @@
                 });
 
                 if (manipulateDOM) {
-                    try { self.getEl().removeChild(__el); }
+                    try { self.getDragArea().removeChild(__el); }
                     catch (e) {
                         jsPlumbUtil.log("Could not remove element from Group " + e);
                     }
