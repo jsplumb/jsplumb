7433ab3c5fd7b5e5469640284efc2dbe8cf6bc5b
sporritt <simon.porritt@gmail.com>
2016-08-12 23:02:19 +1000

    upgrade to Mottle 0.7.3 and build 2.1.5 final

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,7 +2,7 @@
 var versions = {
         JS_BEZIER : { f:"jsBezier", v:"0.8" },
         BILTONG : { f:"biltong", v:"0.3" },
-        MOTTLE : {f:"mottle", v:"0.7.2" },
+        MOTTLE : {f:"mottle", v:"0.7.3" },
         KATAVORIO : {f:"katavorio", v:"0.17.0" }
     },
     get = function(name) { return "lib/" + versions[name].f + "-" + versions[name].v + ".js"; },
