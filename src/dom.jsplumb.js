a138ce23f3b9bd56587e61fdb1dad51d70f03daf
sporritt <simon.porritt@gmail.com>
2018-12-28 14:36:55 +1100

    moved the code from base library adapter into the dom adapter, as it was code shared by the yui/mootools/jquery versions, and is DOM specific. headless/react native setups would not include that code.

--- src/dom.jsplumb.js
+++ src/dom.jsplumb.js
@@ -16,6 +16,14 @@
     var root = this, _jp = root.jsPlumb, _ju = root.jsPlumbUtil,
         _jk = root.Katavorio, _jg = root.Biltong, draggableStates = {};
 
+    var _getEventManager = function(instance) {
+        var e = instance._mottle;
+        if (!e) {
+            e = instance._mottle = new root.Mottle();
+        }
+        return e;
+    };
+
     var _getDragManager = function (instance, category) {
 
         category = category || "main";
@@ -963,6 +971,22 @@
                     this[key].reset();
                 }
             }
+        },
+        getEventManager:function() {
+            return _getEventManager(this);
+        },
+        on : function(el, event, callback) {
+            // TODO: here we would like to map the tap event if we know its
+            // an internal bind to a click. we have to know its internal because only
+            // then can we be sure that the UP event wont be consumed (tap is a synthesized
+            // event from a mousedown followed by a mouseup).
+            //event = { "click":"tap", "dblclick":"dbltap"}[event] || event;
+            this.getEventManager().on.apply(this, arguments);
+            return this;
+        },
+        off : function(el, event, callback) {
+            this.getEventManager().off.apply(this, arguments);
+            return this;
         }
 
     });
