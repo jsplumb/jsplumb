1851d774b91c8a4343eec144940c8e617ed2e228
sporritt <simon.porritt@gmail.com>
2019-04-01 11:33:37 +0100

    created. not in use yet.

---
+++ ts/constants.ts
@@ -0,0 +1,10 @@
+namespace jsPlumbConstants {
+    export const SOURCE_DEFINITION_LIST = "_jsPlumbSourceDefinitions";
+    export const TARGET_DEFINITION_LIST = "_jsPlumbTargetDefinitions";
+    export const DEFAULT = "default";
+    export const WILDCARD = "*";
+    export const SOURCE = "source";
+    export const TARGET = "target";
+    export const SOURCE_INDEX = 0;
+    export const TARGET_INDEX = 1;
+}
