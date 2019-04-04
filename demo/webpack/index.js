1a6a13808b65e32a3641b434816baed215d10039
sporritt <simon.porritt@gmail.com>
2019-04-04 09:09:48 +0100

    'Container' -> 'container'

--- demo/webpack/index.js
+++ demo/webpack/index.js
@@ -4,7 +4,8 @@ var j = require("./node_modules/jsplumb/dist/js/jsplumb.js").jsPlumb.getInstance
     Endpoint: [ "Dot", { radius: 2 }],
     ConnectionOverlays: [
         [ "Arrow", { location: 0, width: 10, length: 7, foldbackPoint: 0.62, direction:-1 }]
-    ]
+    ],
+    container:document.body
 });
 
 j.connect({source: "one", target: "two" });
