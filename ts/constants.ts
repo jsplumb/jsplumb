16696026a4c2d5cbddebd1a6395eedfc3dece080
sporritt <simon.porritt@gmail.com>
2019-04-04 09:04:46 +0100

    more constants. still unused

--- ts/constants.ts
+++ ts/constants.ts
@@ -8,4 +8,12 @@ namespace jsPlumbConstants {
     export const SOURCE_INDEX = 0;
     export const TARGET_INDEX = 1;
     export const GROUP_KEY = "_jsPlumbGroup";
+
+    namespace Attributes {
+        export const MANAGED = "jtk-managed";
+        export const GROUP = "jtk-group";
+        export const SOURCE = "jtk-source";
+        export const TARGET = "jtk-target";
+    }
+
 }
