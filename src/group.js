d8e7ca5a72df292cf978dd0130890e65913a50d9
sporritt <simon.porritt@gmail.com>
2019-06-04 20:09:36 +0100

    fix refs to expand/collapse methods

--- src/group.js
+++ src/group.js
@@ -127,11 +127,11 @@
                             c.setVisible(false);
                             if (c.endpoints[oidx].element._jsPlumbGroup === group) {
                                 c.endpoints[oidx].setVisible(false);
-                                self.expandConnection(c, oidx, group);
+                                _expandConnection(c, oidx, group);
                             }
                             else {
                                 c.endpoints[index].setVisible(false);
-                                self.collapseConnection(c, index, group);
+                                _collapseConnection(c, index, group);
                             }
                         });
                     };
