661f7f8829de0ba6cf745339a615961908dc94e4
sporritt <simon.porritt@gmail.com>
2019-06-04 09:32:42 +0100

    first working lists demo, somewhat hardcoded in the lib still.

--- demo/lists/demo.js
+++ demo/lists/demo.js
@@ -1,39 +1,53 @@
 jsPlumb.ready(function () {
 
     var instance = jsPlumb.getInstance({
-        Connector: "StateMachine",
+        Connector: "Straight",
         PaintStyle: { strokeWidth: 3, stroke: "#ffa500", "dashstyle": "2 4" },
         Endpoint: [ "Dot", { radius: 5 } ],
         EndpointStyle: { fill: "#ffa500" },
-        Container: "perimeter-demo"
+        Container: "canvas"
     });
 
     window.jsp = instance;
 
-    // attach scroll listeners to uls
+    // register the two elements that contain a list inside them
+    var list1 = document.querySelector("#list-one"),
+        list2 = document.querySelector("#list-two");
+
+    instance.manage("list-one", list1);
+    instance.manage("list-two", list2);
+
+    instance.draggable(list1);
+    instance.draggable(list2);
+
+    // get uls
     var lists = jsPlumb.getSelector("ul");
 
     // suspend drawing and initialise.
     instance.batch(function () {
         for (var l = 0; l < lists.length; l++) {
 
+
+            var isSource = lists[l].getAttribute("source") != null,
+                isTarget = lists[l].getAttribute("target") != null;
+
             // configure items
             var items = lists[l].querySelectorAll("li");
             for (var i = 0; i < items.length; i++) {
-                instance.makeSource(items[i], {
-                    allowLoopback: false,
-                    anchor: "Right"
-                });
 
-                instance.makeTarget(items[i], {
-                    anchor: "Left"
-                });
+                if (isSource) {
+                    instance.makeSource(items[i], {
+                        allowLoopback: false,
+                        anchor: ["Left", "Right" ]
+                    });
+                }
+
+                if (isTarget) {
+                    instance.makeTarget(items[i], {
+                        anchor: ["Left", "Right" ]
+                    });
+                }
             }
-
-            // configure scroll listener
-            instance.on(lists[l], "scroll", function () {
-                console.log("i will repaint");
-            });
         }
     });
 
