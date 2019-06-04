974bf26c57e08f7c178034fca46a152189df1ae3
sporritt <simon.porritt@gmail.com>
2019-06-04 18:49:42 +0100

    - document the demo a little - connect a few items at init time

--- demo/lists/demo.js
+++ demo/lists/demo.js
@@ -10,13 +10,23 @@ jsPlumb.ready(function () {
 
     window.jsp = instance;
 
-    // register the two elements that contain a list inside them
+    // get the two elements that contain a list inside them
     var list1 = document.querySelector("#list-one"),
         list2 = document.querySelector("#list-two");
 
-    instance.manage("list-one", list1);
-    instance.manage("list-two", list2);
 
+    /*
+
+    There are two ways of registering a list.
+
+    1. by including a `jtk-scrollable-list` attribute on some element, and then calling `draggable` or `manage` on either that element
+    or, as is the case here, on an ancestor of it.
+
+    2. Add a list via the `addList(el, options)` method on a jsPlumb instance. The advantage of this approach is that you can provide endpoint and anchor specifications.
+
+     */
+
+    // approach #1: make draggable the ancestor of some element that has a `jtk-scrollable-group` attribute.
     instance.draggable(list1);
     instance.draggable(list2);
 
@@ -25,8 +35,10 @@ jsPlumb.ready(function () {
 
     // suspend drawing and initialise.
     instance.batch(function () {
-        for (var l = 0; l < lists.length; l++) {
 
+        var selectedSources = [], selectedTargets = [];
+
+        for (var l = 0; l < lists.length; l++) {
 
             var isSource = lists[l].getAttribute("source") != null,
                 isTarget = lists[l].getAttribute("target") != null;
@@ -40,16 +52,37 @@ jsPlumb.ready(function () {
                         allowLoopback: false,
                         anchor: ["Left", "Right" ]
                     });
+
+                    if (Math.random() < 0.2) {
+                        selectedSources.push(items[i]);
+                    }
                 }
 
                 if (isTarget) {
                     instance.makeTarget(items[i], {
                         anchor: ["Left", "Right" ]
                     });
+                    if (Math.random() < 0.2) {
+                        selectedTargets.push(items[i]);
+                    }
                 }
             }
         }
+
+        var connCount = Math.min(selectedSources.length, selectedTargets.length);
+        for (var i = 0; i < connCount; i++) {
+            instance.connect({source:selectedSources[i], target:selectedTargets[i]});
+        }
     });
+
+    // approach #2: directly add a list, with options.
+    instance.addList(list1.querySelector("ul"), {
+        endpoint:["Rectangle", {width:20, height:20}]
+    });
+
+
+
+    instance.bind("click", function(c) { instance.deleteConnection(c); });
 
     jsPlumb.fire("jsPlumbDemoLoaded", instance);
 });
