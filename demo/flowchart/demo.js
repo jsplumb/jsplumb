77c4b5571d322fbc2d07c2668655208bad9d6f72
sporritt <simon.porritt@gmail.com>
2019-07-05 09:31:30 +0100

    dont use arrow overlay for the time being - it isnt yet done

--- demo/flowchart/demo.js
+++ demo/flowchart/demo.js
@@ -1,21 +1,21 @@
 jsPlumb.ready(function () {
 
-    var instance = window.jsp = jsPlumb.getInstance({
+    var instance = window.jsp = jsPlumb.newInstance({
         // default drag options
         dragOptions: { cursor: 'pointer', zIndex: 2000, grid:[20,20] },
         // the overlays to decorate each connection with.  note that the label overlay uses a function to generate the label text; in this
         // case it returns the 'labelText' member that we set on each connection in the 'init' method below.
         connectionOverlays: [
-            [ "Arrow", {
-                location: 1,
-                visible:true,
-                width:11,
-                length:11,
-                id:"ARROW",
-                events:{
-                    click:function() { alert("you clicked on the arrow overlay")}
-                }
-            } ],
+            // [ "Arrow", {
+            //     location: 1,
+            //     visible:true,
+            //     width:11,
+            //     length:11,
+            //     id:"ARROW",
+            //     events:{
+            //         click:function() { alert("you clicked on the arrow overlay")}
+            //     }
+            // } ],
             [ "Label", {
                 location: 0.1,
                 id: "label",
@@ -154,6 +154,6 @@ jsPlumb.ready(function () {
         });
     });
 
-    jsPlumb.fire("jsPlumbDemoLoaded", instance);
+   // jsPlumb.fire("jsPlumbDemoLoaded", instance);
 
 });
