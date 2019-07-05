661d3b166e8e71bf531a3b964e0c6cc7ab068375
sporritt <simon.porritt@gmail.com>
2019-07-05 09:30:53 +0100

    comment out for the time being

--- demo/demo-list.js
+++ demo/demo-list.js
@@ -17,53 +17,53 @@
         [ "groups", "Groups" ]
     ];
 
-    jsPlumb.bind("ready", function () {
-        var current = document.body.getAttribute("data-demo-id");
-
-        if (current) {
-            var idx = jsPlumbUtil.findWithFunction(list, function (i) {
-                    return i[0] == current;
-                }),
-                prev = idx == 0 ? list.length - 1 : idx - 1,
-                next = idx == list.length - 1 ? 0 : idx + 1,
-                _d = function (tag, clazz, parent, html, atts, atStart) {
-                    var d = document.createElement(tag);
-                    d.className = clazz;
-                    if (parent) {
-                        if (atStart && parent.childNodes.length > 0)
-                            parent.insertBefore(d, parent.firstChild);
-                        else
-                            parent.appendChild(d);
-                    }
-                    if (html) d.innerHTML = html;
-                    if (atts) {
-                        for (var i in atts)
-                            d.setAttribute(i, atts[i]);
-                    }
-                    return d;
-                };
-
-            // next/previous links
-            var d = _d("div", "demo-links", document.body),
-                dp = _d("div", "", d, "<a href='../" + list[prev][0] + "/dom.html'>" + list[prev][1] + "<i class='fa fa-arrow-left'></i></a>"),
-                dc = _d("div", "current-library", d, list[idx][1]),
-                dn = _d("div", "", d, "<a href='../" + list[next][0] + "/dom.html'><i class='fa fa-arrow-right'></i>" + list[next][1] + "</a>");
-
-            // make a drop down.
-            var m = document.querySelectorAll(".menu")[0],
-                sel = _d("select", "", m, null, null, true);
-            for (var i = 0; i < list.length; i++) {
-                var opts = {"data-href": "../" + list[i][0] + "/dom.html"};
-                if (list[i][0] == current)
-                    opts["selected"] = true;
-                _d("option", "", sel, list[i][1], opts, null);
-            }
-            jsPlumb.on(sel, "change", function () {
-                document.location = sel.options[sel.selectedIndex].getAttribute("data-href");
-            });
-            sel.style.marginRight = "20px";
-
-        }
+    jsPlumb.ready(function () {
+        // var current = document.body.getAttribute("data-demo-id");
+        //
+        // if (current) {
+        //     var idx = jsPlumbUtil.findWithFunction(list, function (i) {
+        //             return i[0] == current;
+        //         }),
+        //         prev = idx == 0 ? list.length - 1 : idx - 1,
+        //         next = idx == list.length - 1 ? 0 : idx + 1,
+        //         _d = function (tag, clazz, parent, html, atts, atStart) {
+        //             var d = document.createElement(tag);
+        //             d.className = clazz;
+        //             if (parent) {
+        //                 if (atStart && parent.childNodes.length > 0)
+        //                     parent.insertBefore(d, parent.firstChild);
+        //                 else
+        //                     parent.appendChild(d);
+        //             }
+        //             if (html) d.innerHTML = html;
+        //             if (atts) {
+        //                 for (var i in atts)
+        //                     d.setAttribute(i, atts[i]);
+        //             }
+        //             return d;
+        //         };
+        //
+        //     // next/previous links
+        //     var d = _d("div", "demo-links", document.body),
+        //         dp = _d("div", "", d, "<a href='../" + list[prev][0] + "/dom.html'>" + list[prev][1] + "<i class='fa fa-arrow-left'></i></a>"),
+        //         dc = _d("div", "current-library", d, list[idx][1]),
+        //         dn = _d("div", "", d, "<a href='../" + list[next][0] + "/dom.html'><i class='fa fa-arrow-right'></i>" + list[next][1] + "</a>");
+        //
+        //     // make a drop down.
+        //     var m = document.querySelectorAll(".menu")[0],
+        //         sel = _d("select", "", m, null, null, true);
+        //     for (var i = 0; i < list.length; i++) {
+        //         var opts = {"data-href": "../" + list[i][0] + "/dom.html"};
+        //         if (list[i][0] == current)
+        //             opts["selected"] = true;
+        //         _d("option", "", sel, list[i][1], opts, null);
+        //     }
+        //     jsPlumb.on(sel, "change", function () {
+        //         document.location = sel.options[sel.selectedIndex].getAttribute("data-href");
+        //     });
+        //     sel.style.marginRight = "20px";
+        //
+        // }
 
     });
 })();
