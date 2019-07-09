78392b9311709dbe38888aa19a9923424f801fcd
sporritt <simon.porritt@gmail.com>
2019-07-09 12:11:33 +0100

    first working pass at groups

--- ts/group/group-manager.ts
+++ ts/group/group-manager.ts
@@ -1,15 +1,316 @@
-import {jsPlumbInstance} from "../core";
+import {ConnectionSelection, Dictionary, jsPlumbInstance} from "../core";
 import {Group} from "./group";
+import * as Constants from "../constants";
+import {IS, removeWithFunction, suggest} from "../util";
+import {Connection} from "..";
 export class GroupManager<E> {
 
-    groups:Array<Group<E>> = [];
+
+    groupMap:Dictionary<Group<E>> = {};
+    _connectionSourceMap:Dictionary<Group<E>> = {};
+    _connectionTargetMap:Dictionary<Group<E>> = {};
+
 
     constructor(public instance:jsPlumbInstance<E>) {
 
+        instance.bind("connection", (p:any) => {
+            if (p.source[Constants.GROUP_KEY] != null && p.target[Constants.GROUP_KEY] != null && p.source[Constants.GROUP_KEY] === p.target[Constants.GROUP_KEY]) {
+                this._connectionSourceMap[p.connection.id] = p.source[Constants.GROUP_KEY];
+                this._connectionTargetMap[p.connection.id] = p.source[Constants.GROUP_KEY];
+            }
+            else {
+                if (p.source[Constants.GROUP_KEY] != null) {
+                    suggest(p.source[Constants.GROUP_KEY].connections.source, p.connection);
+                    this._connectionSourceMap[p.connection.id] = p.source[Constants.GROUP_KEY];
+                }
+                if (p.target[Constants.GROUP_KEY] != null) {
+                    suggest(p.target[Constants.GROUP_KEY].connections.target, p.connection);
+                    this._connectionTargetMap[p.connection.id] = p.target[Constants.GROUP_KEY];
+                }
+            }
+        });
+
+        instance.bind(Constants.EVENT_INTERNAL_CONNECTION_DETACHED, (p:any) => {
+            this._cleanupDetachedConnection(p.connection);
+        });
+
+        instance.bind(Constants.EVENT_CONNECTION_MOVED, (p:any) => {
+            let connMap = p.index === 0 ? this._connectionSourceMap : this._connectionTargetMap;
+            let group = connMap[p.connection.id];
+            if (group) {
+                let list = group.connections[p.index === 0 ? "source" : "target"];
+                let idx = list.indexOf(p.connection);
+                if (idx !== -1) {
+                    list.splice(idx, 1);
+                }
+            }
+        });
     }
 
-    addToGroup(targetGroup:Group<E>, el:E, aBoolean?:boolean) {
+    private _cleanupDetachedConnection(conn:Connection<E>) {
+        delete conn.proxies;
+        var group = this._connectionSourceMap[conn.id], f;
+        if (group != null) {
+            f = (c:Connection<E>) => { return c.id === conn.id; };
+            removeWithFunction(group.connections.source, f);
+            removeWithFunction(group.connections.target, f);
+            delete this._connectionSourceMap[conn.id];
+        }
 
+        group = this._connectionTargetMap[conn.id];
+        if (group != null) {
+            f = (c:Connection<E>) => { return c.id === conn.id; };
+            removeWithFunction(group.connections.source, f);
+            removeWithFunction(group.connections.target, f);
+            delete this._connectionTargetMap[conn.id];
+        }
+    }
+
+
+    addGroup(params:any) {
+
+        if (this.groupMap[params.id] != null) {
+            throw new TypeError("cannot create Group [" + params.id + "]; a Group with that ID exists");
+        }
+        if (params.el[Constants.GROUP_KEY] != null) {
+            throw new TypeError("cannot create Group [" + params.id + "]; the given element is already a Group");
+        }
+        let group = new Group<E>(this.instance, params.el, params);
+        this.groupMap[group.id] = group;
+        if (params.collapsed) {
+            this.collapseGroup(group);
+        }
+
+        this.instance.addClass(group.el, Constants.GROUP_EXPANDED_CLASS);
+        //_managedGroups[group.id] = group;
+        group.manager = this;
+        this._updateConnectionsForGroup(group);
+
+        this.instance.fire(Constants.EVENT_GROUP_ADDED, { group:group });
+
+        return group;
+    }
+
+    getGroup (groupId:string | Group<E>):Group<E> {
+        let group = groupId;
+        if (IS.aString(groupId)) {
+            group = this.groupMap[groupId as string];
+            if (group == null) {
+                throw new TypeError("No such group [" + groupId + "]");
+            }
+        }
+        return group as Group<E>;
+    }
+
+    forEach(f:(g:Group<E>) => any) {
+        for (let key in this.groupMap) {
+            f(this.groupMap[key]);
+        }
+    }
+
+    private _setGroupVisible(group:Group<E>, state:boolean) {
+        let m = group.children;
+        for (let i = 0; i < m.length; i++) {
+            this.instance[state ? Constants.CMD_SHOW : Constants.CMD_HIDE](m[i], true);
+        }
+    }
+
+    _updateConnectionsForGroup(group:Group<E>) {
+        let members = group.children;
+        const c1 = this.instance.getConnections({source:members, scope:"*"}, true) as Array<Connection<E>>;
+        const c2 = this.instance.getConnections({target:members, scope:"*"}, true) as Array<Connection<E>>;
+        const processed = {};
+        group.connections.source.length = 0;
+        group.connections.target.length = 0;
+        const oneSet = (c:Array<Connection<E>>) => {
+            for (let i = 0; i < c.length; i++) {
+                if (processed[c[i].id]) {
+                    continue;
+                }
+                processed[c[i].id] = true;
+                if (c[i].source[Constants.GROUP_KEY] === group) {
+                    if (c[i].target[Constants.GROUP_KEY] !== group) {
+                        group.connections.source.push(c[i]);
+                    }
+                    this._connectionSourceMap[c[i].id] = group;
+                }
+                else if (c[i].target[Constants.GROUP_KEY] === group) {
+                    group.connections.target.push(c[i]);
+                    this._connectionTargetMap[c[i].id] = group;
+                }
+            }
+        };
+        oneSet(c1); oneSet(c2);
+    }
+
+    private _collapseConnection(conn:Connection<E>, index:number, group:Group<E>) {
+        let otherEl = conn.endpoints[index === 0 ? 1 : 0].element;
+        if (otherEl[Constants.GROUP_KEY] && (!otherEl[Constants.GROUP_KEY].proxied && otherEl[Constants.GROUP_KEY].collapsed)) {
+            return;
+        }
+
+        let groupEl = group.el, groupElId = this.instance.getId(groupEl);
+        this.instance.proxyConnection(conn, index, groupEl, groupElId, (conn:Connection<E>, index:number) => { return group.getEndpoint(conn, index); }, (conn:Connection<E>, index:number) => { return group.getAnchor(conn, index); });
+    }
+
+    private _expandConnection(c:Connection<E>, index:number, group:Group<E>) {
+        this.instance.unproxyConnection(c, index, this.instance.getId(group.el));
+    }
+
+    collapseGroup (group:string | Group<E>) {
+        let actualGroup = this.getGroup(group);
+        if (actualGroup == null || actualGroup.collapsed) {
+            return;
+        }
+        var groupEl = actualGroup.el;
+
+        // todo remove old proxy endpoints first, just in case?
+        //group.proxies.length = 0;
+
+        // hide all connections
+        this._setGroupVisible(actualGroup, false);
+
+        if (actualGroup.proxied) {
+            // collapses all connections in a group.
+            const _collapseSet =  (conns:Array<Connection<E>>, index:number) => {
+                for (let i = 0; i < conns.length; i++) {
+                    let c = conns[i];
+                    this._collapseConnection(c, index, actualGroup);
+                }
+            };
+
+            // setup proxies for sources and targets
+            _collapseSet(actualGroup.connections.source, 0);
+            _collapseSet(actualGroup.connections.target, 1);
+        }
+
+        actualGroup.collapsed = true;
+        this.instance.removeClass(groupEl, Constants.GROUP_EXPANDED_CLASS);
+        this.instance.addClass(groupEl, Constants.GROUP_COLLAPSED_CLASS);
+        this.instance.revalidate(groupEl);
+        this.instance.fire(Constants.EVENT_COLLAPSE, { group:actualGroup  });
+    }
+
+    expandGroup(group:string | Group<E>, doNotFireEvent?:boolean) {
+
+        let actualGroup = this.getGroup(group);
+
+        if (actualGroup == null || !actualGroup.collapsed) {
+            return;
+        }
+        var groupEl = actualGroup.el;
+
+        this._setGroupVisible(actualGroup, true);
+
+        if (actualGroup.proxied) {
+            // collapses all connections in a group.
+            var _expandSet = (conns:Array<Connection<E>>, index:number) => {
+                for (let i = 0; i < conns.length; i++) {
+                    let c = conns[i];
+                    this._expandConnection(c, index, actualGroup);
+                }
+            };
+
+            // setup proxies for sources and targets
+            _expandSet(actualGroup.connections.source, 0);
+            _expandSet(actualGroup.connections.target, 1);
+        }
+
+        actualGroup.collapsed = false;
+        this.instance.addClass(groupEl, Constants.GROUP_EXPANDED_CLASS);
+        this.instance.removeClass(groupEl, Constants.GROUP_COLLAPSED_CLASS);
+        this.instance.revalidate(groupEl);
+        this.repaintGroup(actualGroup);
+        if (!doNotFireEvent) {
+            this.instance.fire(Constants.EVENT_EXPAND, { group: group});
+        }
+    }
+
+    repaintGroup (group:string|Group<E>) {
+        let actualGroup = this.getGroup(group);
+        const m = actualGroup.children;
+        for (let i = 0; i < m.length; i++) {
+            this.instance.revalidate(m[i]);
+        }
+    }
+
+    addToGroup(group:string | Group<E>, el:E | Array<E>, doNotFireEvent?:boolean) {
+        let actualGroup = this.getGroup(group);
+        if (actualGroup) {
+            let groupEl = actualGroup.el;
+
+            const _one = (el:E) => {
+                if (el[Constants.GROUP_KEY] != null) {
+                    console.log("the thing being added is a group! is it possible to support nested groups")
+                }
+
+                let currentGroup = el[Constants.IS_GROUP_KEY];
+                // if already a member of this group, do nothing
+                if (currentGroup !== actualGroup) {
+                    let elpos = this.instance.getOffset(el, true);
+                    let cpos = actualGroup.collapsed ? this.instance.getOffset(groupEl, true) : this.instance.getOffset(actualGroup.getDragArea(), true);
+
+                    // otherwise, transfer to this group.
+                    if (currentGroup != null) {
+                        currentGroup.remove(el, false, doNotFireEvent, false, actualGroup);
+                        this._updateConnectionsForGroup(currentGroup);
+                    }
+                    actualGroup.add(el, doNotFireEvent/*, currentGroup*/);
+
+                    const handleDroppedConnections = (list:ConnectionSelection<E>, index:number) => {
+                        let oidx = index === 0 ? 1 : 0;
+                        list.each( (c:Connection<E>) => {
+                            c.setVisible(false);
+                            if (c.endpoints[oidx].element[Constants.GROUP_KEY] === actualGroup) {
+                                c.endpoints[oidx].setVisible(false);
+                                this._expandConnection(c, oidx, actualGroup);
+                            }
+                            else {
+                                c.endpoints[index].setVisible(false);
+                                this._collapseConnection(c, index, actualGroup);
+                            }
+                        });
+                    };
+
+                    if (actualGroup.collapsed) {
+                        handleDroppedConnections(this.instance.select({source: el}), 0);
+                        handleDroppedConnections(this.instance.select({target: el}), 1);
+                    }
+
+                    var elId = this.instance.getId(el);
+                    //this.instance.dragManager.setParent(el, elId, groupEl, this.instance.getId(groupEl), elpos);
+
+                    var newPosition = { left: elpos.left - cpos.left, top: elpos.top - cpos.top };
+
+                    this.instance.setPosition(el, newPosition);
+
+                    //this.instance.dragManager.revalidateParent(el, elId, elpos);
+
+                    this._updateConnectionsForGroup(actualGroup);
+
+                    this.instance.revalidate(elId);
+
+                    if (!doNotFireEvent) {
+                        let p = {group: actualGroup, el: el, pos:newPosition};
+                        if (currentGroup) {
+                            (<any>p).sourceGroup = currentGroup;
+                        }
+                        this.instance.fire(Constants.EVENT_CHILD_ADDED, p);
+                    }
+                }
+            }
+
+
+            this.instance.each(el, _one);
+
+        }
+    }
+
+    removeFromGroup (group:string | Group<E>, el:E, doNotFireEvent?:boolean) {
+        let actualGroup = this.getGroup(group);
+        if (actualGroup) {
+            actualGroup.remove(el, null, doNotFireEvent);
+        }
     }
 
 }
--- ts/group/group.ts
+++ ts/group/group.ts
@@ -1,9 +1,20 @@
+import {jsPlumbInstance, Offset, PointXY} from "../core";
+import {AnchorSpec, Connection, EndpointSpec, GroupManager, log, removeWithFunction, uuid} from "..";
+import * as Constants from "../constants";
+
 export interface GroupOptions {
+    id?:string;
     droppable?:boolean;
     enabled?:boolean;
     orphan?:boolean;
     constrain?:boolean;
-
+    proxied?:boolean;
+    ghost?:boolean;
+    revert?:boolean;
+    prune?:boolean;
+    dropOverride?:boolean;
+    anchor?:AnchorSpec;
+    endpoint?:EndpointSpec;
 }
 
 export class Group<E> {
@@ -12,25 +23,188 @@ export class Group<E> {
 
     el:E;
 
+    collapsed:boolean = false;
+
     droppable:boolean;
     enabled:boolean;
     orphan:boolean;
     constrain:boolean;
+    proxied:boolean;
+    ghost:boolean;
+    revert:boolean;
+    prune:boolean;
+    dropOverride:boolean;
+    anchor:AnchorSpec;
+    endpoint:EndpointSpec;
 
-    constructor(el:E, options:GroupOptions) {
+    connections:{source:Array<Connection<E>>, target:Array<Connection<E>>, internal:Array<Connection<E>>} = {source:[], target:[], internal:[]};
+    groups:Array<Group<E>> = [];
+
+    manager:GroupManager<E>;
+
+    id:string;
+
+    constructor(public instance:jsPlumbInstance<E>, el:E, options:GroupOptions) {
         this.el = el;
+        this.el[Constants.IS_GROUP_KEY] = true;
+        this.revert = options.revert !== false;
         this.droppable = options.droppable !== false;
+        this.ghost = options.ghost === true;
         this.enabled = options.enabled !== false;
-        this.orphan = options.orphan !== false;
-        this.constrain = options.constrain !== false;
-
-
+        this.orphan = options.orphan === true;
+        this.prune = options.prune === true;
+        this.constrain = this.ghost || (options.constrain === true);
+        this.proxied = options.proxied !== false;
+        this.id = options.id || uuid();
+        this.dropOverride = options.dropOverride === true;
+        this.anchor = options.anchor;
+        this.endpoint = options.endpoint;
+        this.anchor = options.anchor;
     }
 
     overrideDrop(el:E, targetGroup:Group<E>):boolean {
-        return false;
+        return this.dropOverride && (this.revert || this.prune || this.orphan);
+    }
+
+    getDragArea():E {
+        let da = this.instance.getSelector(this.el, Constants.SELECTOR_GROUP_CONTAINER);
+        return da && da.length > 0 ? da[0] : this.el;
+    };
+
+    // this function, and getEndpoint below, are stubs for a future setup in which we can choose endpoint
+    // and anchor based upon the connection and the index (source/target) of the endpoint to be proxied.
+    getAnchor (conn:Connection<E>, endpointIndex:number) {
+        return this.anchor || "Continuous";
+    };
+
+    getEndpoint (conn:Connection<E>, endpointIndex:number) {
+        return this.endpoint || [ "Dot", { radius:10 }];
+    }
+
+    add(_el:E, doNotFireEvent?:boolean) {
+        const dragArea = this.getDragArea();
+        this.instance.each(_el, (__el:E) => {
+
+            if (__el[Constants.GROUP_KEY] != null) {
+                if (__el[Constants.GROUP_KEY] === this) {
+                    return;
+                } else {
+                    __el[Constants.GROUP_KEY].remove(__el, true, doNotFireEvent, false);
+                }
+            }
+
+            __el[Constants.GROUP_KEY] = this;
+            this.children.push(__el);
+
+            this.manager.instance.appendElement(__el, dragArea);
+
+            // if (!doNotFireEvent) {
+            //     var p = {group: self, el: __el};
+            //     if (sourceGroup) {
+            //         p.sourceGroup = sourceGroup;
+            //     }
+            //     //_jsPlumb.fire(EVT_CHILD_ADDED, p);
+            // }
+        });
+
+        this.manager._updateConnectionsForGroup(this);
+    }
+
+    remove (el:E | Array<E>, manipulateDOM?:boolean, doNotFireEvent?:boolean, doNotUpdateConnections?:boolean, targetGroup?:Group<E>) {
+
+        this.instance.each(el, (__el:E) => {
+            delete __el[Constants.GROUP_KEY];
+            removeWithFunction(this.children, (e:E) => {
+                return e === __el;
+            });
+
+            if (manipulateDOM) {
+                try { (<any>this.getDragArea()).removeChild(__el); }
+                catch (e) {
+                    log("Could not remove element from Group " + e);
+                }
+            }
+            //this._unbindDragHandlers(__el);
+            if (!doNotFireEvent) {
+                var p = {group: this, el: __el};
+                if (targetGroup) {
+                    (<any>p).targetGroup = targetGroup;
+                }
+                this.manager.instance.fire(Constants.EVENT_CHILD_REMOVED, p);
+            }
+        });
+        if (!doNotUpdateConnections) {
+            this.manager._updateConnectionsForGroup(this);
+        }
+    }
+
+    removeAll(manipulateDOM?:boolean, doNotFireEvent?:boolean) {
+        for (let i = 0, l = this.children.length; i < l; i++) {
+            let el = this.children[0];
+            this.remove(el, manipulateDOM, doNotFireEvent, true);
+            this.manager.instance.remove(el, true);
+        }
+        this.children.length = 0;
+        this.manager._updateConnectionsForGroup(this);
+    }
+
+    private _orphan(_el:E):[string, Offset] {
+        const id = this.manager.instance.getId(_el);
+        let pos = this.manager.instance.getOffset(_el);
+        (<any>_el).parentNode.removeChild(_el);
+
+        this.instance.appendElement(_el); // set back as child of container
+
+        this.instance.setPosition(_el, pos);
+        delete _el[Constants.GROUP_KEY];
+        return [id, pos];
+    }
+
+    orphanAll () {
+        let orphanedPositions = {};
+        for (let i = 0; i < this.children.length; i++) {
+            let newPosition = this._orphan(this.children[i]);
+            orphanedPositions[newPosition[0]] = newPosition[1];
+        }
+        this.children.length = 0;
+
+        return orphanedPositions;
+    }
+
+    private _isInsideParent(_el:E, pos:PointXY):boolean {
+        // TODO this knows about the DOM.
+        var p = (<any>_el).offsetParent,
+            s = this.instance.getSize(p),
+            ss = this.instance.getSize(_el),
+            leftEdge = pos[0],
+            rightEdge = leftEdge + ss[0],
+            topEdge = pos[1],
+            bottomEdge = topEdge + ss[1];
+
+        return rightEdge > 0 && leftEdge < s[0] && bottomEdge > 0 && topEdge < s[1];
+    }
+
+    private _pruneOrOrphan(p:any) {
+        let orphanedPosition = null;
+        if (!this._isInsideParent(p.el, p.pos)) {
+            let group = p.el[Constants.GROUP_KEY];
+            if (this.prune) {
+                this.instance.remove(p.el);
+            } else {
+                orphanedPosition = this._orphan(p.el);
+            }
+
+            group.remove(p.el);
+        }
+
+        return orphanedPosition;
     }
 
 
+
+    // private _revalidate(_el:E) {
+    //     let id = this.instance.getId(_el);
+    //     this.instance.revalidate(_el);
+    // }
 
 }
