603af00d6a8f5761fbcdc99c7dc95e2e521f88ef
sporritt <simon.porritt@gmail.com>
2019-07-09 12:12:27 +0100

    add perimeter anchor

--- ts/factory/anchor-factory.ts
+++ ts/factory/anchor-factory.ts
@@ -6,6 +6,7 @@ import {Anchor} from "../anchor/anchor";
 import {DynamicAnchor} from "../anchor/dynamic-anchor";
 import {IS, isArray, isNumber, isString} from "../util";
 import {ContinuousAnchor} from "../anchor/continuous-anchor";
+import {ShapeFunction} from "..";
 
 export type AnchorOrientationHint = -1 | 0 | 1;
 export type Orientation = [ AnchorOrientationHint, AnchorOrientationHint ];
@@ -187,6 +188,128 @@ anchorMap["AutoDefault"] = function<E>(instance:jsPlumbInstance<E>, params:any):
     let a = new DynamicAnchor(instance, {anchors:DEFAULT_DYNAMIC_ANCHORS.map((da:string) => getNamedAnchor(instance, da, params))});
     a.type = "AutoDefault";
     return a;
+};
+
+// --------------------------- perimeter -----------------------
+
+function _circle (anchorCount:number) {
+    let r = 0.5, step = Math.PI * 2 / anchorCount, current = 0, a = [];
+    for (let i = 0; i < anchorCount; i++) {
+        let x = r + (r * Math.sin(current)),
+            y = r + (r * Math.cos(current));
+        a.push([ x, y, 0, 0 ]);
+        current += step;
+    }
+    return a;
+}
+
+function _path (anchorCount:number, segments:Array<any>):Array<any> {
+    let anchorsPerFace = anchorCount / segments.length, a:Array<any> = [],
+    _computeFace = (x1:number, y1:number, x2:number, y2:number, fractionalLength:number, ox?:number, oy?:number) => {
+        anchorsPerFace = anchorCount * fractionalLength;
+        let dx = (x2 - x1) / anchorsPerFace, dy = (y2 - y1) / anchorsPerFace;
+        for (let i = 0; i < anchorsPerFace; i++) {
+            a.push([
+                x1 + (dx * i),
+                y1 + (dy * i),
+                ox == null ? 0 : ox,
+                oy == null ? 0 : oy
+            ]);
+        }
+    };
+
+    for (let i = 0; i < segments.length; i++) {
+        _computeFace.apply(null, segments[i]);
+    }
+
+    return a;
+}
+
+function _shape (anchorCount:number, faces:Array<any>):Array<any> {
+    let s = [];
+    for (let i = 0; i < faces.length; i++) {
+        s.push([faces[i][0], faces[i][1], faces[i][2], faces[i][3], 1 / faces.length, faces[i][4], faces[i][5]]);
+    }
+    return _path(anchorCount, s);
+}
+
+function _rectangle (anchorCount:number):Array<any> {
+    return _shape(anchorCount, [
+        [ 0, 0, 1, 0, 0, -1 ],
+        [ 1, 0, 1, 1, 1, 0 ],
+        [ 1, 1, 0, 1, 0, 1 ],
+        [ 0, 1, 0, 0, -1, 0 ]
+    ]);
+}
+
+function _rotate (points:Array<any>, amountInDegrees:number):Array<any> {
+    let o = [], theta = amountInDegrees / 180 * Math.PI;
+    for (let i = 0; i < points.length; i++) {
+        let _x = points[i][0] - 0.5,
+            _y = points[i][1] - 0.5;
+
+        o.push([
+            0.5 + ((_x * Math.cos(theta)) - (_y * Math.sin(theta))),
+            0.5 + ((_x * Math.sin(theta)) + (_y * Math.cos(theta))),
+            points[i][2],
+            points[i][3]
+        ]);
+    }
+    return o;
+}
+
+export type ShapeFunction = (anchorCount:number, p?:any) => Array<any>;
+
+const _shapes:Dictionary<ShapeFunction> = {
+    "Circle": _circle,
+    "Ellipse": _circle,
+    "Diamond": (anchorCount:number) => {
+        return _shape(anchorCount, [
+            [ 0.5, 0, 1, 0.5 ],
+            [ 1, 0.5, 0.5, 1 ],
+            [ 0.5, 1, 0, 0.5 ],
+            [ 0, 0.5, 0.5, 0 ]
+        ]);
+    },
+    "Rectangle": _rectangle,
+    "Square": _rectangle,
+    "Triangle":  (anchorCount:number) => {
+        return _shape(anchorCount,[
+            [ 0.5, 0, 1, 1 ],
+            [ 1, 1, 0, 1 ],
+            [ 0, 1, 0.5, 0]
+        ]);
+    },
+    "Path": (anchorCount:number, params:any) => {
+        let points:Array<any> = params.points, p = [], tl = 0;
+        for (let i = 0; i < points.length - 1; i++) {
+            let l = Math.sqrt(Math.pow(points[i][2] - points[i][0], 2) + Math.pow(points[i][3] - points[i][1], 2));
+            tl += l;
+            p.push([points[i][0], points[i][1], points[i + 1][0], points[i + 1][1], l]);
+        }
+        for (let j = 0; j < p.length; j++) {
+            p[j][4] = p[j][4] / tl;
+        }
+        return _path(anchorCount, p);
+    }
+};
+
+anchorMap["Perimeter"] = function<E>(instance:jsPlumbInstance<E>, params:any):Anchor<E> {
+    let anchorCount = params.anchorCount || 60;
+
+    if (!params.shape) {
+        throw new Error("no shape supplied to Perimeter Anchor type");
+    }
+
+    if (!_shapes[params.shape]) {
+        throw new Error("Shape [" + params.shape + "] is unknown by Perimeter Anchor type");
+    }
+
+    let da = _shapes[params.shape](anchorCount, params);
+    if (params.rotation) {
+        da = _rotate(da, params.rotation);
+    }
+    return new DynamicAnchor(instance, {anchors:da});
 };
 
 // ------------------------- CONTINUOUS ANCHOR -------------------
