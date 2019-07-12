deb763227d19543300e7e96ff7fdbf63dc62b1e1
sporritt <simon.porritt@gmail.com>
2019-07-12 10:36:39 +0100

    pass related Connection to constructor

--- ts/connector/connectors.ts
+++ ts/connector/connectors.ts
@@ -1,16 +1,17 @@
 import {AbstractConnector} from "./abstract-connector";
 import {Constructable, Dictionary, jsPlumbInstance} from "../core";
+import {Connection} from "./connection-impl";
 
 const connectorMap:Dictionary<Constructable<AbstractConnector<any>>> = {};
 
 export const Connectors = {
-    get:(instance:jsPlumbInstance<any>, name:string, params:any):AbstractConnector<any> => {
+    get:(instance:jsPlumbInstance<any>, connection:Connection<any>, name:string, params:any):AbstractConnector<any> => {
 
         let c:Constructable<AbstractConnector<any>> = connectorMap[name];
         if (!c) {
             throw {message:"jsPlumb: unknown connector type '" + name + "'"};
         } else {
-            return new c(instance, params) as AbstractConnector<any>;
+            return new c(instance, connection, params) as AbstractConnector<any>;
         }
     },
 
