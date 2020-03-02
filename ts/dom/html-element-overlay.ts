ab8dd910eb8a522eda51c99586f216cb133b2dc6
Simon Porritt <simon@jsplumbtoolkit.com>
2020-03-02 16:09:29 +0000

    move from dedicated renderer per overlay to a single renderer for all overlays. this class is all static methods now, and will be likely incorporated into BrowserRenderer

--- ts/dom/html-element-overlay.ts
+++ ts/dom/html-element-overlay.ts
@@ -1,187 +1,68 @@
 
-import {LabelOverlayRenderer, OverlayRenderer} from "../overlay/overlay-renderer";
 import {Overlay} from "../overlay/overlay";
 import {jsPlumbInstance, PointArray, PointXY} from "../core";
 import {Component} from "../component/component";
-import {IS} from "../util";
-import {PaintStyle} from "../styles";
-import {LabelOverlay} from "../overlay/label-overlay";
-import {CustomOverlay} from "../overlay/custom-overlay";
 
-export class HTMLElementOverlay implements OverlayRenderer<HTMLElement> {
-
+interface HTMLElementOverlayHolder extends Overlay<HTMLElement> {
     canvas:HTMLElement;
-
     cachedDimensions:PointArray;
+} 
 
-    constructor(public instance:jsPlumbInstance<HTMLElement>, public overlay: Overlay<HTMLElement>) { }
+export class HTMLElementOverlay {
 
-    protected _createElement(component:Component<HTMLElement>): HTMLElement {
-        return this.instance.createElement("div", {}, this.instance.overlayClass + " " +
-            (this.overlay.cssClass ? this.overlay.cssClass : ""));
+    protected htmlElementOverlay:HTMLElementOverlayHolder;
+
+    constructor(public instance:jsPlumbInstance<HTMLElement>, public overlay: Overlay<HTMLElement>) { 
+        this.htmlElementOverlay = overlay as HTMLElementOverlayHolder;
     }
 
-    getElement (component:Component<HTMLElement>):HTMLElement {
-        if (this.canvas == null) {
-            this.canvas = this._createElement(component);
-            this.canvas.style.position = "absolute";
-            this.instance.appendElement(this.canvas);
-            this.instance.getId(this.canvas);
+    static createElement(o:HTMLElementOverlayHolder):HTMLElement {
+        return o.instance.createElement("div", {}, o.instance.overlayClass + " " +
+            (o.cssClass ? o.cssClass : ""));
+    }
 
-    
-            // in IE the top left corner is what it placed at the desired location.  This will not
-            // be fixed. IE8 is not going to be supported for much longer.
+    static getElement (o:HTMLElementOverlayHolder, component?:Component<HTMLElement>, elementCreator?:(c:Component<HTMLElement>) => HTMLElement):HTMLElement {
+        if (o.canvas == null) {
+
+            if (elementCreator && component) {
+                o.canvas = elementCreator(component);
+            } else {
+                o.canvas = HTMLElementOverlay.createElement(o);
+            }
+
+            o.canvas.style.position = "absolute";
+            o.instance.appendElement(o.canvas);
+            o.instance.getId(o.canvas);
+            
             let ts = "translate(-50%, -50%)";
-            (<any>this.canvas.style).webkitTransform = ts;
-            (<any>this.canvas.style).mozTransform = ts;
-            (<any>this.canvas.style).msTransform = ts;
-            (<any>this.canvas.style).oTransform = ts;
-            (<any>this.canvas.style).transform = ts;
+            (<any>o.canvas.style).webkitTransform = ts;
+            (<any>o.canvas.style).mozTransform = ts;
+            (<any>o.canvas.style).msTransform = ts;
+            (<any>o.canvas.style).oTransform = ts;
+            (<any>o.canvas.style).transform = ts;
     
-            // write the related component into the created element
-            //div._jsPlumb = this;
-    
-            if (!this.overlay.isVisible()) {
-                this.canvas.style.display = "none";
+            if (!o.isVisible()) {
+                o.canvas.style.display = "none";
             }
 
-            (<any>this.canvas).jtk = { overlay:this.overlay };
+            (<any>o.canvas).jtk = { overlay:o };
 
         }
 
-        return this.canvas;
+        return o.canvas;
     }
 
-    private _getDimensions(forceRefresh?:boolean):PointArray {
-        if (this.cachedDimensions == null || forceRefresh) {
-            this.cachedDimensions = this.getDimensions();
+    static destroy(o:HTMLElementOverlayHolder) {
+        const el = HTMLElementOverlay.getElement(o as any);
+        el.parentNode.removeChild(el);
+        delete o.canvas;
+        delete o.cachedDimensions;
+    }
+
+    static _getDimensions(o:HTMLElementOverlayHolder, forceRefresh?:boolean):PointArray {
+        if (o.cachedDimensions == null || forceRefresh) {
+            o.cachedDimensions = [1,1];
         }
-        return this.cachedDimensions;
-    }
-
-    draw(component:Component<HTMLElement>, currentConnectionPaintStyle:PaintStyle, absolutePosition?:PointArray) {
-        let td = this._getDimensions();
-        if (td != null && td.length === 2) {
-            let cxy = { x: 0, y: 0 };
-
-            // absolutePosition would have been set by a call to connection.setAbsoluteOverlayPosition.
-            if (absolutePosition) {
-                cxy = { x: absolutePosition[0], y: absolutePosition[1] };
-            }
-            else if ((<any>component).pointOnPath != null) {
-                let loc = this.overlay.location, absolute = false;
-                if (IS.aString(this.overlay.location) || this.overlay.location < 0 || this.overlay.location > 1) {
-                    loc = parseInt("" + this.overlay.location, 10);
-                    absolute = true;
-                }
-                cxy = (<any>component).pointOnPath(loc as number, absolute);  // a connection
-            }
-            else {
-                let locToUse:[number, number] = this.overlay.location.constructor === Array ? ((<unknown>this.overlay.location) as [number, number]) : this.overlay.endpointLocation;
-                cxy = { x: locToUse[0] * component.w,
-                    y: locToUse[1] * component.h };
-            }
-
-            let minx = cxy.x - (td[0] / 2),
-                miny = cxy.y - (td[1] / 2);
-
-            return {
-                component: this.overlay,
-                d: { minx: minx, miny: miny, td: td, cxy: cxy },
-                minX: minx,
-                maxX: minx + td[0],
-                minY: miny,
-                maxY: miny + td[1]
-            };
-        }
-        else {
-            return {minX: 0, maxX: 0, minY: 0, maxY: 0};
-        }
-    }
-
-    setVisible(v: boolean): void {
-        this.getElement(this.overlay.component).style.display = v ? "block" : "none";
-    }
-
-    destroy(force?: boolean): void {
-        let el = this.getElement(this.overlay.component);
-        if (el) {
-            el.parentNode.removeChild(el);
-        }
-    }
-
-    setHover(h: boolean): void {
-        let el = this.getElement(this.overlay.component);
-        this.instance[h ? "addClass" : "removeClass"](el, this.instance.hoverClass);
-    }
-
-    getDimensions():[number, number] {
-        return [ 1, 1 ];
-    }
-
-    paint(params: any, extents?: any): void {
-        //console.log("PAINT on HTML overlay called")
-
-        let el = this.getElement(this.overlay.component);
-        //params.component.appendDisplayElement(this.canvas);   probably need this - it helps to know which elements should be hiddne/shown on visibility change
-
-        // if (this.detached) {
-        //     this._jsPlumb.div.parentNode.removeChild(this._jsPlumb.div);
-        // }
-
-        // this.canvas.style.left = (params.component.x + params.d.minx) + "px";
-        // this.canvas.style.top = (params.component.y + params.d.miny) + "px";
-
-        let XY = this.overlay.component.getXY();
-
-        this.canvas.style.left = XY.x +  params.d.minx + "px";  // wont work for endpoint. abstracts
-        this.canvas.style.top = XY.y + params.d.miny + "px";
-    }
-
-
-    addClass(clazz: string) {
-        this.instance.addClass(this.canvas, clazz);
-    }
-
-    removeClass(clazz: string) {
-        this.instance.removeClass(this.canvas, clazz);
-    }
-
-    moveParent(newParent: HTMLElement): void {
-        this.instance.appendElement(this.canvas);
-    }
-
-
-}
-
-
-export class HTMLLabelElementOverlay extends HTMLElementOverlay implements LabelOverlayRenderer<HTMLElement> {
-
-    constructor(public instance:jsPlumbInstance<HTMLElement>, public overlay: LabelOverlay<HTMLElement>) {
-
-        super(instance, overlay);
-
-        this.setText(overlay.getLabel());
-
-    }
-
-    setText(t: string): void {
-        this.getElement(this.overlay.component).innerHTML = t;
-    }
-
-}
-
-export class HTMLCustomElementOverlay extends HTMLElementOverlay implements OverlayRenderer<HTMLElement> {
-
-    constructor(public instance:jsPlumbInstance<HTMLElement>, public overlay: CustomOverlay<HTMLElement>) {
-
-        super(instance, overlay);
-    }
-
-
-    _createElement(component:Component<HTMLElement>): HTMLElement {
-        const el = this.overlay.create(component);
-        el.classList.add("jtk-overlay");
-        return el;
+        return o.cachedDimensions;
     }
 }
