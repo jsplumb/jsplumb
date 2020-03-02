e79a5c336e162d9675025e063209b09ac2daab4c
Simon Porritt <simon@jsplumbtoolkit.com>
2020-03-02 16:12:29 +0000

    move from dedicated renderer per overlay to a single renderer for all overlays. this class is all static methods now, and will be likely incorporated into BrowserRenderer

--- ts/overlay/custom-overlay.ts
+++ ts/overlay/custom-overlay.ts
@@ -1,35 +1,21 @@
 import {CustomOverlayOptions, Overlay} from "./overlay";
-import {Dictionary, jsPlumbInstance} from "../core";
-import {Component, Connection, OverlayFactory, PaintStyle, SegmentBounds} from "..";
-
+import { jsPlumbInstance } from "../core";
+import {Component, OverlayFactory } from "..";
 
 export class CustomOverlay<E> extends Overlay<E> {
 
     create:(c:Component<E>) => E;
 
-    constructor(protected instance:jsPlumbInstance<E>, public component:Component<E>,
+    constructor(public instance:jsPlumbInstance<E>, public component:Component<E>,
                 p:CustomOverlayOptions<E>) {
 
         super(instance, component, p);
-
         this.create = p.create;
-        this.setRenderer(this.instance.renderer.assignOverlayRenderer(this.instance, this));
-
     }
-
-    events: Dictionary<Function>;
 
     type:string = "Custom";
 
-    draw(component: any, paintStyle: PaintStyle, absolutePosition?: any): SegmentBounds {
-        return this.getRenderer().draw(component, paintStyle, absolutePosition);
-    }
-
-    updateFrom(d: any): void {
-
-    }
-
-
+    updateFrom(d: any): void { }
 
 }
 
