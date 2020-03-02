004eb3d52a1a884396c5aabad7d514d49955e667
Simon Porritt <simon@jsplumbtoolkit.com>
2020-03-02 16:08:25 +0000

    move from dedicated renderer per overlay to a single renderer for all overlays

--- ts/overlay/label-overlay.ts
+++ ts/overlay/label-overlay.ts
@@ -2,11 +2,8 @@
 import {LabelOverlayOptions, Overlay} from "./overlay";
 import {isFunction} from "../util";
 import {Component} from "../component/component";
-import {PaintStyle} from "../styles";
 import {jsPlumbInstance, PointArray} from "../core";
-import {LabelOverlayRenderer} from "./overlay-renderer";
 import {OverlayFactory} from "../factory/overlay-factory";
-import * as Constants from "../constants";
 
 export class LabelOverlay<E> extends Overlay<E> {
 
@@ -17,12 +14,11 @@ export class LabelOverlay<E> extends Overlay<E> {
 
     cachedDimensions:PointArray;
 
-    constructor(protected instance:jsPlumbInstance<E>, public component:Component<E>,
+    constructor(public instance:jsPlumbInstance<E>, public component:Component<E>,
                 p:LabelOverlayOptions) {
 
         super(instance, component, p);
         p = p || { label:""};
-        this.setRenderer(this.instance.renderer.assignOverlayRenderer(this.instance, this));
         this.setLabel(p.label);
     }
 
@@ -37,48 +33,15 @@ export class LabelOverlay<E> extends Overlay<E> {
     setLabel(l: string | Function): void {
         this.label = l;
         this.labelText = null;
-        this.update();
-    }
-
-    private _getDimensions(forceRefresh?:boolean):PointArray {
-        if (this.cachedDimensions == null || forceRefresh) {
-            this.cachedDimensions = this.getDimensions();
-        }
-        return this.cachedDimensions;
+        this.instance.renderer.updateLabel(this);
     }
 
     getDimensions():PointArray { return [1,1];}
 
-    draw(component:Component<HTMLElement>, paintStyle:PaintStyle, absolutePosition?:PointArray): any {
-        return this.getRenderer().draw(component, paintStyle, absolutePosition);
-    }
 
     updateFrom(d: any): void {
         if(d.label != null){
             this.setLabel(d.label);
-        }
-    }
-
-    update():void {
-        if (isFunction(this.label)) {
-            let lt = (this.label as Function)(this);
-            if (lt != null) {
-                (this.getRenderer() as LabelOverlayRenderer<E>).setText(lt.replace(/\r\n/g, "<br/>"));
-            } else {
-                (this.getRenderer() as LabelOverlayRenderer<E>).setText("");
-            }
-
-        }
-        else {
-            if (this.labelText == null) {
-                this.labelText = this.label as string;
-                if (this.labelText != null) {
-                    (this.getRenderer() as LabelOverlayRenderer<E>).setText(this.labelText.replace(/\r\n/g, "<br/>"));
-                } else {
-                    (this.getRenderer() as LabelOverlayRenderer<E>).setText("");
-                }
-
-            }
         }
     }
 }
