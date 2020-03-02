9ca3bcd5dfde48f51bc1693c02c32a94a7f5abb3
Simon Porritt <simon@jsplumbtoolkit.com>
2020-03-02 16:17:11 +0000

    move from dedicated renderer per overlay to a single renderer for all overlays (WIP)

--- ts/overlay/arrow-overlay.ts
+++ ts/overlay/arrow-overlay.ts
@@ -18,7 +18,7 @@ export class ArrowOverlay<E> extends Overlay<E> {
 
     cachedDimensions:PointArray;
 
-    constructor(protected instance:jsPlumbInstance<E>, public component:Component<E>,
+    constructor(public instance:jsPlumbInstance<E>, public component:Component<E>,
                 p:ArrowOverlayOptions) {
 
         super(instance, component, p);
@@ -29,8 +29,6 @@ export class ArrowOverlay<E> extends Overlay<E> {
         this.direction = (p.direction || 1) < 0 ? -1 : 1;
         this.foldback = p.foldback || 0.623;
         this.paintStyle = p.paintStyle || { "strokeWidth": 1 };
-
-        this.setRenderer(this.instance.renderer.assignOverlayRenderer(this.instance, this));
     }
 
 
@@ -40,60 +38,57 @@ export class ArrowOverlay<E> extends Overlay<E> {
 
             let connector = component as AbstractConnector<E>;
 
-            var hxy, mid, txy, tail, cxy;
+            let hxy, mid, txy, tail, cxy;
 
-                if (this.location > 1 || this.location < 0) {
-                    let fromLoc = this.location < 0 ? 1 : 0;
-                    hxy = connector.pointAlongPathFrom(fromLoc, this.location, false);
-                    mid = connector.pointAlongPathFrom(fromLoc, this.location - (this.direction * this.length / 2), false);
-                    txy = pointOnLine(hxy, mid, this.length);
-                } else if (this.location === 1) {
-                    hxy = connector.pointOnPath(this.location);
-                    mid = connector.pointAlongPathFrom(this.location, -(this.length));
-                    txy = pointOnLine(hxy, mid, this.length);
+            if (this.location > 1 || this.location < 0) {
+                let fromLoc = this.location < 0 ? 1 : 0;
+                hxy = connector.pointAlongPathFrom(fromLoc, this.location, false);
+                mid = connector.pointAlongPathFrom(fromLoc, this.location - (this.direction * this.length / 2), false);
+                txy = pointOnLine(hxy, mid, this.length);
+            } else if (this.location === 1) {
+                hxy = connector.pointOnPath(this.location);
+                mid = connector.pointAlongPathFrom(this.location, -(this.length));
+                txy = pointOnLine(hxy, mid, this.length);
 
-                    if (this.direction === -1) {
-                        var _ = txy;
-                        txy = hxy;
-                        hxy = _;
-                    }
-                } else if (this.location === 0) {
-                    txy = connector.pointOnPath(this.location);
-                    mid = connector.pointAlongPathFrom(this.location, this.length);
-                    hxy = pointOnLine(txy, mid, this.length);
-                    if (this.direction === -1) {
-                        var __ = txy;
-                        txy = hxy;
-                        hxy = __;
-                    }
-                } else {
-                    hxy = connector.pointAlongPathFrom(this.location, this.direction * this.length / 2);
-                    mid = connector.pointOnPath(this.location);
-                    txy = pointOnLine(hxy, mid, this.length);
+                if (this.direction === -1) {
+                    var _ = txy;
+                    txy = hxy;
+                    hxy = _;
                 }
+            } else if (this.location === 0) {
+                txy = connector.pointOnPath(this.location);
+                mid = connector.pointAlongPathFrom(this.location, this.length);
+                hxy = pointOnLine(txy, mid, this.length);
+                if (this.direction === -1) {
+                    let __ = txy;
+                    txy = hxy;
+                    hxy = __;
+                }
+            } else {
+                hxy = connector.pointAlongPathFrom(this.location, this.direction * this.length / 2);
+                mid = connector.pointOnPath(this.location);
+                txy = pointOnLine(hxy, mid, this.length);
+            }
 
-                tail = perpendicularLineTo(hxy, txy, this.width);
-                cxy = pointOnLine(hxy, txy, this.foldback * this.length);
+            tail = perpendicularLineTo(hxy, txy, this.width);
+            cxy = pointOnLine(hxy, txy, this.foldback * this.length);
 
-                var d = {hxy: hxy, tail: tail, cxy: cxy},
-                    stroke = this.paintStyle.stroke || currentConnectionPaintStyle.stroke,
-                    fill = this.paintStyle.fill || currentConnectionPaintStyle.stroke,
-                    lineWidth = this.paintStyle.strokeWidth || currentConnectionPaintStyle.strokeWidth;
+            let d = {hxy: hxy, tail: tail, cxy: cxy},
+                stroke = this.paintStyle.stroke || currentConnectionPaintStyle.stroke,
+                fill = this.paintStyle.fill || currentConnectionPaintStyle.stroke,
+                lineWidth = this.paintStyle.strokeWidth || currentConnectionPaintStyle.strokeWidth;
 
-                return {
-                    component: component,
-                    d: d,
-                    "stroke-width": lineWidth,
-                    stroke: stroke,
-                    fill: fill,
-                    minX: Math.min(hxy.x, tail[0].x, tail[1].x),
-                    maxX: Math.max(hxy.x, tail[0].x, tail[1].x),
-                    minY: Math.min(hxy.y, tail[0].y, tail[1].y),
-                    maxY: Math.max(hxy.y, tail[0].y, tail[1].y)
-                };
-            // } else {
-            //     return {component: component, minX: 0, maxX: 0, minY: 0, maxY: 0};
-            // }
+            return {
+                component: component,
+                d: d,
+                "stroke-width": lineWidth,
+                stroke: stroke,
+                fill: fill,
+                minX: Math.min(hxy.x, tail[0].x, tail[1].x),
+                maxX: Math.max(hxy.x, tail[0].x, tail[1].x),
+                minY: Math.min(hxy.y, tail[0].y, tail[1].y),
+                maxY: Math.max(hxy.y, tail[0].y, tail[1].y)
+            };
         }
     }
 
