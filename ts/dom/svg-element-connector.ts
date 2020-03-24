e2ae4d31446f5228d9640028cdf27a807d3c22b3
Simon Porritt <simon@jsplumbtoolkit.com>
2020-03-24 10:46:02 +0000

    this class consists of static methods only now, as there isnt a one renderer per endpoint setup anymore

--- ts/dom/svg-element-connector.ts
+++ ts/dom/svg-element-connector.ts
@@ -1,29 +1,73 @@
-import {extend, jsPlumbInstance, TypeDescriptor} from "../core";
-import {ConnectorRenderer} from "../connector/connector-renderer";
+import {extend} from "../core";
 import {PaintStyle} from "../styles";
-import {SvgComponent} from "../svg/svg-component";
+import {SvgComponent} from "../dom/svg-component";
 import {AbstractConnector} from "../connector/abstract-connector";
 import {_appendAtIndex, _applyStyles, _attr, _node} from "../svg/svg-util";
 
 /**
  * Renderer for a connector that uses an `svg` element in the DOM.
  */
-export class SvgElementConnector extends SvgComponent implements ConnectorRenderer<HTMLElement> {
+export class SvgElementConnector {
 
-    bgPath:SVGElement;
-    path:SVGElement;
+    static paint(connector:AbstractConnector<HTMLElement>, paintStyle:PaintStyle, extents?:any) {
 
-    constructor(public instance:jsPlumbInstance<HTMLElement>,
-                public connector:AbstractConnector<HTMLElement>) {
+        this.getConnectorElement(connector);
 
-        super(instance, connector, null);
-        if (connector.cssClass != null) {
-            instance.addClass(this.canvas, connector.cssClass);
+        SvgComponent.paint(connector, false, paintStyle, extents);
+
+        let segments = connector.segments;
+
+        let p = "", offset = [0, 0];
+        if (extents.xmin < 0) {
+            offset[0] = -extents.xmin;
         }
-        instance.addClass(this.canvas, instance.connectorClass);
+        if (extents.ymin < 0) {
+            offset[1] = -extents.ymin;
+        }
 
-        (<any>this.canvas).jtk = (<any>this.canvas).jtk || { };
-        (<any>this.canvas).jtk.connector = connector;
+        if (segments.length > 0) {
+
+            p = connector.getPathData();
+
+            let a = {
+                    d: p,
+                    transform: "translate(" + offset[0] + "," + offset[1] + ")",
+                    "pointer-events": "visibleStroke"
+                },
+                outlineStyle:PaintStyle = null,
+                d = [connector.x, connector.y, connector.w, connector.h];
+
+            // outline style.  actually means drawing an svg object underneath the main one.
+            if (paintStyle.outlineStroke) {
+                let outlineWidth = paintStyle.outlineWidth || 1,
+                    outlineStrokeWidth = paintStyle.strokeWidth + (2 * outlineWidth);
+                outlineStyle = extend({}, paintStyle);
+                delete outlineStyle.gradient;
+                outlineStyle.stroke = paintStyle.outlineStroke;
+                outlineStyle.strokeWidth = outlineStrokeWidth;
+
+                if ((connector as any).bgPath == null) {
+                    (connector as any).bgPath = _node(connector.instance, "path", a);
+                    connector.instance.addClass((connector as any).bgPath as any, connector.instance.connectorOutlineClass);
+                    _appendAtIndex((connector as any).canvas, (connector as any).bgPath, 0);
+                }
+                else {
+                    _attr((connector as any).bgPath, a);
+                }
+
+                _applyStyles((connector as any).canvas, (connector as any).bgPath, outlineStyle, d, null);
+            }
+
+            if ((connector as any).path == null) {
+                (connector as any).path = _node(connector.instance, "path", a);
+                _appendAtIndex((connector as any).canvas, (connector as any).path, paintStyle.outlineStroke ? 1 : 0);
+            }
+            else {
+                _attr((connector as any).path, a);
+            }
+
+            _applyStyles((connector as any).canvas, (connector as any).path, paintStyle, d, <any>this);
+        }
     }
 
     static getConnectorElement(c:AbstractConnector<HTMLElement>):HTMLElement {
@@ -40,6 +84,8 @@ export class SvgElementConnector extends SvgComponent implements ConnectorRender
             (c as any).canvas = svg;
             c.instance.appendElement((c as any).canvas, c.instance.getContainer());
 
+            // TODO BG CANVAS! does it even need to be a canvas? i suppose not.
+
             if (c.cssClass != null) {
                 c.instance.addClass(svg, c.cssClass);
             }
@@ -52,71 +98,9 @@ export class SvgElementConnector extends SvgComponent implements ConnectorRender
         }
     }
 
-    paint(paintStyle: PaintStyle, extents?:any): void {
-
-        super.paint(paintStyle, extents);
-
-        let segments = this.connector.segments;
-
-        let p = "", offset = [0, 0];
-        if (extents.xmin < 0) {
-            offset[0] = -extents.xmin;
-        }
-        if (extents.ymin < 0) {
-            offset[1] = -extents.ymin;
-        }
-
-        if (segments.length > 0) {
-
-            p = this.connector.getPathData();
-
-            let a = {
-                    d: p,
-                    transform: "translate(" + offset[0] + "," + offset[1] + ")",
-                    "pointer-events": "visibleStroke"
-                },
-                outlineStyle:PaintStyle = null,
-                d = [this.connector.x, this.connector.y, this.connector.w, this.connector.h];
-
-            // outline style.  actually means drawing an svg object underneath the main one.
-            if (paintStyle.outlineStroke) {
-                let outlineWidth = paintStyle.outlineWidth || 1,
-                    outlineStrokeWidth = paintStyle.strokeWidth + (2 * outlineWidth);
-                outlineStyle = extend({}, paintStyle);
-                delete outlineStyle.gradient;
-                outlineStyle.stroke = paintStyle.outlineStroke;
-                outlineStyle.strokeWidth = outlineStrokeWidth;
-
-                if (this.bgPath == null) {
-                    this.bgPath = _node(this.instance, "path", a);
-                    this.instance.addClass(this.bgPath as any, this.instance.connectorOutlineClass);
-                    _appendAtIndex(this.svg, this.bgPath, 0);
-                }
-                else {
-                    _attr(this.bgPath, a);
-                }
-
-                _applyStyles(this.svg, this.bgPath, outlineStyle, d, null);
-            }
-
-            if (this.path == null) {
-                this.path = _node(this.instance, "path", a);
-                _appendAtIndex(this.svg, this.path, paintStyle.outlineStroke ? 1 : 0);
-            }
-            else {
-                _attr(this.path, a);
-            }
-
-            _applyStyles(this.svg, this.path, paintStyle, d, <any>this);
-        }
-
-    }
-
-    applyType(t: TypeDescriptor): void {
-        if (t.cssClass != null && this.svg) {
-            this.instance.addClass(<any>this.svg, t.cssClass);
-        }
-    }
-
-
+    // applyType(t: TypeDescriptor): void {
+    //     if (t.cssClass != null && this.canvas) {
+    //         this.instance.addClass(<any>this.canvas, t.cssClass);
+    //     }
+    // }
 }
