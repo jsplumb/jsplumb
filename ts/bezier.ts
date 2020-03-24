07d72169cc2193256e48166950819c3856d73d5f
Simon Porritt <simon@jsplumbtoolkit.com>
2020-03-24 11:54:39 +0000

    add a note about possible speed enhancements

--- ts/bezier.ts
+++ ts/bezier.ts
@@ -431,6 +431,9 @@ export function isPoint (curve:Curve):boolean {
  * finds the point that is 'distance' along the path from 'location'.  this method returns both the x,y location of the point and also
  * its 'location' (proportion of travel along the path); the method below - _pointAlongPathFrom - calls this method and just returns the
  * point.
+ *
+ * TODO The compute length functionality was made much faster recently, using a lookup table. is it possible to use that lookup table find
+ * a value for the point some distance along the curve from somewhere?
  */
 export function pointAlongPath (curve:Curve, location:number, distance:number):PointOnPath {
 
