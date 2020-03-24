595bb0a3fb4f153f54d0332689e848f25b1ee611
Simon Porritt <simon@jsplumbtoolkit.com>
2020-03-24 10:46:14 +0000

    removed gradients

--- ts/svg/svg-util.ts
+++ ts/svg/svg-util.ts
@@ -43,63 +43,10 @@ export function _pos (d:[number, number]):string {
     return "position:absolute;left:" + d[0] + "px;top:" + d[1] + "px";
 }
 
-export function _clearGradient (parent:any) {
-    let els = parent.querySelectorAll(" defs,linearGradient,radialGradient");
-    for (let i = 0; i < els.length; i++) {
-        els[i].parentNode.removeChild(els[i]);
-    }
-}
-
-export function _updateGradient<E>(parent:any, node:any, style:any, dimensions:any, uiComponent:Component<E>) {
-    let id = JSPLUMB_GRADIENT + (<any>uiComponent).instance._idstamp();
-    // first clear out any existing gradient
-    _clearGradient(parent);
-    // this checks for an 'offset' property in the gradient, and in the absence of it, assumes
-    // we want a linear gradient. if it's there, we create a radial gradient.
-    // it is possible that a more explicit means of defining the gradient type would be
-    // better. relying on 'offset' means that we can never have a radial gradient that uses
-    // some default offset, for instance.
-    // issue 244 suggested the 'gradientUnits' attribute; without this, straight/flowchart connectors with gradients would
-    // not show gradients when the line was perfectly horizontal or vertical.
-    let g;
-    if (!style.gradient.offset) {
-        g = _node((<any>uiComponent).instance, LINEAR_GRADIENT, {id: id, gradientUnits: "userSpaceOnUse"});
-    }
-    else {
-        g = _node((<any>uiComponent).instance, RADIAL_GRADIENT, { id: id });
-    }
-
-    let defs = _node((<any>uiComponent).instance, DEFS);
-    parent.appendChild(defs);
-    defs.appendChild(g);
-
-    // the svg radial gradient seems to treat stops in the reverse
-    // order to how canvas does it.  so we want to keep all the maths the same, but
-    // iterate the actual style declarations in reverse order, if the x indexes are not in order.
-    for (let i = 0; i < style.gradient.stops.length; i++) {
-        let styleToUse = uiComponent.segment === 1 || uiComponent.segment === 2 ? i : style.gradient.stops.length - 1 - i,
-            stopColor = style.gradient.stops[styleToUse][1],
-            s = _node((<any>uiComponent).instance, STOP, {"offset": Math.floor(style.gradient.stops[i][0] * 100) + "%", "stop-color": stopColor});
-
-        g.appendChild(s);
-    }
-    let applyGradientTo = style.stroke ? STROKE : FILL;
-    node.setAttribute(applyGradientTo, "url(#" + id + ")");
-}
-
 export function _applyStyles<E>(parent:any, node:SVGElement, style:any, dimensions:any, uiComponent:Component<E>) {
 
     node.setAttribute(FILL, style.fill ? style.fill : Constants.NONE);
     node.setAttribute(STROKE, style.stroke ? style.stroke : Constants.NONE);
-
-    if (style.gradient) {
-        _updateGradient(parent, node, style, dimensions, uiComponent);
-    }
-    else {
-        // make sure we clear any existing gradient
-        _clearGradient(parent);
-        node.setAttribute(STYLE, "");
-    }
 
     if (style.strokeWidth) {
         node.setAttribute(STROKE_WIDTH, style.strokeWidth);
