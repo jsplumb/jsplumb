59450cf0450c31de023d4afa6756d7171ae2efc1
Simon Porritt <simon@jsplumbtoolkit.com>
2020-03-24 10:40:57 +0000

    switch from class based renderer to object with functions

--- ts/dom/dot-endpoint-renderer.ts
+++ ts/dom/dot-endpoint-renderer.ts
@@ -1,36 +1,22 @@
 import {registerEndpointRenderer} from "./browser-renderer";
 import {_attr, _node} from "../svg/svg-util";
 import {PaintStyle} from "../styles";
-import {SvgComponentOptions} from "../svg/svg-component";
-import {ComputedDotEndpoint, DotEndpoint} from "../endpoint/dot-endpoint";
 import {jsPlumbInstance} from "../core";
-import {SvgEndpoint} from "./svg-element-endpoint";
-import {Endpoint} from "../endpoint/endpoint-impl";
 
-/**
- * SVG DOM element Dot endpoint renderer.
- */
-class SvgElementDotEndpointRenderer extends SvgEndpoint<ComputedDotEndpoint> {
-
-    constructor(protected endpoint:Endpoint<HTMLElement>, public ep:DotEndpoint<HTMLElement>, options?:SvgComponentOptions) {
-        super(endpoint, ep, options);
-    }
-
-    makeNode (style:PaintStyle) {
-        return _node(this.instance, "circle", {
-            "cx": this.ep.w / 2,
-            "cy": this.ep.h / 2,
-            "r": this.ep.radius
+registerEndpointRenderer("Dot", {
+    makeNode : (instance:jsPlumbInstance<any>, ep:any, style:PaintStyle) => {
+        return _node(instance, "circle", {
+            "cx": ep.w / 2,
+            "cy": ep.h / 2,
+            "r": ep.radius
         });
-    }
+    },
 
-    updateNode (node:SVGElement) {
+    updateNode : (ep:any, node:SVGElement) => {
         _attr(node, {
-            "cx": "" + (this.ep.w / 2),
-            "cy": "" + (this.ep.h / 2),
-            "r": "" + this.ep.radius
+            "cx": "" + (ep.w / 2),
+            "cy": "" + (ep.h / 2),
+            "r": "" + ep.radius
         });
     }
-}
-
-registerEndpointRenderer("Dot", SvgElementDotEndpointRenderer);
+});
