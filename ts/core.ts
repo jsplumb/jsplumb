22899bc77ec9fc9c5275f87188fc963277a74433
Simon Porritt <simon@jsplumbtoolkit.com>
2020-03-26 14:21:24 +0000

    move responsibility to the browser instance to set the attribute on the container (core doesnt care about it)

--- ts/core.ts
+++ ts/core.ts
@@ -759,8 +759,6 @@ export abstract class jsPlumbInstance<E> extends EventGenerator {
         // set container.
         this._container = _c;
 
-        this.setAttribute(this._container, Constants.ATTRIBUTE_CONTAINER, uuid().replace("-", ""));
-
         this.fire(Constants.EVENT_CONTAINER_CHANGE, this._container);
     }
 
