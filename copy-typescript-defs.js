141b4aa4a6e82c12508ff1170ad80363c36e53ee
Simon Porritt <simon@jsplumbtoolkit.com>
2020-04-16 12:19:16 +0100

    add script to copy typescript def files

---
+++ copy-typescript-defs.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+
+const sourceDir = "./_build_es6";
+const targetDir = "./dist";
+
+function isDirectory(path) {
+    return fs.lstatSync(path).isDirectory()
+}
+
+const _one = (dir, subdir) => {
+
+    const path = subdir == null ? dir : dir + "/" + subdir;
+
+    if (subdir != null) {
+        fs.mkdirSync(targetDir + "/" + subdir);
+    }
+
+    let inputPath;
+
+    const files = fs.readdirSync(path);
+    files.forEach((filename) => {
+        inputPath = path + "/" + filename;
+        if (isDirectory(inputPath)) {
+            _one(dir, (subdir ? subdir + "/" : "") + filename);
+        } else {
+            if (filename.endsWith(".d.ts")) {
+                const s = fs.readFileSync(path + "/" + filename);
+                fs.writeFileSync(targetDir + "/" + (subdir ? subdir + "/" : "") + filename, s);
+            }
+        }
+    })
+
+};
+
+_one(sourceDir, null);
