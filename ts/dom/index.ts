567a3a6db01a476008acdbf36d555512e3106145
Simon Porritt <simon@jsplumbtoolkit.com>
2020-06-03 20:34:48 +0100

    begin gathering DOM stuff in a single ts

---
+++ ts/dom/index.ts
@@ -0,0 +1,6 @@
+export * from './collicat';
+export * from "./browser-renderer";
+export * from "./browser-jsplumb-instance";
+export * from "./drag-manager";
+export * from "./svg-component";
+export * from './event-manager';
