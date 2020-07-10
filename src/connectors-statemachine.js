9827bafe3a3d7b9cb0dc1bfdb9648b97f9a30603
Simon Porritt <simon@jsplumbtoolkit.com>
2020-07-10 07:52:50 +0100

    update copyright date

--- src/connectors-statemachine.js
+++ src/connectors-statemachine.js
@@ -1,7 +1,7 @@
 /*
  * This file contains the state machine connectors, which extend AbstractBezierConnector.
  *
- * Copyright (c) 2010 - 2018 jsPlumb (hello@jsplumbtoolkit.com)
+ * Copyright (c) 2010 - 2020 jsPlumb (hello@jsplumbtoolkit.com)
  *
  * https://jsplumbtoolkit.com
  * https://github.com/jsplumb/jsplumb
