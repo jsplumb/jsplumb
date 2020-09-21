20a2dc06e9640e171aeca15f8ff8747a36ed12c7
Simon Porritt <simon@jsplumbtoolkit.com>
2020-09-21 19:23:41 +1000

    make some members private

--- ts/connector/flowchart-connector.ts
+++ ts/connector/flowchart-connector.ts
@@ -43,7 +43,7 @@ export class FlowchartConnector extends AbstractConnector {
 
     type = "Flowchart"
 
-    internalSegments:Array<FlowchartSegment> = []
+    private internalSegments:Array<FlowchartSegment> = []
     midpoint:number
     alwaysRespectStubs:boolean
     cornerRadius:number
