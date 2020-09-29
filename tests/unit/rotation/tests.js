6223d76cedd2a0a1f535fe633ba6d18072ae0fd0
Simon Porritt <simon@jsplumbtoolkit.com>
2020-09-30 02:55:37 +1000

    cleanup continuous anchor rotation test

--- tests/unit/rotation/tests.js
+++ tests/unit/rotation/tests.js
@@ -157,49 +157,20 @@ var testSuite = function (_jsPlumb) {
         equal(e16.anchor.getCurrentFace(), "right", "e16's anchor face is 'right'") ;
         equal(e17.anchor.getCurrentFace(), "top", "e17's anchor face is 'top'");
 
-        // var e16Loc = e16.anchor.lastReturnValue.slice(),
-        //     e17Loc = e17.anchor.lastReturnValue.slice();
-        //
-        // equal(e16Loc[0], 150, "e16 x position is correct initially");
-        // equal(e16Loc[1], 125, "e16 y position is correct initially");
-        // equal(e17Loc[0], 250, "e17 x position is correct initially");
-        // equal(e17Loc[1], 325, "e17 y position is correct initially");
-        //
-        // equal(e16.anchor.getOrientation()[0], 1, "e16 x orientation is correct initially");
-        // equal(e16.anchor.getOrientation()[1], 0, "e16 y orientation is correct initially");
-        // equal(e17.anchor.getOrientation()[0], -1, "e17 x orientation is correct initially");
-        // equal(e17.anchor.getOrientation()[1], 0, "e17 y orientation is correct initially");
-
         // now rotate e16 by 90 degrees
         _jsPlumb.rotate("d16", 90);
 
         equal(e16.anchor.getCurrentFace(), "top", "e16's anchor face is 'top'") ;
         equal(e17.anchor.getCurrentFace(), "top", "e17's anchor face is 'top'");
 
-        // var e16Loc2 = e16.anchor.lastReturnValue.slice();
-        // var e17Loc2 = e17.anchor.lastReturnValue.slice();
-        //
-        // equal("center center", d16.style.transformOrigin, "transform origin was set");
-        // equal(d16.style.transform, "rotate(90deg)", "rotate transform was set");
-        //
-        // equal(e16Loc2[0], 100, "e16 x position is correct after rotation of d16");
-        // equal(e16Loc2[1], 175, "e16 y position is correct after rotation of d16");
-        // // these unchanged currently
-        // equal(e17Loc2[0], 250, "e17 x position is unchanged after rotation of d16");
-        // equal(e17Loc2[1], 325, "e17 y position is unchanged after rotation of d16");
-
         // rotate d17 by 90 degrees
         _jsPlumb.rotate("d17", 90);
 
-        var e16Loc3 = e16.anchor.lastReturnValue.slice();
-        var e17Loc3 = e17.anchor.lastReturnValue.slice();
+        equal(e16.anchor.getCurrentFace(), "top", "e16's anchor face is 'top'") ;
+        equal(e17.anchor.getCurrentFace(), "left", "e17's anchor face is 'left'");
 
         equal("center center", d17.style.transformOrigin, "d17 transform origin was set");
         equal(d17.style.transform, "rotate(90deg)", "d17 rotate transform was set");
-        equal(e16Loc3[0], 100, "e16 x position is correct after rotation of 1d7");
-        equal(e16Loc3[1], 175, "e16 y position is correct after rotation of d17");
-        equal(e17Loc3[0], 300, "e17 x position is correct after rotation of d17");
-        equal(e17Loc3[1], 275, "e17 y position is correct after rotation of d17");
     });
 
 };
