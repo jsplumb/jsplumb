2f45b7486273f8a757d5fe28cca1f36ff2d63e9b
Simon Porritt <simon@jsplumbtoolkit.com>
2020-10-17 07:24:56 +1100

    updated to build es5/umd/cjs bundles

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,6 @@
 import resolve from 'rollup-plugin-node-resolve';
-//import commonjs from 'rollup-plugin-commonjs';
-
+import commonjs from 'rollup-plugin-commonjs';
 import babel from 'rollup-plugin-babel';
-//import typescript from 'rollup-plugin-typescript';
 
 const extensions = [
     '.ts'
@@ -10,38 +8,29 @@ const extensions = [
 
 
 export default [
-    // browser-friendly UMD build
     {
         input: './ts/index.ts',
-        output: {
-            name: 'jsplumb',
-            file: 'dist/js/jsplumb.js',
-            format: 'umd'
-        },
+        output: [
+            {
+                name: 'jsplumb',
+                file: 'dist/js/jsplumb-cjs.js',
+                format: 'cjs'
+            },
+            {
+                name: 'jsplumb',
+                file: 'dist/js/jsplumb-es.js',
+                format: 'es'
+            },
+            {
+                name: 'jsplumb',
+                file: 'dist/js/jsplumb.js',
+                format: 'umd'
+            }
+        ],
         plugins: [
-            resolve({ extensions }),   // so Rollup can find `ms`
-            //commonjs(),  // so Rollup can convert `ms` to an ES module
-            //typescript() // so Rollup can convert TypeScript to JavaScript
-
+            resolve({ extensions }),
+            commonjs(),
             babel({ extensions, include: ['ts/**/*'] })
         ]
-    }//,
-
-    // CommonJS (for Node) and ES module (for bundlers) build.
-    // (We could have three entries in the configuration array
-    // instead of two, but it's quicker to generate multiple
-    // builds from a single configuration where possible, using
-    // an array for the `output` option, where we can specify
-    // `file` and `format` for each target)
-    // {
-    //     input: 'src/main.ts',
-    //     external: ['ms'],
-    //     plugins: [
-    //         typescript() // so Rollup can convert TypeScript to JavaScript
-    //     ],
-    //     output: [
-    //         { file: pkg.main, format: 'cjs' },
-    //         { file: pkg.module, format: 'es' }
-    //     ]
-    // }
+    }
 ];
