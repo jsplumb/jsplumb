9715417b94ab2f412febf48bf1982dfc59f6e703
Simon Porritt <simon@jsplumbtoolkit.com>
2020-10-17 13:29:44 +1100

    implement repaint method

--- ts/selection/connection-selection.ts
+++ ts/selection/connection-selection.ts
@@ -3,20 +3,28 @@ import {Connection, ConnectorSpec} from ".."
 
 export class ConnectionSelection extends SelectionBase<Connection> {
 
-    setDetachable(d:boolean) {
+    setDetachable(d:boolean):ConnectionSelection {
         this.each((c:Connection) => c.setDetachable(d))
+        return this
     }
 
-    setReattach(d:boolean) {
+    setReattach(d:boolean):ConnectionSelection {
         this.each((c:Connection) => c.setReattach(d))
+        return this
     }
 
-    setConnector(spec:ConnectorSpec) {
+    setConnector(spec:ConnectorSpec):ConnectionSelection {
         this.each((c:Connection) => c.setConnector(spec))
+        return this
     }
 
     deleteAll() {
         this.each((c:Connection) => this.instance.deleteConnection(c))
         this.clear()
+    }
+
+    repaint():ConnectionSelection {
+        this.each((c:Connection) => c.paint())
+        return this
     }
 }
