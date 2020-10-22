04c2bce2276cffe9102dfb7abef3032c94afa84c
Simon Porritt <simon@jsplumbtoolkit.com>
2020-10-22 13:21:54 +1100

    removed. not needed.

--- scripts/create-packages.js
+++
@@ -1,9 +0,0 @@
-const version = require("../package.json").version
-const fs = require('fs')
-
-const inputFile = "./jsplumb-community-" + version + ".tgz"
-const j = fs.readFileSync(inputFile)
-fs.writeFileSync("./jsplumb.tgz", j)
-
-fs.unlink(inputFile, function() {})
-
