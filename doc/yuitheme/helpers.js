a2ba316ef313bc79b3b1b8be4729cc9270ef9d29
Simon Porritt <simon@jsplumbtoolkit.com>
2020-12-05 15:52:26 +1100

    tidy up

--- doc/yuitheme/helpers.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-	// find the project base dir from the assetPath
-    baseDir: function(assetPath) {
-        return "\"" + (assetPath.indexOf("../") == -1 ? ".." : "../..") + "\"";
-    }
-};
