53ccf9763e8e5a25161dd8b2f4061f602341bdb5
Simon Porritt <simon@jsplumbtoolkit.com>
2020-12-07 06:58:49 +1100

    refactored to copy defs into a subdirectory so the browser build can reference the core types

--- scripts/copy-typescript-defs.js
+++ scripts/copy-typescript-defs.js
@@ -1,19 +1,18 @@
 const fs = require('fs');
+const g = require('./gatlight');
 
-const sourceDir = "./_build_es6/ts";
-const targetDir = "./dist";
 
 function isDirectory(path) {
     return fs.lstatSync(path).isDirectory()
 }
 
-const _one = (dir, subdir) => {
+const _one = (dir, subdir, targetBase) => {
 
     const path = subdir == null ? dir : dir + "/" + subdir;
 
     if (subdir != null) {
         try {
-            fs.mkdirSync(targetDir + "/" + subdir);
+            fs.mkdirSync(targetBase + "/" + subdir);
         }
         catch (e) {
             // dir exists, no problem.
@@ -26,15 +25,21 @@ const _one = (dir, subdir) => {
     files.forEach((filename) => {
         inputPath = path + "/" + filename;
         if (isDirectory(inputPath)) {
-            _one(dir, (subdir ? subdir + "/" : "") + filename);
+            _one(dir, (subdir ? subdir + "/" : "") + filename, targetBase);
         } else {
             if (filename.endsWith(".d.ts")) {
                 const s = fs.readFileSync(path + "/" + filename);
-                fs.writeFileSync(targetDir + "/" + (subdir ? subdir + "/" : "") + filename, s);
+                fs.writeFileSync(targetBase + "/" + (subdir ? subdir + "/" : "") + filename, s);
             }
         }
     })
 
 };
 
-_one(sourceDir, null);
+g.mkdirs(targetDir + "/core/types");
+g.mkdirs(targetDir + "/dom/types/core");
+g.mkdirs(targetDir + "/dom/types/dom");
+
+_one("./_build_es6/ts/core",null, "./dist/core/types");
+_one("./_build_es6/ts/dom",null, "./dist/dom/types/dom");
+_one("./_build_es6/ts/core",null, "./dist/dom/types/core");
