05cdb6694ccd94f4501e3fe7facf913b4919a31a
Simon Porritt <simon@jsplumbtoolkit.com>
2020-12-07 11:32:37 +1100

    remove unused import

--- ts/dom/drag-manager.ts
+++ ts/dom/drag-manager.ts
@@ -1,6 +1,6 @@
 import {BrowserJsPlumbInstance, jsPlumbDOMElement} from "./browser-jsplumb-instance"
 
-import { BoundingBox, Dictionary, PointArray } from '../core/common'
+import { Dictionary, PointArray } from '../core/common'
 import { PARENT_GROUP_KEY } from '../core/constants'
 import { extend, wrap } from '../core/util'
 
