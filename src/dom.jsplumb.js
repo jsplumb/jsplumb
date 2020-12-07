19df93cef95016fcc253b590b8f69680ef5c4e11
Simon Porritt <simon@jsplumbtoolkit.com>
2020-12-08 08:37:11 +1100

    fix for #997, in which a `select` element was causing an infinite loop

--- src/dom.jsplumb.js
+++ src/dom.jsplumb.js
@@ -842,6 +842,7 @@
         },
         animationSupported:true,
         getElement: function (el) {
+
             if (el == null) {
                 return null;
             }
@@ -849,7 +850,7 @@
             // this is not my favourite thing to do, but previous versions of
             // jsplumb supported jquery selectors, and it is possible a selector
             // will be passed in here.
-            el = typeof el === "string" ? el : el.length != null && el.enctype == null ? el[0] : el;
+            el = typeof el === "string" ? el : (el.tagName == null && el.length != null && el.enctype == null) ? el[0] : el;
             return typeof el === "string" ? document.getElementById(el) : el;
         },
         removeElement: function (element) {
