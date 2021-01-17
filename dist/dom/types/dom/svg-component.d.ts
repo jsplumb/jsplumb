75bf385f7a35be9f98f64be5790f84856915f71f
Simon Porritt <simon@jsplumbtoolkit.com>
2021-01-18 10:37:45 +1100

    removed `dom` dir

--- dist/dom/types/dom/svg-component.d.ts
+++
@@ -1,4 +0,0 @@
-import { PaintStyle } from '../core/styles';
-export declare class SvgComponent {
-    static paint<E>(connector: any, useDivWrapper: boolean, paintStyle: PaintStyle, extents?: any): void;
-}
