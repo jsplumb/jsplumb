f41506f6528b6f7b1c73d1165664dc1a8fae509f
Simon Porritt <simon@jsplumbtoolkit.com>
2021-01-18 10:40:42 +1100

    removed all endpoint/connector registration (moved to community-core)

--- ts/dom/index.ts
+++ ts/dom/index.ts
@@ -1,28 +1,11 @@
 
 import {BrowserJsPlumbDefaults, BrowserJsPlumbInstance} from "./browser-jsplumb-instance"
-import {jsPlumbHelperFunctions} from '../core/defaults'
-
-import * as DotEndpoint from '../core/endpoint/dot-endpoint'
-import * as BlankEndpoint from '../core/endpoint/blank-endpoint'
-import * as RectangleEndpoint from '../core/endpoint/rectangle-endpoint'
 
 import * as DotEndpointRenderer from './dot-endpoint-renderer'
 import * as RectangleEndpointRenderer from './rectangle-endpoint-renderer'
 import * as BlankEndpointRenderer from './blank-endpoint-renderer'
 
-import * as StraightConnector from '../core/connector/straight-connector'
-import * as BezierConnector from '../core/connector/bezier-connector'
-import * as FlowchartConnector from '../core/connector/flowchart-connector'
-import * as StateMachineConnector from '../core/connector/statemachine-connector'
-
-BezierConnector.register()
-StraightConnector.register()
-FlowchartConnector.register()
-StateMachineConnector.register()
-
-DotEndpoint.register()
-BlankEndpoint.register()
-RectangleEndpoint.register()
+import {jsPlumbHelperFunctions} from "@jsplumb/community-core"
 
 DotEndpointRenderer.register()
 BlankEndpointRenderer.register()
@@ -35,11 +18,9 @@ function getInstanceIndex ():number {
     return i
 }
 
-export { JsPlumbInstance } from '../core/core'
 export * from './browser-jsplumb-instance'
 export * from './collicat'
 export { EventManager } from './event-manager'
-export { uuid, extend } from '../core/util'
 
 export function newInstance(defaults?:BrowserJsPlumbDefaults, helpers?:jsPlumbHelperFunctions): BrowserJsPlumbInstance {
     return new BrowserJsPlumbInstance(getInstanceIndex(), defaults, helpers)
