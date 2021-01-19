2e5c109cd4f12cb7a5673cc2df78716abecb3cc1
Simon Porritt <simon@jsplumbtoolkit.com>
2021-01-19 16:43:57 +1100

    alpha build with global name changed back to `jsPlumb`

--- dist/core/js/jsplumb.core.umd.js
+++ dist/core/js/jsplumb.core.umd.js
@@ -1,7 +1,7 @@
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
   typeof define === 'function' && define.amd ? define(['exports'], factory) :
-  (global = typeof globalThis !== 'undefined' ? globalThis : global || self, factory(global['@jsplumb/community-core'] = {}));
+  (global = typeof globalThis !== 'undefined' ? globalThis : global || self, factory(global.jsPlumb = {}));
 }(this, (function (exports) { 'use strict';
 
   function _classCallCheck(instance, Constructor) {
