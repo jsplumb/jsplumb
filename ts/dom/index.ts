cfadcdae5e1e9e9c6e02439bb8d4269ef78f1214
Simon Porritt <simon@jsplumbtoolkit.com>
2021-01-22 15:12:27 +1100

    add a few extra event related exports

--- ts/dom/index.ts
+++ ts/dom/index.ts
@@ -20,7 +20,9 @@ function getInstanceIndex ():number {
 
 export * from './browser-jsplumb-instance'
 export * from './collicat'
-export { EventManager } from './event-manager'
+export { EVENT_DRAG_START, EVENT_DRAG_MOVE, EVENT_DRAG_STOP } from './drag-manager'
+export { EventManager, pageLocation } from './event-manager'
+export * from "./browser-util"
 
 export function newInstance(defaults?:BrowserJsPlumbDefaults, helpers?:jsPlumbHelperFunctions): BrowserJsPlumbInstance {
     return new BrowserJsPlumbInstance(getInstanceIndex(), defaults, helpers)
