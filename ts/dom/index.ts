a19a1585fa25cdfae34f0a8ad5d68efc6215f32d
Simon Porritt <simon@jsplumbtoolkit.com>
2021-02-12 08:59:48 +1100

    export more events

--- ts/dom/index.ts
+++ ts/dom/index.ts
@@ -18,7 +18,7 @@ function getInstanceIndex ():number {
 
 export * from './browser-jsplumb-instance'
 export * from './collicat'
-export { EVENT_DRAG_START, EVENT_DRAG_MOVE, EVENT_DRAG_STOP } from './drag-manager'
+export { EVENT_DRAG_START, EVENT_DRAG_MOVE, EVENT_DRAG_STOP, EVENT_CONNECTION_DRAG, EVENT_CONNECTION_ABORT} from './drag-manager'
 export { EventManager, pageLocation } from './event-manager'
 export * from "./browser-util"
 
