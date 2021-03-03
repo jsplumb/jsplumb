60cf39fa800e21f1dec64be1122c77218f973c9c
Simon Porritt <simon@jsplumbtoolkit.com>
2021-03-03 15:43:07 +1100

    added EndpointStyle interface

--- ts/core/endpoint/endpoint.ts
+++ ts/core/endpoint/endpoint.ts
@@ -18,6 +18,7 @@ export type EndpointParams = any
 export type FullEndpointSpec = {type:EndpointId, options:EndpointParams}
 export type EndpointSpec = EndpointId | FullEndpointSpec
 
+export interface EndpointStyle extends PaintStyle, Record<string, any> {}
 
 export interface InternalEndpointOptions<E> extends EndpointOptions<E> {
     isTemporarySource?:boolean
