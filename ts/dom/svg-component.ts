a94a3acba286df7b2c010654899dac62cfaacc38
Simon Porritt <simon@jsplumbtoolkit.com>
2021-04-10 13:09:28 +1000

    added 'isFinite' test on width/height, to avoid trying to paint the svg element with inappropriate values

--- ts/dom/svg-component.ts
+++ ts/dom/svg-component.ts
@@ -22,25 +22,26 @@ export class SvgComponent {
                 wh[1] = extents.ymax + ((extents.ymin < 0) ? -extents.ymin : 0)
             }
 
-            if (useDivWrapper) {
-                _size((connector as any).canvas, xy[0], xy[1], wh[0], wh[1])
-                xy[0] = 0
-                xy[1] = 0
-                p = _pos([ 0, 0 ])
+            if (isFinite(wh[0]) && isFinite(wh[1])) {
+                if (useDivWrapper) {
+                    _size((connector as any).canvas, xy[0], xy[1], wh[0], wh[1])
+                    xy[0] = 0
+                    xy[1] = 0
+                    p = _pos([0, 0])
 
-                _attr(connector.svg, {
-                    "style": p,
-                    "width": "" + (wh[0] || 0),
-                    "height": "" + (wh[1] || 0)
-                })
-            }
-            else {
-                p = _pos([ xy[0], xy[1] ])
-                _attr(connector.canvas, {
-                    "style": p,
-                    "width": "" + (wh[0] || 0),
-                    "height": "" + (wh[1] || 0)
-                })
+                    _attr(connector.svg, {
+                        "style": p,
+                        "width": "" + (wh[0] || 0),
+                        "height": "" + (wh[1] || 0)
+                    })
+                } else {
+                    p = _pos([xy[0], xy[1]])
+                    _attr(connector.canvas, {
+                        "style": p,
+                        "width": "" + (wh[0] || 0),
+                        "height": "" + (wh[1] || 0)
+                    })
+                }
             }
         }
     }
