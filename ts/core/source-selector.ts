6abe65dc55051c8afb897bb2fc6f95ba107196e1
Simon Porritt <simon@jsplumbtoolkit.com>
2021-05-19 13:01:47 +1000

    added support for "strict" or "any" redrop policies

--- ts/core/source-selector.ts
+++ ts/core/source-selector.ts
@@ -13,11 +13,29 @@ export class ConnectionDragSelector {
     }
 }
 
+/**
+ * Indicates that when dragging an existing connection by its source endpoint, it can only be relocated onto some other source element by
+ * dropping it on the part of that element defined by its source selector.
+ */
+export const REDROP_POLICY_STRICT = "strict"
+/**
+ * Indicates that when dragging an existing connection by its source endpoint, it can be relocated onto some other source element by
+ * dropping it anywhere on that element.
+ */
+export const REDROP_POLICY_ANY = "any"
+/**
+ * Defines how redrop of source endpoints can be done.
+ */
+export type RedropPolicy = typeof REDROP_POLICY_STRICT | typeof REDROP_POLICY_ANY
+
 export class SourceSelector extends ConnectionDragSelector {
+    redrop:RedropPolicy
     constructor(selector:string, public def:SourceDefinition, exclude:boolean) {
         super(selector, def, exclude)
+        this.redrop = def.def.redrop || REDROP_POLICY_STRICT
     }
 }
+
 export class TargetSelector extends ConnectionDragSelector {
     constructor(selector:string, public def:TargetDefinition, exclude:boolean) {
         super(selector, def, exclude)
