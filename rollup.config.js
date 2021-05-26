85037f61fdcd41cb1d4741a9e564cf09a2db77b7
Simon Porritt <simon@jsplumbtoolkit.com>
2021-05-27 08:32:13 +1000

    refactored rollup config to generate the config from a list of packages and their global names

--- rollup.config.js
+++ rollup.config.js
@@ -18,156 +18,56 @@ function ON_WARN(warning, rollupWarn) {
     }
 }
 
-const EXTERNALS = {
-    "@jsplumb/core":'jsPlumb',
-    "@jsplumb/util":'jsPlumbUtil',
-    "@jsplumb/bezier":'jsPlumbBezier',
-    "@jsplumb/geom":'jsPlumbGeom'
+// package list is stored in package.json
+const packages = require("./package.json").packages
+// as are names for the packages.
+const packageNames = require("./package.json").packageNames
+
+//
+// gets the list of packages considered external to the current one, which is every package except the current one
+//
+function getExternalsList(current) {
+    return packages.filter(p => p !== current).map(p => `@jsplumb/${p}`)
 }
 
-export default [
-    {
-        input: './ts/util/index.ts',
-        output: [
-            {
-                name: 'jsPlumbUtil',
-                file: 'dist/util/js/jsplumb.util.cjs.js',
-                format: 'cjs'
-            },
-            {
-                name: 'jsPlumbUtil',
-                file: 'dist/util/js/jsplumb.util.es.js',
-                format: 'es'
-            },
-            {
-                name: 'jsPlumbUtil',
-                file: 'dist/util/js/jsplumb.util.umd.js',
-                format: 'umd'
-            }
-        ],
-        plugins: [
-            resolve({ extensions }),
-            commonjs(),
-            babel({ extensions, include: ['ts/util/**/*'] }),
-            cleanup({ extensions:['ts', 'js']})
-        ],
-        onwarn:ON_WARN
-    },
-    {
-        input: './ts/bezier/index.ts',
-        output: [
-            {
-                name: 'jsPlumbBezier',
-                file: 'dist/bezier/js/jsplumb.bezier.cjs.js',
-                format: 'cjs'
-            },
-            {
-                name: 'jsPlumbBezier',
-                file: 'dist/bezier/js/jsplumb.bezier.es.js',
-                format: 'es'
-            },
-            {
-                name: 'jsPlumbBezier',
-                file: 'dist/bezier/js/jsplumb.bezier.umd.js',
-                format: 'umd'
-            }
-        ],
-        plugins: [
-            resolve({ extensions }),
-            commonjs(),
-            babel({ extensions, include: ['ts/bezier/**/*'] }),
-            cleanup({ extensions:['ts', 'js']})
-        ],
-        onwarn:ON_WARN
-    },
-    {
-        input: './ts/geom/index.ts',
-        output: [
-            {
-                name: 'jsPlumbGeom',
-                file: 'dist/geom/js/jsplumb.geom.cjs.js',
-                format: 'cjs'
-            },
-            {
-                name: 'jsPlumbGeom',
-                file: 'dist/geom/js/jsplumb.geom.es.js',
-                format: 'es'
-            },
-            {
-                name: 'jsPlumbGeom',
-                file: 'dist/geom/js/jsplumb.geom.umd.js',
-                format: 'umd'
-            }
-        ],
-        plugins: [
-            resolve({ extensions }),
-            commonjs(),
-            babel({ extensions, include: ['ts/geom/**/*'] }),
-            cleanup({ extensions:['ts', 'js']})
-        ],
-        onwarn:ON_WARN
-    },
-    {
-        input: './ts/core/index.ts',
-        output: [
-            {
-                name: 'jsPlumb',
-                file: 'dist/core/js/jsplumb.core.cjs.js',
-                format: 'cjs'
-            },
-            {
-                name: 'jsPlumb',
-                file: 'dist/core/js/jsplumb.core.es.js',
-                format: 'es'
-            },
-            {
-                name: 'jsPlumb',
-                file: 'dist/core/js/jsplumb.core.umd.js',
-                format: 'umd',
-                globals:{
-                    "@jsplumb/util":'jsPlumbUtil',
-                    "@jsplumb/bezier":'jsPlumbBezier',
-                    "@jsplumb/geom":'jsPlumbGeom'
-                }
-            }
-        ],
-        plugins: [
-            resolve({ extensions }),
-            commonjs(),
-            babel({ extensions, include: ['ts/core/**/*'] }),
-            cleanup({ extensions:['ts', 'js']})
-        ],
-        onwarn:ON_WARN
-    },
-    {
-        input: './ts/dom/index.ts',
-        external: ['@jsplumb/core'],
-        output: [
-            {
-                name: 'jsPlumbBrowserUI',
-                file: 'dist/browser-ui/js/jsplumb.browser-ui.cjs.js',
-                format: 'cjs'
-            },
-            {
-                name: 'jsPlumbBrowserUI',
-                file: 'dist/browser-ui/js/jsplumb.browser-ui.es.js',
-                format: 'es'
-            },
-            {
-                name: 'jsPlumbBrowserUI',
-                file: 'dist/browser-ui/js/jsplumb.browser-ui.umd.js',
-                format: 'umd',
-                globals:EXTERNALS
-            }
-        ],
-        plugins: [
-            resolve({ extensions }),
-            commonjs(),
-            babel({ extensions, include: ['ts/**/*'] }),
-            cleanup({ extensions:['ts', 'js']})
-        ],
-        onwarn:ON_WARN
+const out = []
+packages.filter(p => {
+    const name = packageNames[`@jsplumb/${p}`]
+    if (name == null) {
+        throw `No package name found in package.json for package ${p}. Add a mapping in the "packageNames" map for this package.`
     }
-];
+    out.push({
+        input: `./ts/${p}/index.ts`,
+        external:getExternalsList(p),
+        output: [
+            {
+                name: name,
+                file: `dist/${p}/js/jsplumb.${p}.cjs.js`,
+                format: 'cjs'
+            },
+            {
+                name: name,
+                file: `dist/${p}/js/jsplumb.${p}.es.js`,
+                format: 'es'
+            },
+            {
+                name: name,
+                file: `dist/${p}/js/jsplumb.${p}.umd.js`,
+                format: 'umd',
+                globals:packageNames
+            }
+        ],
+        plugins: [
+            resolve({ extensions }),
+            commonjs(),
+            babel({ extensions, include: [`ts/${p}/**/*`] }),
+            cleanup({ extensions:['ts', 'js']})
+        ],
+        onwarn:ON_WARN
+    })
+})
+
+export default out
+
 
 
