3d4b768162e38f20f247dc51ebd5dde74a6cf3fa
Simon Porritt <simon@jsplumbtoolkit.com>
2021-05-27 09:55:36 +1000

    - removed unnecessary static class and just export the methods. - getPathData is now on the instance and not on AbstractConnector

--- ts/browser-ui/svg-element-connector.ts
+++ ts/browser-ui/svg-element-connector.ts
@@ -4,98 +4,92 @@ import {extend, Extents} from "@jsplumb/util"
 
 import {SvgComponent} from "./svg-component"
 import {_appendAtIndex, _applyStyles, _attr, _node, ELEMENT_PATH, ELEMENT_SVG} from './svg-util'
+import {BrowserJsPlumbInstance} from "@jsplumb/browser-ui/browser-jsplumb-instance"
 
 
-/**
- * Renderer for a connector that uses an `svg` element in the DOM.
- */
-export class SvgElementConnector {
+export function paintSvgConnector(instance:BrowserJsPlumbInstance, connector:AbstractConnector, paintStyle:PaintStyle, extents?:Extents) {
 
-    static paint(connector:AbstractConnector, paintStyle:PaintStyle, extents?:Extents) {
+    getConnectorElement(instance, connector)
 
-        this.getConnectorElement(connector)
+    SvgComponent.paint(connector, false, paintStyle, extents)
 
-        SvgComponent.paint(connector, false, paintStyle, extents)
+    let segments = connector.getSegments()
 
-        let segments = connector.getSegments()
-
-        let p = "", offset = [0, 0]
-        if (extents.xmin < 0) {
-            offset[0] = -extents.xmin
-        }
-        if (extents.ymin < 0) {
-            offset[1] = -extents.ymin
-        }
-
-        if (segments.length > 0) {
-
-            p = connector.getPathData()
-
-            let a = {
-                    d: p,
-                    transform: "translate(" + offset[0] + "," + offset[1] + ")",
-                    "pointer-events": "visibleStroke"
-                },
-                outlineStyle:PaintStyle = null,
-                d = [connector.x, connector.y, connector.w, connector.h]
-
-            // outline style.  actually means drawing an svg object underneath the main one.
-            if (paintStyle.outlineStroke) {
-                let outlineWidth = paintStyle.outlineWidth || 1,
-                    outlineStrokeWidth = paintStyle.strokeWidth + (2 * outlineWidth)
-                outlineStyle = extend({}, paintStyle)
-                outlineStyle.stroke = paintStyle.outlineStroke
-                outlineStyle.strokeWidth = outlineStrokeWidth
-
-                if ((connector as any).bgPath == null) {
-                    (connector as any).bgPath = _node(ELEMENT_PATH, a)
-                    connector.instance.addClass((connector as any).bgPath as any, connector.instance.connectorOutlineClass)
-                    _appendAtIndex((connector as any).canvas, (connector as any).bgPath, 0)
-                }
-                else {
-                    _attr((connector as any).bgPath, a)
-                }
-
-                _applyStyles((connector as any).canvas, (connector as any).bgPath, outlineStyle)
-            }
-
-            if ((connector as any).path == null) {
-                (connector as any).path = _node(ELEMENT_PATH, a)
-                _appendAtIndex((connector as any).canvas, (connector as any).path, paintStyle.outlineStroke ? 1 : 0)
-            }
-            else {
-                _attr((connector as any).path, a)
-            }
-
-            _applyStyles((connector as any).canvas, (connector as any).path, paintStyle, )
-        }
+    let p = "", offset = [0, 0]
+    if (extents.xmin < 0) {
+        offset[0] = -extents.xmin
+    }
+    if (extents.ymin < 0) {
+        offset[1] = -extents.ymin
     }
 
-    static getConnectorElement(c:AbstractConnector):SVGElement {
-        if ((c as any).canvas != null) {
-            return (c as any).canvas
-        } else {
-            const svg:any = _node(ELEMENT_SVG, {
-                "style": "",
-                "width": "0",
-                "height": "0",
-                "pointer-events": NONE,
-                "position": ABSOLUTE
-            });
-            (c as any).canvas = svg
-            c.instance._appendElement((c as any).canvas, c.instance.getContainer())
+    if (segments.length > 0) {
 
-            // TODO BG CANVAS! does it even need to be a canvas? i suppose not.
+        p = instance.getPathData(connector)
 
-            if (c.cssClass != null) {
-                c.instance.addClass(svg, c.cssClass)
+        let a = {
+                d: p,
+                transform: "translate(" + offset[0] + "," + offset[1] + ")",
+                "pointer-events": "visibleStroke"
+            },
+            outlineStyle:PaintStyle = null
+
+        // outline style.  actually means drawing an svg object underneath the main one.
+        if (paintStyle.outlineStroke) {
+            let outlineWidth = paintStyle.outlineWidth || 1,
+                outlineStrokeWidth = paintStyle.strokeWidth + (2 * outlineWidth)
+            outlineStyle = extend({}, paintStyle)
+            outlineStyle.stroke = paintStyle.outlineStroke
+            outlineStyle.strokeWidth = outlineStrokeWidth
+
+            if ((connector as any).bgPath == null) {
+                (connector as any).bgPath = _node(ELEMENT_PATH, a)
+                instance.addClass((connector as any).bgPath as any, instance.connectorOutlineClass)
+                _appendAtIndex((connector as any).canvas, (connector as any).bgPath, 0)
             }
-            c.instance.addClass(svg, c.instance.connectorClass)
+            else {
+                _attr((connector as any).bgPath, a)
+            }
 
-            svg.jtk = svg.jtk || { }
-            svg.jtk.connector = c
-
-            return svg as SVGElement
+            _applyStyles((connector as any).canvas, (connector as any).bgPath, outlineStyle)
         }
+
+        if ((connector as any).path == null) {
+            (connector as any).path = _node(ELEMENT_PATH, a)
+            _appendAtIndex((connector as any).canvas, (connector as any).path, paintStyle.outlineStroke ? 1 : 0)
+        }
+        else {
+            _attr((connector as any).path, a)
+        }
+
+        _applyStyles((connector as any).canvas, (connector as any).path, paintStyle, )
+    }
+}
+
+export function getConnectorElement(instance:BrowserJsPlumbInstance, c:AbstractConnector):SVGElement {
+    if ((c as any).canvas != null) {
+        return (c as any).canvas
+    } else {
+        const svg:any = _node(ELEMENT_SVG, {
+            "style": "",
+            "width": "0",
+            "height": "0",
+            "pointer-events": NONE,
+            "position": ABSOLUTE
+        });
+        (c as any).canvas = svg
+        instance._appendElement((c as any).canvas, instance.getContainer())
+
+        // TODO BG CANVAS! does it even need to be a canvas? i suppose not.
+
+        if (c.cssClass != null) {
+            instance.addClass(svg, c.cssClass)
+        }
+        instance.addClass(svg, instance.connectorClass)
+
+        svg.jtk = svg.jtk || { }
+        svg.jtk.connector = c
+
+        return svg as SVGElement
     }
 }
