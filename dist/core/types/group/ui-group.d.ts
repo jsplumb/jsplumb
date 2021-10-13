e36e9aedcbfdbdfe0230b568d6a87f9998a7d251
Simon Porritt <simon@jsplumbtoolkit.com>
2021-10-13 13:58:44 +1100

    added some .d.ts

---
+++ dist/core/types/group/ui-group.d.ts
@@ -0,0 +1,4 @@
+import { UINode } from "./ui-node";
+export interface UIGroup extends UINode {
+}
+//# sourceMappingURL=ui-group.d.ts.map
---
+++ dist/core/types/group/ui-node.d.ts
@@ -0,0 +1,5 @@
+import { UIGroup } from "./ui-group";
+export interface UINode {
+    group: UIGroup;
+}
+//# sourceMappingURL=ui-node.d.ts.map
