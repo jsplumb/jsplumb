255fac6ccdffdb35d5949bbc00f9dbdf5b0ddd16
Simon Porritt <simon@jsplumbtoolkit.com>
2022-06-03 19:37:07 +1000

    add keys for defaults as constants

--- ts/core/defaults.ts
+++ ts/core/defaults.ts
@@ -4,36 +4,59 @@ export interface ListSpec {
     endpoint?: EndpointSpec
 }
 
+export const DEFAULT_KEY_ALLOW_NESTED_GROUPS = "allowNestedGroups"
+export const DEFAULT_KEY_ANCHOR = "anchor"
+export const DEFAULT_KEY_ANCHORS = "anchors"
+export const DEFAULT_KEY_CONNECTION_OVERLAYS = "connectionOverlays"
+export const DEFAULT_KEY_CONNECTIONS_DETACHABLE = "connectionsDetachable"
+export const DEFAULT_KEY_CONNECTOR = "connector"
+export const DEFAULT_KEY_CONTAINER = "container"
+export const DEFAULT_KEY_ENDPOINT = "endpoint"
+export const DEFAULT_KEY_ENDPOINT_OVERLAYS = "endpointOverlays"
+export const DEFAULT_KEY_ENDPOINTS = "endpoints"
+export const DEFAULT_KEY_ENDPOINT_STYLE = "endpointStyle"
+export const DEFAULT_KEY_ENDPOINT_STYLES = "endpointStyles"
+export const DEFAULT_KEY_ENDPOINT_HOVER_STYLE = "endpointHoverStyle"
+export const DEFAULT_KEY_ENDPOINT_HOVER_STYLES = "endpointHoverStyles"
+export const DEFAULT_KEY_HOVER_CLASS = "hoverClass"
+export const DEFAULT_KEY_HOVER_PAINT_STYLE = "hoverPaintStyle"
+export const DEFAULT_KEY_LIST_STYLE = "listStyle"
+export const DEFAULT_KEY_MAX_CONNECTIONS = "maxConnections"
+export const DEFAULT_KEY_PAINT_STYLE = "paintStyle"
+export const DEFAULT_KEY_REATTACH_CONNECTIONS = "reattachConnections"
+export const DEFAULT_KEY_SCOPE = "scope"
+
+
 export interface JsPlumbDefaults<E> {
-    endpoint?: EndpointSpec
-    endpoints?: [ EndpointSpec, EndpointSpec ]
-    anchor?: AnchorSpec
-    anchors?: [ AnchorSpec, AnchorSpec ]
-    paintStyle?: PaintStyle
-    hoverPaintStyle?: PaintStyle
+    [DEFAULT_KEY_ENDPOINT]?: EndpointSpec
+    [DEFAULT_KEY_ENDPOINTS]?: [ EndpointSpec, EndpointSpec ]
+    [DEFAULT_KEY_ANCHOR]?: AnchorSpec
+    [DEFAULT_KEY_ANCHORS]?: [ AnchorSpec, AnchorSpec ]
+    [DEFAULT_KEY_PAINT_STYLE]?: PaintStyle
+    [DEFAULT_KEY_HOVER_PAINT_STYLE]?: PaintStyle
 
-    endpointStyle?: EndpointStyle
-    endpointHoverStyle?: EndpointStyle
-    endpointStyles?: [ EndpointStyle, EndpointStyle ]
-    endpointHoverStyles?: [ EndpointStyle, EndpointStyle ],
+    [DEFAULT_KEY_ENDPOINT_STYLE]?: EndpointStyle
+    [DEFAULT_KEY_ENDPOINT_HOVER_STYLE]?: EndpointStyle
+    [DEFAULT_KEY_ENDPOINT_STYLES]?: [ EndpointStyle, EndpointStyle ]
+    [DEFAULT_KEY_ENDPOINT_HOVER_STYLES]?: [ EndpointStyle, EndpointStyle ],
 
-    connectionsDetachable?: boolean
-    reattachConnections?: boolean
+    [DEFAULT_KEY_CONNECTIONS_DETACHABLE]?: boolean
+    [DEFAULT_KEY_REATTACH_CONNECTIONS]?: boolean
 
-    endpointOverlays?: Array<OverlaySpec>
-    connectionOverlays?: Array<OverlaySpec>
+    [DEFAULT_KEY_ENDPOINT_OVERLAYS]?: Array<OverlaySpec>
+    [DEFAULT_KEY_CONNECTION_OVERLAYS]?: Array<OverlaySpec>
 
-    listStyle?: ListSpec
+    [DEFAULT_KEY_LIST_STYLE]?: ListSpec
 
-    container?: E
-    connector?:ConnectorSpec
-    scope?:string
+    [DEFAULT_KEY_CONTAINER]?: E
+    [DEFAULT_KEY_CONNECTOR]?:ConnectorSpec
+    [DEFAULT_KEY_SCOPE]?:string
 
-    maxConnections?:number
+    [DEFAULT_KEY_MAX_CONNECTIONS]?:number
 
-    hoverClass?:string
+    [DEFAULT_KEY_HOVER_CLASS]?:string
 
-    allowNestedGroups?:boolean
+    [DEFAULT_KEY_ALLOW_NESTED_GROUPS]?:boolean
 
 }
 
