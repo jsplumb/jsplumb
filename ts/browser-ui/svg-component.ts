f2038f3338a2d007990de9343c0d783d2dfe658b
Simon Porritt <simon@jsplumbtoolkit.com>
2022-07-04 19:31:30 +1000

    remove support for div wrapper

--- ts/browser-ui/svg-component.ts
+++ ts/browser-ui/svg-component.ts
@@ -23,25 +23,12 @@ export class SvgComponent {
             }
 
             if (isFinite(wh[0]) && isFinite(wh[1])) {
-                if (useDivWrapper) {
-                    _size((connector as any).canvas, xy[0], xy[1], wh[0], wh[1])
-                    xy[0] = 0
-                    xy[1] = 0
-                    p = _pos([0, 0])
-
-                    _attr(connector.svg, {
-                        "style": p,
-                        "width": "" + (wh[0] || 0),
-                        "height": "" + (wh[1] || 0)
-                    })
-                } else {
-                    p = _pos([xy[0], xy[1]])
-                    _attr(connector.canvas, {
-                        "style": p,
-                        "width": "" + (wh[0] || 0),
-                        "height": "" + (wh[1] || 0)
-                    })
-                }
+                p = _pos([xy[0], xy[1]])
+                _attr(connector.canvas, {
+                    "style": p,
+                    "width": "" + (wh[0] || 0),
+                    "height": "" + (wh[1] || 0)
+                })
             }
         }
     }
