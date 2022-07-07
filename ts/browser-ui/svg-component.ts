85f28a3c2bcd5ad06292d6e7faaeb4ca987ec080
Simon Porritt <simon@jsplumbtoolkit.com>
2022-07-07 13:36:58 +1000

    update to support svg container vs html container

--- ts/browser-ui/svg-component.ts
+++ ts/browser-ui/svg-component.ts
@@ -1,15 +1,17 @@
 import {PaintStyle} from "@jsplumb/common"
-import {Extents} from "@jsplumb/util"
+import {extend, Extents} from "@jsplumb/util"
 
 import { _attr, _pos, _size  } from './svg-util'
+import {BrowserJsPlumbInstance} from "./browser-jsplumb-instance"
+import {ElementTypes} from "./browser-util"
 
 export class SvgComponent {
 
-    static paint<E>(connector:any, useDivWrapper:boolean, paintStyle:PaintStyle, extents?:Extents):void {
+    static paint<E>(connector:any, instance:BrowserJsPlumbInstance, paintStyle:PaintStyle, extents?:Extents):void {
         if (paintStyle != null) {
 
             let xy = [ connector.x, connector.y ],
-                wh = [ connector.w, connector.h ], p
+                wh = [ connector.w, connector.h ]
 
             if (extents != null) {
                 if (extents.xmin < 0) {
@@ -23,12 +25,22 @@ export class SvgComponent {
             }
 
             if (isFinite(wh[0]) && isFinite(wh[1])) {
-                p = _pos([xy[0], xy[1]])
-                _attr(connector.canvas, {
-                    "style": p,
+
+                const attrs = {
                     "width": "" + (wh[0] || 0),
                     "height": "" + (wh[1] || 0)
-                })
+                }
+
+                if (instance.containerType === ElementTypes.HTML) {
+                    _attr(connector.canvas, extend(attrs as any,{
+                        style:_pos([xy[0], xy[1]])
+                    }))
+                } else {
+                    _attr(connector.canvas, extend(attrs as any,{
+                        x:xy[0],
+                        y:xy[1]
+                    }))
+                }
             }
         }
     }
